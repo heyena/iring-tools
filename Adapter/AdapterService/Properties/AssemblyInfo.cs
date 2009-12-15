1a251448ecc481ef32f3126b47d32e9295305899
monika.saxena <monika.saxena@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2009-12-15 16:12:42 +0000

    Fixed Assembly Infor for UnitTests, Utilities and Services and Editors.

--- Adapter/AdapterService/Properties/AssemblyInfo.cs
+++ Adapter/AdapterService/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdapterService")]
+[assembly: AssemblyTitle("Adapter Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bechtel Corporation")]
-[assembly: AssemblyProduct("AdapterService")]
-[assembly: AssemblyCopyright("Copyright © Bechtel Corporation 2009")]
+[assembly: AssemblyCompany("iringug.org")]
+[assembly: AssemblyProduct("iRINGTools")]
+[assembly: AssemblyCopyright("Copyright © 2009, iringug.org")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
