347c3ca68a47d27acb6aef32034c6becdd025357
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2009-12-22 15:09:12 +0000

    Fixed error running SQL command.

--- trunk/InterfaceService.SemWeb/setup.bat
+++ trunk/InterfaceService.SemWeb/setup.bat
@@ -9,13 +9,33 @@ for /f "tokens=1,2,3 delims== " %%i in (setup.conf) do (
   if "%%i" equ "sql_password" set dbPassword=%%j
 )
 
-if %dbUser% equ "" if %dbPassword% equ "" (@echo Windows authentication SQLCMD -S %dbServer% -i  Scripts\iRING.sql) 
-if %dbUser% neq "" if %dbPassword% neq "" (@echo SqlServer authentication SQLCMD -U %dbUser% -P %dbPassword% -S %dbServer% -i  .\Scripts\iRING.sql)
+if %dbUser% equ "" (
+  if %dbPassword% equ "" (
+    @echo Using Windows authentication ...
+    SQLCMD -S %dbServer% -i  .\Scripts\iRING.sql
+    
+    if %errorlevel% neq 0 (
+      goto end
+    )
+  ) 
+)
+if %dbUser% neq "" (
+  if %dbPassword% neq "" (
+    @echo Using SQLServer authentication ...
+    SQLCMD -U %dbUser% -P %dbPassword% -S %dbServer% -i  .\Scripts\iRING.sql
+    
+    if %errorlevel% neq 0 (
+      goto end
+    )
+  )
+)
 
 rem Initialize RDF storage
+@echo Initializing RDF storage ...
 .\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;"
 
 rem Update web.config file
+@echo Updating web configuration file ...
 set old_key=".*key=\"/InterfaceService/sparql\".*"
 set new_key="    <add key=\"/InterfaceService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;\"/>"
 .\Bin\replace web.config %old_key% %new_key% > web.config.tmp
@@ -23,4 +43,6 @@ if %errorlevel% equ 0 (
   move /y web.config.tmp web.config >nul
 )
 
+:end
+endlocal
 pause
