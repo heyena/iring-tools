3bdb4fa846c3b1b99f9cdcd3f4280c22dfe4d407
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2009-12-22 00:08:14 +0000

    Used fixed database name and login for SemWeb interface and sandbox service.

--- InterfaceService.SemWeb/setup.bat
+++ InterfaceService.SemWeb/setup.bat
@@ -5,7 +5,6 @@ setlocal
 rem Get properties from setup.conf
 for /f "tokens=1,2,3 delims== " %%i in (setup.conf) do (
   if "%%i" equ "sql_server" set dbServer=%%j
-  if "%%i" equ "sql_catalog" set dbCatalog=%%j
   if "%%i" equ "sql_username" set dbUser=%%j
   if "%%i" equ "sql_password" set dbPassword=%%j
 )
@@ -14,11 +13,11 @@ if %dbUser% equ "" if %dbPassword% equ "" (@echo Windows authentication SQLCMD -
 if %dbUser% neq "" if %dbPassword% neq "" (@echo SqlServer authentication SQLCMD -U %dbUser% -P %dbPassword% -S %dbServer% -i  .\Scripts\iRING.sql)
 
 rem Initialize RDF storage
-.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=%dbCatalog%; User Id=%dbUser%; Password=%dbPassword%;"
+.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;"
 
 rem Update web.config file
 set old_key=".*key=\"/InterfaceService/sparql\".*"
-set new_key="    <add key=\"/InterfaceService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=%dbCatalog%; User Id=%dbUser%; Password=%dbPassword%;\"/>"
+set new_key="    <add key=\"/InterfaceService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;\"/>"
 .\Bin\replace web.config %old_key% %new_key% > web.config.tmp
 if %errorlevel% equ 0 (
   move /y web.config.tmp web.config >nul
--- SandboxService.SemWeb/setup.bat
+++ SandboxService.SemWeb/setup.bat
@@ -5,7 +5,6 @@ setlocal
 rem Get properties from setup.conf
 for /f "tokens=1,2,3 delims== " %%i in (setup.conf) do (
   if "%%i" equ "sql_server" set dbServer=%%j
-  if "%%i" equ "sql_catalog" set dbCatalog=%%j
   if "%%i" equ "sql_username" set dbUser=%%j
   if "%%i" equ "sql_password" set dbPassword=%%j
 )
@@ -14,11 +13,11 @@ if %dbUser% equ "" if %dbPassword% equ "" (@echo Windows authentication SQLCMD -
 if %dbUser% neq "" if %dbPassword% neq "" (@echo SqlServer authentication SQLCMD -U %dbUser% -P %dbPassword% -S %dbServer% -i  .\Scripts\sandbox.sql)
 
 rem Initialize RDF storage
-.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=%dbCatalog%; User Id=%dbUser%; Password=%dbPassword%;"
+.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=sandbox; User Id=sandbox; Password=sandbox;"
 
 rem Update web.config file
 set old_key=".*key=\"/SandboxService/sparql\".*"
-set new_key="    <add key=\"/SandboxService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=%dbCatalog%; User Id=%dbUser%; Password=%dbPassword%;\"/>"
+set new_key="    <add key=\"/SandboxService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=sandbox; User Id=sandbox; Password=sandbox;\"/>"
 .\Bin\replace web.config %old_key% %new_key% > web.config.tmp
 if %errorlevel% equ 0 (
   move /y web.config.tmp web.config >nul
