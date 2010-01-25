6f2d6e9eaafd5faeb9cea091d2d4deed80721275
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-01-25 21:28:39 +0000

    Added ErrorHandlingDemo solution to Prototypes.

---
+++ Prototypes/ErrorHandlingDemo/TestService/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+
+namespace TestService
+{
+  [ServiceContract]
+  public interface IService
+  {
+    [OperationContract]
+    [WebGet(UriTemplate = "/hello")]
+    string SayHello();
+
+    [OperationContract]
+    [FaultContract(typeof(ArithmeticFault))]
+    [WebGet(UriTemplate = "/fault")]
+    string GetFault();
+  }
+}
