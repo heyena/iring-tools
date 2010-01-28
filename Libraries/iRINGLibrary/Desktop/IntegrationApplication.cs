d1dca68a32e5c0f7ef0c7ee96d16dc02c3fd03af
jkinter@gmail.com <jkinter@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-01-28 00:19:23 +0000

    Added comments and changed the IntegrationApplication from a Response to an IntegrationApplication.

--- Libraries/iRINGLibrary/Desktop/IntegrationApplication.cs
+++ Libraries/iRINGLibrary/Desktop/IntegrationApplication.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace org.iringtools.library
+﻿namespace org.iringtools.library
 {
+    /// <summary>
+    /// This class represents an application that is using the data for a specific project
+    /// </summary>
     public class IntegrationApplication
     {
         private string _name;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public IntegrationApplication()
         {
             _name = string.Empty;
         }
 
-        public IntegrationApplication(string Name)
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="name">The name of the application</param>
+        public IntegrationApplication(string name)
         {
-            _name = Name;
+            _name = name;
         }
 
+        /// <summary>
+        /// The name of the application represented by this instance
+        /// </summary>
+        /// <returns>A string of the Name of the application</returns>
         public string Name()
         {
             return _name;
--- Libraries/iRINGLibrary/Desktop/IntegrationProject.cs
+++ Libraries/iRINGLibrary/Desktop/IntegrationProject.cs
@@ -1,33 +1,58 @@
-﻿namespace org.iringtools.library
+﻿using System.Collections.Generic;
+
+namespace org.iringtools.library
 {
+    /// <summary>
+    /// This class represents a node in the Project.xml file with
+    /// specific attention paid to the parent/child relationship between projects and applications
+    /// </summary>
    public class IntegrationProject
     {
         private string _name;
-        private Response _applications;
+        private List<IntegrationApplication> _applications;
 
+       /// <summary>
+       /// Default constructor
+       /// </summary>
         public IntegrationProject()
         {
             _name = string.Empty;
-            Response _applications = new Response();
+            List<IntegrationApplication> _applications = new List<IntegrationApplication>();
         }
 
-        public IntegrationProject(string Name)
+       /// <summary>
+       /// Overloaded constructor allowing the project name to be set at instantitaion
+       /// </summary>
+       /// <param name="name">The name of the project</param>
+        public IntegrationProject(string name)
         {
-            _name = Name;
-            Response _applications = new Response();
+            _name = name;
+            List<IntegrationApplication> _applications = new List<IntegrationApplication>();
         }
 
+       /// <summary>
+       /// The name of the project
+       /// </summary>
+       /// <returns>a string</returns>
         public string Name()
         {
             return _name;
         }
 
-        public void AddApplication(IntegrationApplication Application)
+       /// <summary>
+       /// Public method to add a new application to the Applications collection
+       /// </summary>
+       /// <param name="application">A populated IntegrationApplication object</param>
+        public void AddApplication(IntegrationApplication application)
         {
-            _applications.Add(Application.Name());
+            _applications.Add(application);
         }
 
-        public Response Applications()
+       /// <summary>
+       /// The collection of associated IntegrationApplications
+       /// </summary>
+       /// <returns>A strongly type List of IntegrationApplication objects</returns>
+        public List<IntegrationApplication> Applications()
         {
             return _applications;
         }
