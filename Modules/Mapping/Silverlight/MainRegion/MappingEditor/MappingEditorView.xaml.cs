06dc67a0619552c56e2ffae681ff85a04b0d228b
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-02 15:02:25 +0000

    Mapping editor UI changes -- adding of ComboBoxes

--- Modules/Mapping/Silverlight/MainRegion/MappingEditor/MappingEditorView.xaml.cs
+++ Modules/Mapping/Silverlight/MainRegion/MappingEditor/MappingEditorView.xaml.cs
@@ -14,12 +14,21 @@ namespace org.iringtools.modules.mainregion
       LayoutRoot.LayoutUpdated += new System.EventHandler(LayoutRoot_LayoutUpdated);
 
       LayoutRoot.SizeChanged += new System.Windows.SizeChangedEventHandler(LayoutRoot_SizeChanged);
-
+      ProjectApplication.Loaded += new System.Windows.RoutedEventHandler(ProjectApplication_SizeChanged);
       DataSource.Loaded += new System.Windows.RoutedEventHandler(DataSource_SizeChanged);
       Search.Loaded += new System.Windows.RoutedEventHandler(Search_SizeChanged);
       //Navigation.Loaded += new System.Windows.RoutedEventHandler(Navigation_SizeChanged);
       Details.Loaded += new System.Windows.RoutedEventHandler(Details_SizeChanged);
       Mapping.Loaded += new System.Windows.RoutedEventHandler(Mapping_SizeChanged);
+    }
+
+    void ProjectApplication_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (ProjectApplication.Items.Count > 0)
+        {
+            UserControl userControl = (UserControl)ProjectApplication.Items[0];
+            userControl.Height = LayoutRoot.RowDefinitions[0].ActualHeight;
+        }
     }
 
     void LayoutRoot_LayoutUpdated(object sender, System.EventArgs e)
@@ -32,6 +41,7 @@ namespace org.iringtools.modules.mainregion
     {
       DataSource_SizeChanged(sender, e);
       Search_SizeChanged(sender, e);
+      ProjectApplication_SizeChanged(sender, e);
       //Navigation_SizeChanged(sender, e);
       Details_SizeChanged(sender, e);
       Mapping_SizeChanged(sender, e);
@@ -43,7 +53,7 @@ namespace org.iringtools.modules.mainregion
       {
         UserControl userControl = (UserControl)DataSource.Items[0];
         //double rowSpanHeight = LayoutRoot.RowDefinitions[0].ActualHeight + LayoutRoot.RowDefinitions[1].ActualHeight;
-        userControl.Height = LayoutRoot.RowDefinitions[0].ActualHeight; //rowSpanHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight; //rowSpanHeight;
         //userControl.Width = LayoutRoot.ColumnDefinitions[0].ActualWidth;
       }
     }
@@ -53,7 +63,7 @@ namespace org.iringtools.modules.mainregion
       if (Search.Items.Count > 0)
       {
         UserControl userControl = (UserControl)Search.Items[0];
-        userControl.Height = LayoutRoot.RowDefinitions[0].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight;
         //userControl.Width = LayoutRoot.ColumnDefinitions[1].ActualWidth;
       }
     }
@@ -73,7 +83,7 @@ namespace org.iringtools.modules.mainregion
       if (Details.Items.Count > 0)
       {
         UserControl userControl = (UserControl)Details.Items[0];
-        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[2].ActualHeight;
         //userControl.Width = LayoutRoot.ColumnDefinitions[0].ActualWidth;
       }
     }
@@ -83,7 +93,7 @@ namespace org.iringtools.modules.mainregion
       if (Mapping.Items.Count > 0)
       {
         UserControl userControl = (UserControl)Mapping.Items[0];
-        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[2].ActualHeight;
         //userControl.Width = LayoutRoot.ColumnDefinitions[1].ActualWidth;
       }
     }
---
+++ Modules/Mapping/Silverlight/_MappingEditor/ProjectApplicationRegion/ProjectApplicationPresenter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using Microsoft.Practices.Composite.Events;
+using Microsoft.Practices.Unity;
+
+using org.iringtools.modulelibrary.entities;
+using org.iringtools.modulelibrary.events;
+using org.iringtools.modulelibrary.extensions;
+using org.iringtools.modulelibrary.types;
+
+using org.iringtools.ontologyservice.presentation.presentationmodels;
+using org.iringtools.informationmodel.events;
+using PrismContrib.Base;
+
+using InformationModel.Events;
+using org.iringtools.library;
+
+
+using org.iringtools.library.configuration;
+
+namespace org.iringtools.modules.projectapplicationregion
+{
+    public class ProjectApplicationPresenter : PresenterBase<ProjectApplicationView>
+    {
+        private IEventAggregator aggregator = null;
+        private IAdapter adapterProxy = null;
+        IIMPresentationModel model = null;
+
+        private ComboBox prjCB { get { return ComboBoxCtrl("ProjectCombo"); } }
+
+        private ComboBox appCB { get { return ComboBoxCtrl("AppCombo"); } }
+  
+
+        public ProjectApplicationPresenter(IProjectApplicationView view, IIMPresentationModel model,
+      IEventAggregator aggregator,
+      IAdapter adapterProxy,
+      IUnityContainer container)
+            : base(view, model)
+        {
+           
+            prjCB.SelectionChanged += new SelectionChangedEventHandler(prjCB_SelectionChanged);
+            appCB.SelectionChanged += new SelectionChangedEventHandler(appCB_SelectionChanged);
+            adapterProxy.OnDataArrived += new EventHandler<EventArgs>(adapterProxy_OnDataArrived);
+            
+        }
+
+        void adapterProxy_OnDataArrived(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        void appCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (prjCB.SelectedIndex != -1)
+            {
+              
+            }
+        }
+
+        void prjCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
