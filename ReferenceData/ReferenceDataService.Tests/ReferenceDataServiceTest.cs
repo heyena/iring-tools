50ffd98b920cc280ac6f1efdb6cdc5e6d4050993
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-03 20:40:21 +0000

    Fixed ReferenceDataService unit tests using ^vortex, 15th page of valve, and transmitter.

--- ReferenceData/ReferenceDataService.Tests/ReferenceDataServiceTest.cs
+++ ReferenceData/ReferenceDataService.Tests/ReferenceDataServiceTest.cs
@@ -27,96 +27,148 @@ namespace ReferenceDataService.Tests
     public void SearchTest()
     {
       ReferenceDataProxy target = new ReferenceDataProxy();
-      RefDataEntities actual = target.Search("in-line piping");
-      Assert.AreEqual(8, actual.Count);
+      RefDataEntities actual = target.Search("^vortex");
+      Assert.AreEqual(10, actual.Count);
     }
 
     [TestMethod()]
     public void SearchResetTest()
     {
       ReferenceDataProxy target = new ReferenceDataProxy();
-      RefDataEntities actual = target.SearchReset("in-line piping");
-      Assert.AreEqual(8, actual.Count);
+
+      RefDataEntities initial = target.Search("^vortex");
+
+      //TODO: We need to fix RefDataService to enable SemWeb TripleStore.
+      //QMXF @class = new QMXF();
+
+      //ClassDefinition classDefinition = new ClassDefinition()
+      //{
+      //  identifier = "TestClassVortex",
+      //  name = new List<QMXFName>() 
+      //  {
+      //    new QMXFName { lang = "en-us", value = "Vortex Test Class" },
+      //  },
+      //};
+
+      //@class.classDefinitions.Add(classDefinition);
+
+      //Response response = target.PostClass(@class);
+
+      //Assert.AreNotEqual(response.Level, StatusLevel.Error);
+
+      RefDataEntities actual = target.SearchReset("^vortex");
+
+      Assert.AreEqual(initial.Count, actual.Count);
     }
 
     [TestMethod()]
     public void SearchPageTest()
     {
       ReferenceDataProxy target = new ReferenceDataProxy();
-      RefDataEntities actual = target.SearchPage("in-line piping", "1");
-      Assert.AreEqual(8, actual.Count);
+      RefDataEntities actual = target.SearchPage("valve", "15");
+      Assert.AreEqual(74, actual.Count);
     }
 
     [TestMethod()]
     public void SearchPageResetTest()
     {
       ReferenceDataProxy target = new ReferenceDataProxy();
-      RefDataEntities actual = target.SearchPageReset("in-line piping", "1");
-      Assert.AreEqual(8, actual.Count);
+
+      RefDataEntities initial = target.SearchPage("valve", "15");
+
+      //TODO: We need to fix RefDataService to enable SemWeb TripleStore.
+      //QMXF @class = new QMXF();
+
+      //ClassDefinition classDefinition = new ClassDefinition()
+      //{
+      //  identifier = "TestClassValve",
+      //  name = new List<QMXFName>() 
+      //  {
+      //    new QMXFName { lang = "en-us", value = "Test Class Valve" },
+      //  },
+      //};
+
+      //@class.classDefinitions.Add(classDefinition);
+
+      //Response response = target.PostClass(@class);
+
+      //Assert.AreNotEqual(response.Level, StatusLevel.Error);
+
+      RefDataEntities actual = target.SearchPageReset("valve", "15");
+
+      Assert.AreEqual(initial.Count, actual.Count);
     }
 
     [TestMethod()]
     public void FindTest()
     {
       ReferenceDataProxy target = new ReferenceDataProxy();
-      List<Entity> actual = target.Find("in-line piping component");
+      List<Entity> actual = target.Find("transmitter");
       Assert.AreEqual(1, actual.Count);
     }
 
     [TestMethod()]
     public void GetClassTest()
     {
+      //Transmitter
       ReferenceDataProxy target = new ReferenceDataProxy();
-      QMXF actual = target.GetClass("R71668234814");
+      QMXF actual = target.GetClass("R19535665699");
       Assert.AreEqual(1, actual.classDefinitions.Count);
     }
 
     [TestMethod()]
     public void GetClassLabelTest()
     {
+      //Transmitter
       ReferenceDataProxy target = new ReferenceDataProxy();
-      string actual = target.GetClassLabel("R71668234814");
+      string actual = target.GetClassLabel("R19535665699");
       Assert.AreNotEqual(string.Empty, actual);
     }
 
     [TestMethod()]
     public void GetSuperClassesTest()
     {
+      //Transmitter - 1
       ReferenceDataProxy target = new ReferenceDataProxy();
-      List<Entity> actual = target.GetSuperClasses("R45162754880");
+      List<Entity> actual = target.GetSuperClasses("R19535665699");
       Assert.AreEqual(1, actual.Count);
     }
 
     [TestMethod()]
     public void GetSubClassesTest()
     {
+      //Transmitter - 19
       ReferenceDataProxy target = new ReferenceDataProxy();
-      List<Entity> actual = target.GetSubClasses("R45162754880");
-      Assert.AreEqual(0, actual.Count);
+      List<Entity> actual = target.GetSubClasses("R19535665699");
+      Assert.AreEqual(19, actual.Count);
     }
 
     [TestMethod()]
     public void GetClassTemplatesTest()
     {
+      //Transmitter - 6
       ReferenceDataProxy target = new ReferenceDataProxy();
-      List<Entity> actual = target.GetClassTemplates("R45162754880");
-      Assert.AreEqual(9, actual.Count);
+      List<Entity> actual = target.GetClassTemplates("R19535665699");
+      Assert.AreEqual(6, actual.Count);
     }
 
     [TestMethod()]
     public void GetTemplateTest()
     {
+      //IdentificationByTag
       ReferenceDataProxy target = new ReferenceDataProxy();
-      QMXF actual = target.GetTemplate("R41551704022");
+      QMXF actual = target.GetTemplate("R34191521901");
       Assert.AreEqual(1, actual.templateQualifications.Count);
     }
 
+    //TODO: We need to fix RefDataService to enable SemWeb TripleStore.
     //[TestMethod()]
     //public void PostTemplateTest()
     //{
     //  Assert.Inconclusive();
     //}
 
+    //TODO: We need to fix RefDataService to enable SemWeb TripleStore.
     //[TestMethod()]
     //public void PostClassTest()
     //{
