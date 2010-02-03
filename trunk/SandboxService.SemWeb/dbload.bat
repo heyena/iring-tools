0fa9e5a639296bd37c1aa16cbb01b3cb8881a5ba
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-03 14:57:38 +0000

    make dbload.bat clear the store before loading.

--- trunk/SandboxService.SemWeb/dbload.bat
+++ trunk/SandboxService.SemWeb/dbload.bat
@@ -1,3 +1,3 @@
 @echo off
-.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=.\SQLEXPRESS; Initial Catalog=sandbox; User Id=sandbox; Password=sandbox;" %1
+.\Bin\rdfstorage.exe -clear -out "sqlserver:rdf:Database=rdf;data source=.\SQLEXPRESS; Initial Catalog=sandbox; User Id=sandbox; Password=sandbox;" %1
 
