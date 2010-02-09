7e6e40138ae8f7852730a467668197bd6984a02b
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-09 21:47:25 +0000

    Rectify layout to enable editor edit screens

--- Modules/RDSWIPModelling/MainRegion/RefDataEditor/RefDataBrowserView.xaml.cs
+++ Modules/RDSWIPModelling/MainRegion/RefDataEditor/RefDataBrowserView.xaml.cs
@@ -12,10 +12,10 @@ namespace org.iringtools.modelling.mainregion.refdatabrowser
       LayoutRoot.LayoutUpdated += new System.EventHandler(LayoutRoot_Updated);
       LayoutRoot.SizeChanged += new System.Windows.SizeChangedEventHandler(LayoutRoot_SizeChanged);
       Search.Loaded += new System.Windows.RoutedEventHandler(Search_SizeChanged);
+      Details.Loaded += new System.Windows.RoutedEventHandler(Details_SizeChanged);
       Edits.Loaded += new System.Windows.RoutedEventHandler(Edits_SizeChanged);
       ClassEditor.Loaded += new System.Windows.RoutedEventHandler(ClassEditor_SizeChanged);
       TemplateEditor.Loaded += new System.Windows.RoutedEventHandler(TemplateEditor_SizeChanged);
-      Details.Loaded += new System.Windows.RoutedEventHandler(Details_SizeChanged);
     }
 
     #region Resize Event Handlers
@@ -27,19 +27,11 @@ namespace org.iringtools.modelling.mainregion.refdatabrowser
     void LayoutRoot_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
     {
       Search_SizeChanged(sender, e);
+      //Navigation_SizeChanged(sender, e);
+      Details_SizeChanged(sender, e);
       Edits_SizeChanged(sender, e);
       ClassEditor_SizeChanged(sender, e);
       TemplateEditor_SizeChanged(sender, e);
-      Details_SizeChanged(sender, e);
-    }
-
-    void Search_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
-    {
-      if (Search.Items.Count > 0)
-      {
-        UserControl userControl = (UserControl)Search.Items[0];
-        userControl.Height = LayoutRoot.RowDefinitions[0].ActualHeight + LayoutRoot.RowDefinitions[1].ActualHeight;
-      }
     }
 
     void Edits_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
@@ -47,25 +39,19 @@ namespace org.iringtools.modelling.mainregion.refdatabrowser
       if (Edits.Items.Count > 0)
       {
         UserControl userControl = (UserControl)Edits.Items[0];
-        userControl.Height = LayoutEditor.RowDefinitions[0].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight;
       }
     }
 
-    void ClassEditor_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
+    void Search_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
     {
-      if (ClassEditor.Items.Count > 0)
+      if (Search.Items.Count > 0)
       {
-        UserControl userControl = (UserControl)ClassEditor.Items[0];
-        userControl.Height = LayoutEditor.RowDefinitions[0].ActualHeight;
-      }
-    }
-
-    void TemplateEditor_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
-    {
-      if (TemplateEditor.Items.Count > 0)
-      {
-        UserControl userControl = (UserControl)TemplateEditor.Items[0];
-        userControl.Height = LayoutEditor.RowDefinitions[0].ActualHeight;
+        UserControl userControl = (UserControl)Search.Items[0];
+        userControl.Height = LayoutRoot.RowDefinitions[0].ActualHeight +
+            LayoutRoot.RowDefinitions[1].ActualHeight +
+            LayoutRoot.RowDefinitions[2].ActualHeight; //rowSpanHeight;
+        //userControl.Width = LayoutRoot.ColumnDefinitions[0].ActualWidth;
       }
     }
 
@@ -74,7 +60,31 @@ namespace org.iringtools.modelling.mainregion.refdatabrowser
       if (Details.Items.Count > 0)
       {
         UserControl userControl = (UserControl)Details.Items[0];
-        userControl.Height = LayoutEditor.RowDefinitions[1].ActualHeight;
+        //double rowSpanHeight = LayoutRoot.RowDefinitions[0].ActualHeight + LayoutRoot.RowDefinitions[1].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[2].ActualHeight; //rowSpanHeight;
+        //userControl.Width = LayoutRoot.ColumnDefinitions[1].ActualWidth;
+      }
+    }
+
+    void ClassEditor_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
+    {
+      if (ClassEditor.Items.Count > 0)
+      {
+        UserControl userControl = (UserControl)ClassEditor.Items[0];
+        //double rowSpanHeight = LayoutRoot.RowDefinitions[0].ActualHeight + LayoutRoot.RowDefinitions[1].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight; //rowSpanHeight;
+        //userControl.Width = LayoutRoot.ColumnDefinitions[1].ActualWidth;
+      }
+    }
+
+    void TemplateEditor_SizeChanged(object sender, System.Windows.RoutedEventArgs e)
+    {
+      if (TemplateEditor.Items.Count > 0)
+      {
+        UserControl userControl = (UserControl)TemplateEditor.Items[0];
+        //double rowSpanHeight = LayoutRoot.RowDefinitions[0].ActualHeight + LayoutRoot.RowDefinitions[1].ActualHeight;
+        userControl.Height = LayoutRoot.RowDefinitions[1].ActualHeight; //rowSpanHeight;
+        //userControl.Width = LayoutRoot.ColumnDefinitions[1].ActualWidth;
       }
     }
     #endregion
