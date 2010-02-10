1930098f2e3d232fa3f11b02fe12898ced6493b5
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-10 23:16:12 +0000

    Update setup config & script and migration guide to accommodate the interface and sandbox service installation

--- InterfaceService/setup.cmd
+++ InterfaceService/setup.cmd
@@ -4,7 +4,7 @@ setlocal
 
 rem Get properties from setup.conf
 for /f "tokens=1,2,3 delims== " %%i in (setup.conf) do (
-  if "%%i" equ "sql_server" set dbServer=%%j
+  if "%%i" equ "sql_instance" set dbInstance=%%j
   if "%%i" equ "sql_username" set dbUser=%%j
   if "%%i" equ "sql_password" set dbPassword=%%j
 )
@@ -12,13 +12,13 @@ for /f "tokens=1,2,3 delims== " %%i in (setup.conf) do (
 rem Set up database
 @echo Setting up interface service database ...
 if %dbUser% equ "" if %dbPassword% equ "" (
-  SQLCMD -S %dbServer% -i  .\Scripts\iRING.sql  
+  SQLCMD -S %dbInstance% -i  .\Scripts\iRING.sql  
   if %errorlevel% equ 0 (
     goto InitRDF
   )  
 )
 
-SQLCMD -U %dbUser% -P %dbPassword% -S %dbServer% -i  .\Scripts\iRING.sql
+SQLCMD -U %dbUser% -P %dbPassword% -S %dbInstance% -i  .\Scripts\iRING.sql
 if %errorlevel% equ 0 (
   goto InitRDF
 ) 
@@ -29,12 +29,12 @@ goto End
 :InitRDF
 rem Initialize RDF storage
 @echo Initializing RDF storage ...
-.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;"
+.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=%dbInstance%; Initial Catalog=iring; User Id=iring; Password=iring;"
 
 rem Update web.config file
 @echo Updating web configuration file ...
 set old_key=".*key=\"/InterfaceService/sparql\".*"
-set new_key="    <add key=\"/InterfaceService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbServer%; Initial Catalog=iring; User Id=iring; Password=iring;\"/>"
+set new_key="    <add key=\"/InterfaceService/sparql\" value=\"noreuse,rdfs+sqlserver:rdf:Database=rdf;data source=%dbInstance%; Initial Catalog=iring; User Id=iring; Password=iring;\"/>"
 .\Bin\replace web.config %old_key% %new_key% > web.config.tmp
 if %errorlevel% equ 0 (
   move /y web.config.tmp web.config >nul
