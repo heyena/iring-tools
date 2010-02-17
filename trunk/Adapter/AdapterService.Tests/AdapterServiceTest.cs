ed1935a62dac21c1042ee66b8f602eeebf51adaa
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-02-17 01:08:43 +0000

    Fixed Unit Tests

--- trunk/Adapter/AdapterService.Tests/AdapterServiceTest.cs
+++ trunk/Adapter/AdapterService.Tests/AdapterServiceTest.cs
@@ -27,6 +27,8 @@ namespace AdapterService.Tests
 
       string bindingConfigPath = @"C:\iring-tools\Adapter\AdapterService.Tests\XML\BindingConfiguration.12345_000.ABC.xml";
       string bindingConfigTestPath = @"C:\iring-tools\Adapter\AdapterService.Tests\XML\BindingConfiguration.12345_000.ABC.Test.xml";
+      string nhMappingPath = @"C:\iring-tools\Adapter\AdapterService.Tests\XML\nh-mapping.12345_000.ABC.xml";
+      string nhMappingTestPath = @"C:\iring-tools\Adapter\AdapterService.Tests\XML\nh-mapping.12345_000.ABC.Test.xml";
 
       string dbDictionaryPath = @"C:\iring-tools\Adapter\AdapterService.Tests\XML\DatabaseDictionary.12345_000.ABC.xml";
 
@@ -36,6 +38,7 @@ namespace AdapterService.Tests
       Assert.AreEqual("Entities generated successfully.", actual[0]);
 
       File.Copy(bindingConfigTestPath, bindingConfigPath, true);
+      File.Copy(nhMappingTestPath, nhMappingPath, true);
     }
 
     [TestMethod()]
