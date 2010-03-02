f2b22b3ee2a124af19f5d3c38a785af507d2df04
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-02 00:10:22 +0000

    update the interface endpoint creation

--- Demonstration/DemoControlPanel/MainPage.xaml.cs
+++ Demonstration/DemoControlPanel/MainPage.xaml.cs
@@ -695,8 +695,9 @@ namespace DemoControlPanel
             break;
           }
         }
-
-        interfaceEndpoint.serviceUri += String.Format("/{0}/{1}/sparql", scenario.senderProjectName, scenario.senderApplicationName);
+                  
+        interfaceEndpoint.ProjectName = scenario.senderProjectName;
+        interfaceEndpoint.ApplicationName = scenario.senderApplicationName;
         string graphName = (string)cbxReceiverGraphName.SelectedItem;
         _client.PullAsync(adapterEndpoint, interfaceEndpoint, scenario.receiverProjectName, scenario.receiverApplicationName, graphName);
       }
