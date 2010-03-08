1d21df267c90adaf5b650b453cebdd3a7dbb1b1f
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-08 20:26:43 +0000

    Add csv service project

---
+++ CSVDataLayer/CSVService/CSVService/CSVService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Configuration;
+using org.iringtools.utility;
+
+namespace CSVService
+{
+  public class CSVService : ICSVService
+  {
+    public string Save(string fileName, string content)
+    {
+      try
+      {
+        string path = ConfigurationManager.AppSettings["targetLocation"];
+        Utility.WriteString(content, path + fileName);
+
+        return "File \"" + path + fileName + "\" has been saved successfully.";
+      }
+      catch (Exception ex)
+      {
+        return "ERROR: " + ex;
+      }
+    }
+  }
+}
