3abd2a48941cb5d6315da650d95e04c95ca75ffb
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-08 19:35:37 +0000

    Switch to command line svn (http://subversion.tigris.org/)

--- trunk/build.x64.cmd
+++ trunk/build.x64.cmd
@@ -1,6 +1,7 @@
 @echo off
 call "%VS90COMNTOOLS%\vsvars32.bat"
 cd %~dp0%
-"C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /notempfile /command:update /path:build.xml /closeonend:1
+rem "C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /notempfile /command:update /path:build.xml /closeonend:1
+svn update
 msbuild build.x64.xml /t:all /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
 pause
