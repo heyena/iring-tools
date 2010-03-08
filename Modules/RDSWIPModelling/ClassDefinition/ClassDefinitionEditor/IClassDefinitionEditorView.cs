195aefbc97986387789c13b1035ae5d048dfd021
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-08 18:58:01 +0000

    cleanup

--- Modules/RDSWIPModelling/ClassDefinition/ClassDefinitionEditor/IClassDefinitionEditorView.cs
+++
@@ -1,6 +0,0 @@
-﻿using PrismContrib.Base;
-
-namespace org.iringtools.modelling.classdefinition.classdefinitioneditor
-{
-    public interface IClassDefinitionEditorView : IViewBase { }
-}
--- SandboxService/dbload.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-.\Bin\rdfstorage.exe -out "sqlserver:rdf:Database=rdf;data source=.\SQLEXPRESS; Initial Catalog=sandbox; User Id=sandbox; Password=sandbox;" %1
-
