66709d349f77ff1d80fa10b6616cd5a077161bca
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-08 20:26:43 +0000

    Add csv service project

---
+++ branches/demos/CSVDataLayer/CSVService/CSVService/ICSVService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace CSVService
+{
+  [ServiceContract]
+  public interface ICSVService
+  {
+    [OperationContract]
+    [WebGet(UriTemplate = "/save?fileName={fileName}&content={content}")]
+    string Save(string fileName, string content);
+  }
+}
