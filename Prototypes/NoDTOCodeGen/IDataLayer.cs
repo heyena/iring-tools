2297f16c43a3806466aecec7b6305d6e5e274c93
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-31 12:22:22 +0000

    Added "No DTO code gen" prototype

---
+++ Prototypes/NoDTOCodeGen/IDataLayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPrototype
+{
+  public interface IDataLayer
+  {
+    object Create(string dataObjectName);
+    T GetPropertyValue<T>(object dataObject, string propertyName);
+    void SetPropertyValue<T>(object dataObject, string propertyName, T value);
+    object Get(string dataObjectName, string filter);
+    IList<object> GetList(string dataObjectName, string filter);
+    void Post(object dataObject);
+    void PostList(List<object> dataObjects);
+  }
+}
