1793e3ea6a9deb14191c7f703354d57450258a71
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-03-31 12:22:22 +0000

    Added "No DTO code gen" prototype

---
+++ trunk/Prototypes/NoDTOCodeGen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPrototype
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      NHibernateDataLayer dataLayer = new NHibernateDataLayer();
+
+      // get
+      IList<object> valves = dataLayer.GetList("AdapterPrototype.Valve", null);
+      foreach (object valve in valves)
+      {
+        String valveTag = dataLayer.GetPropertyValue<String>(valve, "tag");
+        Single valveDiameter = dataLayer.GetPropertyValue<Single>(valve, "diameter");
+        String valveSystem = dataLayer.GetPropertyValue<String>(valve, "system");
+        Console.WriteLine(valveTag + ", " + valveDiameter + ", " + valveSystem);
+      }
+
+      // update
+      object line = dataLayer.Get("AdapterPrototype.Line", null);
+      Single diameter = dataLayer.GetPropertyValue<Single>(line, "diameter");
+      dataLayer.SetPropertyValue<Single>(line, "diameter", 5);
+      dataLayer.Post(line);
+
+      // add
+      object newLine = dataLayer.Create("AdapterPrototype.Line");
+      dataLayer.SetPropertyValue<String>(newLine, "tag", "L0001");
+      dataLayer.SetPropertyValue<Single>(newLine, "diameter", 1.5f);
+      dataLayer.SetPropertyValue<String>(newLine, "system", "AB");
+      dataLayer.Post(newLine);
+
+      Console.WriteLine("Press any key to continue ...");
+      Console.ReadKey();
+    }
+  }
+}
