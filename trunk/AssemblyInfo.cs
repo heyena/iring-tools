8964bfbf438e794351fd4f727d950198e5cef786
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-04-13 02:11:14 +0000

    Fixed Code Generation UnitTest and Build 701

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.1.679")]
-[assembly: AssemblyFileVersion("1.2.1.679")]
+[assembly: AssemblyVersion("1.2.1.701")]
+[assembly: AssemblyFileVersion("1.2.1.701")]
