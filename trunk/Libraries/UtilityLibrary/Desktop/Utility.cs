c6c34c65b12b993b2f7564b23e4f2f5a7e65b48f
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-04-22 20:57:57 +0000

    Added XElement conversion

--- trunk/Libraries/UtilityLibrary/Desktop/Utility.cs
+++ trunk/Libraries/UtilityLibrary/Desktop/Utility.cs
@@ -969,5 +969,23 @@ namespace org.iringtools.utility
 
       return id;
     }
+
+    public static XElement GetXElement(this XmlNode node)
+    {
+      XDocument xDoc = new XDocument();
+      XmlWriter xmlWriter = xDoc.CreateWriter();
+      node.WriteTo(xmlWriter);
+      return xDoc.Root;
+    }
+
+    public static XmlNode GetXmlNode(this XElement element)
+    {
+      using (XmlReader xmlReader = element.CreateReader())
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(xmlReader);
+        return xmlDoc;
+      }
+    }
   }
 }
