e9fe3f6f829cd2b4cc1667cf64603de71b0cee3f
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-05-05 18:24:26 +0000

    oops!

--- tags/1.2.1/Adapter/AdapterService.Tests/Database/VALVE_DATA.sql
+++
@@ -1,1 +0,0 @@
--- INSERTING into VALVE
--- tags/1.2.1/Modules/Mapping/Silverlight/_MappingEditor/ProjectApplicationRegion/IProjectApplicationView.cs
+++
@@ -1,9 +0,0 @@
-﻿using PrismContrib.Base;
-
-namespace org.iringtools.modules.projectapplicationregion
-{
-    public interface IProjectApplicationView : IViewBase
-    {
-
-    }
-}
