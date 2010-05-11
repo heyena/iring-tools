8645dd5a4e392463b6e53350c3d570c5c9948588
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-05-11 14:05:43 +0000

    AdapterSettings updates for dotNETRdf

--- trunk/Libraries/iRINGLibrary/Desktop/AdapterSettings.cs
+++ trunk/Libraries/iRINGLibrary/Desktop/AdapterSettings.cs
@@ -11,12 +11,20 @@ namespace org.iringtools.adapter
 {
   public class AdapterSettings : ServiceSettings
   {
+
     private string _encryptedToken = String.Empty;
     private string _tripleStoreConnectionString = String.Empty;
     private string _interfaceServerUri = String.Empty;
     private string _interfaceServerPath = String.Empty;
     private string _binaryPath = string.Empty;
     private string _codePath = string.Empty;
+    private string _dotnetRDFDbName = string.Empty;
+    private string _dotnetRDFDbServer = string.Empty;
+    private string _dotnetRDFDbUser = string.Empty;
+    private string _dotnetRDFDbPassword = string.Empty;
+    private string _interfaceCredential = string.Empty;
+    private string _graphBaseUri = string.Empty;
+    private int _endpointTimeout = 0;
 
     public AdapterSettings(NameValueCollection AppSettings)
     {
@@ -26,6 +34,7 @@ namespace org.iringtools.adapter
       this.ProxyHost = AppSettings["ProxyHost"];
       this.ProxyPort = AppSettings["ProxyPort"];
       this.UseSemweb = Convert.ToBoolean(AppSettings["UseSemweb"]);
+      this.UsedotnetRDF = Convert.ToBoolean(AppSettings["UsedotnetRDF"]);
       this.TripleStoreConnectionString = AppSettings["TripleStoreConnectionString"];
       this.InterfaceServer = AppSettings["InterfaceService"];
       this.InterfaceServerPath = AppSettings["InterfaceServicePath"];
@@ -33,8 +42,34 @@ namespace org.iringtools.adapter
       this.TrimData = Convert.ToBoolean(AppSettings["TrimData"]);
       this.BinaryPath = AppSettings["BinaryPath"];
       this.CodePath = AppSettings["CodePath"];
+      this.DBServer = AppSettings["DBServer"];
+      this.DBname = AppSettings["DBName"];
+      this.DBUser = AppSettings["DBUser"];
+      this.DBPassword = AppSettings["DBPassword"];
+      this.EndpointTimeout = Convert.ToInt32(AppSettings["EndpointTimeout"]);
+      this.InterfaceCredentialToken = AppSettings["InterfaceCredentialToken"];
+      this.GraphBaseUri = AppSettings["GraphBaseUri"];
       this.PrepareCredentials();
     }
+    
+  
+    public string InterfaceCredentialToken 
+    {
+        get
+        { return _interfaceCredential; }
+        set
+        { _interfaceCredential = value; }
+    }
+
+    public string GraphBaseUri
+    {
+        get
+        { return _graphBaseUri; }
+        set
+        { this._graphBaseUri = value; }
+    }
+
+    public WebCredentials InterfaceCredentials { get; set; }
 
     public WebCredentials TargetCredentials { get; set; }
 
@@ -53,6 +88,55 @@ namespace org.iringtools.adapter
         this.TargetCredentials = new WebCredentials(EncryptedToken);
         this.TargetCredentials.Decrypt();
       }
+      if (InterfaceCredentialToken == String.Empty)
+      {
+          this.InterfaceCredentials = new WebCredentials();
+      }
+      else
+      {
+          this.InterfaceCredentials = new WebCredentials(InterfaceCredentialToken);
+          this.InterfaceCredentials.Decrypt();
+      }
+    }
+
+    public int EndpointTimeout
+    {
+        get
+        { return _endpointTimeout; }
+        set
+        { this._endpointTimeout = value; }
+    }
+
+    public string DBname
+    {
+        get
+        { return _dotnetRDFDbName; }
+        set
+        { this._dotnetRDFDbName = value; }
+    }
+
+    public string DBServer
+    {
+        get
+        { return _dotnetRDFDbServer; }
+        set
+        { this._dotnetRDFDbServer = value; }
+    }
+
+    public string DBUser
+    {
+        get
+        { return _dotnetRDFDbUser; }
+        set
+        { this._dotnetRDFDbUser = value; }
+    }
+
+    public string DBPassword
+    {
+        get
+        { return _dotnetRDFDbPassword; }
+        set
+        { this._dotnetRDFDbPassword = value; }
     }
 
     public string TripleStoreConnectionString 
@@ -97,7 +181,10 @@ namespace org.iringtools.adapter
     public bool TrimData { get; set; }
     
     //Booleans are handled by Convert
-    public bool UseSemweb { get; set; } 
+    public bool UseSemweb { get; set; }
+
+    //Booleans are handled by Convert
+    public bool UsedotnetRDF { get; set; } 
 
     public string EncryptedToken 
     {
