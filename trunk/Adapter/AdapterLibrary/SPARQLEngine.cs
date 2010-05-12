b82c73ad92a49a80da3e8764e35d3fbefd87a88c
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-05-12 23:06:45 +0000

    moved code into semantic layer

--- trunk/Adapter/AdapterLibrary/SPARQLEngine.cs
+++ trunk/Adapter/AdapterLibrary/SPARQLEngine.cs
@@ -197,6 +197,18 @@ namespace org.iringtools.adapter.semantic
 
           try
           {
+              List<string> tripleStoreIdentifiers = this.GetIdentifiers(graphName);
+              List<string> identifiersToBeDeleted = tripleStoreIdentifiers;
+              foreach (DataTransferObject commonDTO in dtoList)
+              {
+                  if (tripleStoreIdentifiers.Contains(commonDTO.Identifier))
+                  {
+                      identifiersToBeDeleted.Remove(commonDTO.Identifier);
+                  }
+              }
+              if (identifiersToBeDeleted.Count > 0)
+                  response.Append(this.Delete(graphName, identifiersToBeDeleted));
+
             var graphMaps = from map in _mapping.graphMaps
                             where map.name == graphName
                             select map;
