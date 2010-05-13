a56b5704b20ea8bf105884b28185a147c7c7833c
mohd.moubarak <mohd.moubarak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-05-13 11:46:57 +0000

    Changes to Reference Data Service to incorporate Ninject for loading application settings

---
+++ trunk/ReferenceData/ReferenceDataService/ReferenceDataModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+using org.iringtools.library;
+
+namespace org.ids_adi.iring.referenceData
+{
+    class ReferenceDataModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ReferenceDataSettings>().ToSelf().InSingletonScope();
+        }
+    }
+}
