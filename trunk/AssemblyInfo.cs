bf0652dee006540ccf78daf288406fba467b5bca
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-05-25 02:08:48 +0000

    Build 1.3.0.923

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.3.0.885")]
-[assembly: AssemblyFileVersion("1.3.0.885")]
+[assembly: AssemblyVersion("1.3.0.923")]
+[assembly: AssemblyFileVersion("1.3.0.923")]
