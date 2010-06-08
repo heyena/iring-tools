fe331621c089e4f80242e69c74e12d14905fed7f
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-06-08 21:03:27 +0000

    Added missing files from previous commit

---
+++ trunk/Adapter/AdapterLibrary/IProjectionLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using org.iringtools.library;
+
+namespace org.iringtools.adapter
+{
+  public interface IProjectionLayer
+  {
+    XElement GetRdf(string graphName);
+    XElement GetQtxf(string graphName);
+    XElement GetHierachicalDTOList(string graphName);
+    List<Dictionary<string, string>> GetDTOList(string graphName);
+  }
+}
