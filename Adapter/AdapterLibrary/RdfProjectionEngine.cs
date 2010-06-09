3b02e67c947b7223668fcf8c4385dcee0bc0d2e1
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-06-09 20:52:33 +0000

    Created RdfProjectionEngine

---
+++ Adapter/AdapterLibrary/RdfProjectionEngine.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.library;
+using System.Xml.Linq;
+using Ninject;
+using log4net;
+using System.Text.RegularExpressions;
+using VDS.RDF;
+using VDS.RDF.Storage;
+using org.iringtools.utility;
+
+namespace org.iringtools.adapter.projection
+{
+  public class RdfProjectionEngine : IProjectionLayer
+  {
+    private static readonly string DATALAYER_NS = "org.iringtools.adapter.datalayer";
+
+    private static readonly XNamespace RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    private static readonly XNamespace OWL_NS = "http://www.w3.org/2002/07/owl#";
+    private static readonly XNamespace XSD_NS = "http://www.w3.org/2001/XMLSchema#";
+    private static readonly XNamespace XSI_NS = "http://www.w3.org/2001/XMLSchema-instance#";
+    private static readonly XNamespace TPL_NS = "http://tpl.rdlfacade.org/data#";
+    private static readonly XNamespace RDL_NS = "http://rdl.rdlfacade.org/data#";
+
+    private static readonly XName OWL_THING = OWL_NS + "Thing";
+    private static readonly XName RDF_ABOUT = RDF_NS + "about";
+    private static readonly XName RDF_DESCRIPTION = RDF_NS + "Description";
+    private static readonly XName RDF_TYPE = RDF_NS + "type";
+    private static readonly XName RDF_RESOURCE = RDF_NS + "resource";
+    private static readonly XName RDF_DATATYPE = RDF_NS + "datatype";
+
+    private static readonly string XSD_PREFIX = "xsd:";
+    private static readonly string RDF_PREFIX = "rdf:";
+    private static readonly string RDL_PREFIX = "rdl:";
+    private static readonly string TPL_PREFIX = "tpl:";
+
+    private static readonly string RDF_TYPE_ID = "tpl:R63638239485";
+    private static readonly string CLASSIFICATION_INSTANCE_ID = "tpl:R55055340393";
+    private static readonly string CLASS_INSTANCE_ID = "tpl:R99011248051";
+    private static readonly string RDF_NIL = RDF_PREFIX + "nil";
+
+    private static readonly ILog _logger = LogManager.GetLogger(typeof(RdfProjectionEngine));
+
+    private Mapping _mapping = null;
+    private GraphMap _graphMap = null;
+    private DataDictionary _dataDictionary = null;
+    private Dictionary<string, IList<IDataObject>> _dataObjectSet = null; // dictionary of object names and list of data objects
+    private Dictionary<string, List<string>> _classIdentifiers = null; // dictionary of class ids and list of identifiers
+    private List<Dictionary<string, string>> _xPathValuePairs = null;  // dictionary of property xpath and value pairs
+    private Dictionary<string, List<string>> _hierachicalDTOClasses = null;  // dictionary of class rdlUri and identifiers
+    private XNamespace _graphNs = String.Empty;
+    private string _dataObjectsAssemblyName = String.Empty;
+    private string _dataObjectNs = String.Empty;
+
+    [Inject]
+    public RdfProjectionEngine(AdapterSettings adapterSettings, ApplicationSettings appSettings, IDataLayer dataLayer)
+    {
+      string scope = appSettings.ProjectName + "{0}" + appSettings.ApplicationName;
+
+      _dataObjectSet = new Dictionary<string, IList<IDataObject>>();
+      _classIdentifiers = new Dictionary<string, List<string>>();
+      _xPathValuePairs = new List<Dictionary<string, string>>();
+      _hierachicalDTOClasses = new Dictionary<string, List<string>>();
+
+      _graphNs = String.Format(adapterSettings.GraphBaseUri + scope + "#", "/");
+      _dataObjectNs = String.Format(DATALAYER_NS + ".proj_" + scope, ".");
+      _dataObjectsAssemblyName = adapterSettings.ExecutingAssemblyName;
+    }
+
+    public XElement GetXml(ref Mapping mapping, string graphName, 
+      ref DataDictionary dataDictionary, ref Dictionary<string, IList<IDataObject>> dataObjects)
+    {
+      try
+      {
+        _mapping = mapping;
+        FindGraphMap(graphName);
+
+        _dataDictionary = dataDictionary;
+        _dataObjectSet = dataObjects;
+
+        return GetRdf();
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    #region helper methods
+    private string ExtractId(string qualifiedId)
+    {
+      if (String.IsNullOrEmpty(qualifiedId) || !qualifiedId.Contains(":"))
+        return qualifiedId;
+
+      return qualifiedId.Substring(qualifiedId.IndexOf(":") + 1);
+    }
+
+    private string TitleCase(string value)
+    {
+      string returnValue = String.Empty;
+      string[] words = value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+      foreach (string word in words)
+      {
+        returnValue += word.Substring(0, 1).ToUpper();
+
+        if (word.Length > 1)
+          returnValue += word.Substring(1).ToLower();
+      }
+
+      return returnValue;
+    }
+
+    private string ResolveValueList(string valueList, string value)
+    {
+      if (_mapping != null)// && _mapping.valueMaps.Count > 0)
+      {
+        foreach (ValueList valueLst in _mapping.valueLists)
+        {
+          if (valueLst.name == valueList)
+          {
+            foreach (ValueMap valueMap in valueLst.valueMaps)
+            {
+              if (valueMap.internalValue == value)
+              {
+                return valueMap.uri.Replace("rdl:", RDL_NS.NamespaceName);
+              }
+            }
+          }
+        }
+      }
+
+      return RDF_NIL;
+    }
+
+    private void FindGraphMap(string graphName)
+    {
+      foreach (GraphMap graphMap in _mapping.graphMaps)
+      {
+        if (graphMap.name.ToLower() == graphName.ToLower())
+        {
+          _graphMap = graphMap;
+
+          if (_graphMap.classTemplateListMaps.Count == 0)
+            throw new Exception("Graph [" + graphName + "] is empty.");
+
+          return;
+        }
+      }
+
+      throw new Exception("Graph [" + graphName + "] does not exist.");
+    }
+
+    // get max # of data records from all data objects
+    private int MaxDataObjectsCount()
+    {
+      int maxCount = 0;
+
+      foreach (var pair in _dataObjectSet)
+      {
+        if (pair.Value.Count > maxCount)
+        {
+          maxCount = pair.Value.Count;
+        }
+      }
+
+      return maxCount;
+    }
+
+    private void PopulateClassIdentifiers()
+    {
+      _classIdentifiers.Clear();
+
+      foreach (ClassMap classMap in _graphMap.classTemplateListMaps.Keys)
+      {
+        List<string> classIdentifiers = new List<string>();
+
+        foreach (string identifier in classMap.identifiers)
+        {
+          string[] property = identifier.Split('.');
+          string objectName = property[0].Trim();
+          string propertyName = property[1].Trim();
+
+          IList<IDataObject> dataObjects = _dataObjectSet[objectName];
+          if (dataObjects != null)
+          {
+            for (int i = 0; i < dataObjects.Count; i++)
+            {
+              string value = Convert.ToString(dataObjects[i].GetPropertyValue(propertyName));
+
+              if (classIdentifiers.Count == i)
+              {
+                classIdentifiers.Add(value);
+              }
+              else
+              {
+                classIdentifiers[i] += classMap.identifierDelimeter + value;
+              }
+            }
+          }
+        }
+
+        _classIdentifiers[classMap.classId] = classIdentifiers;
+      }
+    }
+
+    private XElement GetRdf()
+    {
+      XElement graphElement = new XElement(RDF_NS + "RDF",
+        new XAttribute(XNamespace.Xmlns + "rdf", RDF_NS),
+        new XAttribute(XNamespace.Xmlns + "owl", OWL_NS),
+        new XAttribute(XNamespace.Xmlns + "xsd", XSD_NS),
+        new XAttribute(XNamespace.Xmlns + "tpl", TPL_NS));
+
+      foreach (var pair in _graphMap.classTemplateListMaps)
+      {
+        ClassMap classMap = pair.Key;
+        int maxDataObjectsCount = MaxDataObjectsCount();
+
+        for (int i = 0; i < maxDataObjectsCount; i++)
+        {
+          string classId = classMap.classId.Substring(classMap.classId.IndexOf(":") + 1);
+          string classInstance = _graphNs.NamespaceName + _classIdentifiers[classMap.classId][i];
+
+          graphElement.Add(CreateRdfClassElement(classId, classInstance));
+
+          foreach (TemplateMap templateMap in pair.Value)
+          {
+            graphElement.Add(CreateRdfTemplateElement(templateMap, classInstance, i));
+          }
+        }
+      }
+
+      return graphElement;
+    }
+
+    private XElement CreateRdfClassElement(string classId, string classInstance)
+    {
+      return new XElement(OWL_THING, new XAttribute(RDF_ABOUT, classInstance),
+        new XElement(RDF_TYPE, new XAttribute(RDF_RESOURCE, RDL_NS.NamespaceName + classId)));
+    }
+
+    private XElement CreateRdfTemplateElement(TemplateMap templateMap, string classInstance, int dataObjectIndex)
+    {
+      string templateId = templateMap.templateId.Replace(TPL_PREFIX, TPL_NS.NamespaceName);
+
+      XElement templateElement = new XElement(OWL_THING);
+      templateElement.Add(new XElement(RDF_TYPE, new XAttribute(RDF_RESOURCE, templateId)));
+
+      foreach (RoleMap roleMap in templateMap.roleMaps)
+      {
+        string roleId = roleMap.roleId.Substring(roleMap.roleId.IndexOf(":") + 1);
+        string dataType = String.Empty;
+        XElement roleElement = new XElement(TPL_NS + roleId);
+
+        switch (roleMap.type)
+        {
+          case RoleType.ClassRole:
+            {
+              roleElement.Add(new XAttribute(RDF_RESOURCE, classInstance));
+              break;
+            }
+          case RoleType.Reference:
+            {
+              if (roleMap.classMap != null)
+              {
+                string identifierValue = String.Empty;
+
+                foreach (string identifier in roleMap.classMap.identifiers)
+                {
+                  string[] property = identifier.Split('.');
+                  string objectName = property[0].Trim();
+                  string propertyName = property[1].Trim();
+
+                  IDataObject dataObject = _dataObjectSet[objectName].ElementAt(dataObjectIndex);
+
+                  if (dataObject != null)
+                  {
+                    string value = Convert.ToString(dataObject.GetPropertyValue(propertyName));
+
+                    if (identifierValue != String.Empty)
+                      identifierValue += roleMap.classMap.identifierDelimeter;
+
+                    identifierValue += value;
+                  }
+                }
+
+                roleElement.Add(new XAttribute(RDF_RESOURCE, _graphNs.NamespaceName + identifierValue));
+              }
+              else
+              {
+                roleElement.Add(new XAttribute(RDF_RESOURCE, roleMap.value.Replace(RDL_PREFIX, RDL_NS.NamespaceName)));
+              }
+              break;
+            }
+          case RoleType.FixedValue:
+            {
+              dataType = roleMap.dataType.Replace(XSD_PREFIX, XSD_NS.NamespaceName);
+              roleElement.Add(new XAttribute(RDF_DATATYPE, dataType));
+              roleElement.Add(new XText(roleMap.value));
+              break;
+            }
+          case RoleType.Property:
+            {
+              string[] property = roleMap.propertyName.Split('.');
+              string objectName = property[0].Trim();
+              string propertyName = property[1].Trim();
+
+              IDataObject dataObject = _dataObjectSet[objectName].ElementAt(dataObjectIndex);
+              string value = Convert.ToString(dataObject.GetPropertyValue(propertyName));
+
+              if (String.IsNullOrEmpty(roleMap.valueList))
+              {
+                if (String.IsNullOrEmpty(value))
+                {
+                  roleElement.Add(new XAttribute(RDF_RESOURCE, RDF_NIL));
+                }
+                else
+                {
+                  dataType = roleMap.dataType.Replace(XSD_PREFIX, XSD_NS.NamespaceName);
+                  roleElement.Add(new XAttribute(RDF_DATATYPE, dataType));
+                  roleElement.Add(new XText(value));
+                }
+              }
+              else // resolve value list to uri
+              {
+                string valueListUri = ResolveValueList(roleMap.valueList, value);
+                roleElement.Add(new XAttribute(RDF_RESOURCE, valueListUri));
+              }
+
+              break;
+            }
+        }
+
+        templateElement.Add(roleElement);
+      }
+
+      return templateElement;
+    }
+    #endregion
+  }
+}
