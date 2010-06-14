e78f8b2008da5635064360f4eb2d14a93a6fc27f
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-06-14 17:46:04 +0000

    Added SparqlResultsProjectionEngine and XmlProjectionEngine

---
+++ Adapter/AdapterLibrary/SparqlResultsProjectionEngine.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.library;
+using System.Xml.Linq;
+using Ninject;
+using log4net;
+using System.Text.RegularExpressions;
+using VDS.RDF;
+using VDS.RDF.Storage;
+using org.iringtools.utility;
+
+namespace org.iringtools.adapter.projection
+{
+  public class SparqlResultsProjectionEngine : IProjectionLayer
+  {
+    private static readonly string DATALAYER_NS = "org.iringtools.adapter.datalayer";
+
+    private static readonly XNamespace RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    private static readonly XNamespace OWL_NS = "http://www.w3.org/2002/07/owl#";
+    private static readonly XNamespace XSD_NS = "http://www.w3.org/2001/XMLSchema#";
+    private static readonly XNamespace XSI_NS = "http://www.w3.org/2001/XMLSchema-instance#";
+    private static readonly XNamespace TPL_NS = "http://tpl.rdlfacade.org/data#";
+    private static readonly XNamespace RDL_NS = "http://rdl.rdlfacade.org/data#";
+
+    private static readonly XName OWL_THING = OWL_NS + "Thing";
+    private static readonly XName RDF_ABOUT = RDF_NS + "about";
+    private static readonly XName RDF_DESCRIPTION = RDF_NS + "Description";
+    private static readonly XName RDF_TYPE = RDF_NS + "type";
+    private static readonly XName RDF_RESOURCE = RDF_NS + "resource";
+    private static readonly XName RDF_DATATYPE = RDF_NS + "datatype";
+
+    private static readonly string XSD_PREFIX = "xsd:";
+    private static readonly string RDF_PREFIX = "rdf:";
+    private static readonly string RDL_PREFIX = "rdl:";
+    private static readonly string TPL_PREFIX = "tpl:";
+
+    private static readonly string RDF_NIL = RDF_PREFIX + "nil";
+
+    private static readonly ILog _logger = LogManager.GetLogger(typeof(SparqlResultsProjectionEngine));
+
+    private Mapping _mapping = null;
+    private GraphMap _graphMap = null;
+    private DataDictionary _dataDictionary = null;
+    private Dictionary<string, IList<IDataObject>> _dataObjectSet = null; // dictionary of object names and list of data objects
+    private Dictionary<string, List<string>> _classIdentifiers = null; // dictionary of class ids and list of identifiers
+    private List<Dictionary<string, string>> _xPathValuePairs = null;  // dictionary of property xpath and value pairs
+    private Dictionary<string, List<string>> _hierachicalDTOClasses = null;  // dictionary of class rdlUri and identifiers
+    private XNamespace _graphNs = String.Empty;
+    private string _dataObjectsAssemblyName = String.Empty;
+    private string _dataObjectNs = String.Empty;
+
+    [Inject]
+    public SparqlResultsProjectionEngine(AdapterSettings adapterSettings, ApplicationSettings appSettings, IDataLayer dataLayer)
+    {
+      string scope = appSettings.ProjectName + "{0}" + appSettings.ApplicationName;
+
+      _dataObjectSet = new Dictionary<string, IList<IDataObject>>();
+      _classIdentifiers = new Dictionary<string, List<string>>();
+      _xPathValuePairs = new List<Dictionary<string, string>>();
+      _hierachicalDTOClasses = new Dictionary<string, List<string>>();
+
+      _graphNs = String.Format(adapterSettings.GraphBaseUri + scope + "#", "/");
+      _dataObjectNs = String.Format(DATALAYER_NS + ".proj_" + scope, ".");
+      _dataObjectsAssemblyName = adapterSettings.ExecutingAssemblyName;
+    }
+
+    public XElement GetXml(ref Mapping mapping, string graphName,
+      ref DataDictionary dataDictionary, ref Dictionary<string, IList<IDataObject>> dataObjects)
+    {
+      throw new NotImplementedException();
+
+      //try
+      //{
+      //  _mapping = mapping;
+      //  _graphMap = _mapping.FindGraphMap(graphName);
+
+      //  _dataDictionary = dataDictionary;
+
+      //  //getSparqlResults
+
+      //  return _dataObjectSet;
+      //}
+      //catch (Exception ex)
+      //{
+      //  throw ex;
+      //}
+    }
+
+    public Dictionary<string, IList<IDataObject>> GetDataObjects(ref Mapping mapping, string graphName,
+      ref DataDictionary dataDictionary, ref XElement xml)
+    {
+      throw new NotImplementedException();
+
+      //try
+      //{
+      //  _mapping = mapping;
+      //  _graphMap = _mapping.FindGraphMap(graphName);
+
+      //  _dataDictionary = dataDictionary;
+
+      //  //ReadSparqlResults
+
+      //  return _dataObjectSet;
+      //}
+      //catch (Exception ex)
+      //{
+      //  throw ex;
+      //}
+    }
+
+    #region helper methods
+    private string ExtractId(string qualifiedId)
+    {
+      if (String.IsNullOrEmpty(qualifiedId) || !qualifiedId.Contains(":"))
+        return qualifiedId;
+
+      return qualifiedId.Substring(qualifiedId.IndexOf(":") + 1);
+    }
+
+    private string TitleCase(string value)
+    {
+      string returnValue = String.Empty;
+      string[] words = value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+      foreach (string word in words)
+      {
+        returnValue += word.Substring(0, 1).ToUpper();
+
+        if (word.Length > 1)
+          returnValue += word.Substring(1).ToLower();
+      }
+
+      return returnValue;
+    }
+
+    private string ResolveValueList(string valueList, string value)
+    {
+      foreach (ValueList valueLst in _mapping.valueLists)
+      {
+        if (valueLst.name == valueList)
+        {
+          foreach (ValueMap valueMap in valueLst.valueMaps)
+          {
+            if (valueMap.internalValue == value)
+            {
+              return valueMap.uri.Replace("rdl:", RDL_NS.NamespaceName);
+            }
+          }
+        }
+      }
+
+      return RDF_NIL;
+    }
+
+    // get max # of data records from all data objects
+    private int MaxDataObjectsCount()
+    {
+      int maxCount = 0;
+
+      foreach (var pair in _dataObjectSet)
+      {
+        if (pair.Value.Count > maxCount)
+        {
+          maxCount = pair.Value.Count;
+        }
+      }
+
+      return maxCount;
+    }
+
+    private void PopulateClassIdentifiers()
+    {
+      _classIdentifiers.Clear();
+
+      foreach (ClassMap classMap in _graphMap.classTemplateListMaps.Keys)
+      {
+        List<string> classIdentifiers = new List<string>();
+
+        foreach (string identifier in classMap.identifiers)
+        {
+          // identifier is a fixed value
+          if (identifier.StartsWith("#") && identifier.EndsWith("#"))
+          {
+            string value = identifier.Substring(1, identifier.Length - 2);
+            int maxDataObjectsCount = MaxDataObjectsCount();
+
+            for (int i = 0; i < maxDataObjectsCount; i++)
+            {
+              if (classIdentifiers.Count == i)
+              {
+                classIdentifiers.Add(value);
+              }
+              else
+              {
+                classIdentifiers[i] += classMap.identifierDelimeter + value;
+              }
+            }
+          }
+          else  // identifier value comes from a property
+          {
+            string[] property = identifier.Split('.');
+            string objectName = property[0].Trim();
+            string propertyName = property[1].Trim();
+
+            IList<IDataObject> dataObjects = _dataObjectSet[objectName];
+            if (dataObjects != null)
+            {
+              for (int i = 0; i < dataObjects.Count; i++)
+              {
+                string value = Convert.ToString(dataObjects[i].GetPropertyValue(propertyName));
+
+                if (classIdentifiers.Count == i)
+                {
+                  classIdentifiers.Add(value);
+                }
+                else
+                {
+                  classIdentifiers[i] += classMap.identifierDelimeter + value;
+                }
+              }
+            }
+          }
+        }
+
+        _classIdentifiers[classMap.classId] = classIdentifiers;
+      }
+    }
+
+    #endregion
+  }
+}
