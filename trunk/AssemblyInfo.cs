5d315eb2c282c9ddd6d2090dd820419cdfb8124d
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-06-18 17:44:30 +0000

    Converted Solution to VisualStudio 2010

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.1053")]
-[assembly: AssemblyFileVersion("2.0.0.1053")]
+[assembly: AssemblyVersion("2.0.0.1057")]
+[assembly: AssemblyFileVersion("2.0.0.1057")]
