c11954a4f0b26fc655fc4cc0bc6e3492e2204f55
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-06-23 19:09:29 +0000

    Some clean ups.

--- Prototypes/cxfdemo/src/com/example/EmployeeService.java
+++ Prototypes/cxfdemo/src/com/example/EmployeeService.java
@@ -1,13 +1,9 @@
 package com.example;
 
-//import javax.ws.rs.ConsumeMime;
 import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-//import javax.ws.rs.ProduceMime;
 import javax.ws.rs.Produces;
-//import javax.ws.rs.core.Response;
 
 @Path("/")
 public class EmployeeService
