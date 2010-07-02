1a93e7ec7581ef8966d92fb360ba4c7b55d98517
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-07-02 23:30:46 +0000

    Post Commit Cleanup see rev1148

---
+++ Adapter/NHibernateLibrary/AdapterClient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.utility;
+using System.Net;
+using org.iringtools.library;
+using System.Xml.Linq;
+
+namespace org.iringtools.application
+{
+  class AdapterClient : ServiceClient
+  {
+    private ApplicationSettings _settings = null;
+
+    public AdapterClient(ApplicationSettings settings)
+      : base((ServiceSettings)settings, settings["AdapterServiceUri"])
+    {
+      _settings = settings;
+    }
+
+    public Response PostScopes(List<ScopeProject> scopes)
+    {
+      Response response = new Response();
+      try
+      {
+        string relativeUri = "/scopes";
+
+        response = _webHttpClient.Post<List<ScopeProject>, Response>(relativeUri, scopes);
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Error while calling adapter service to update the binding.", ex);
+      }
+      return response;
+    }
+
+    public Response PostBinding(XElement binding)
+    {
+      Response response = new Response();
+      try
+      {
+        string relativeUri = String.Format("/{0}/{1}/binding",
+          _settings["ProjectName"],
+          _settings["ApplicationName"]
+        );
+
+        response = _webHttpClient.Post<XElement, Response>(relativeUri, binding);
+      }
+      catch (Exception ex)
+      {
+        throw new Exception("Error while calling adapter service to update the binding.", ex);
+      }
+      return response;
+    }
+  }
+}
---
+++ Adapter/NHibernateLibrary/ApplicationModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+using org.iringtools.library;
+using System.Collections.Specialized;
+
+namespace org.iringtools.application
+{
+  class ApplicationModule : NinjectModule
+  {
+    public override void Load()
+    {
+      Bind<ApplicationSettings>().ToSelf().InSingletonScope();
+    }
+  }
+}
