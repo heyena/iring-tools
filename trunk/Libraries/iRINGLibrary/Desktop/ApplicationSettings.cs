95ff5211af0ff651b1cf851d4a14d7f6529757d2
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-07-03 01:42:50 +0000

    Added Missing Files

---
+++ trunk/Libraries/iRINGLibrary/Desktop/ApplicationSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using org.iringtools.library;
+using System.Collections.Specialized;
+
+namespace org.iringtools.application
+{
+  public class ApplicationSettings : ServiceSettings
+  {
+    public ApplicationSettings()
+      : base()
+    {
+      this.Add("ExecutingAssemblyName", "App_Code");
+      this.Add("BinaryPath", @".\Bin\");
+      this.Add("CodePath", @".\App_Code\");
+    }
+  }
+}
