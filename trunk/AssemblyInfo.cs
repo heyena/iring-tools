ce4d17e6fc1abfc287bd4cca38cd965381fae426
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-07-13 20:15:45 +0000

    Updated build script.

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.1190")]
-[assembly: AssemblyFileVersion("2.0.0.1190")]
+[assembly: AssemblyVersion("2.0.0.1191")]
+[assembly: AssemblyFileVersion("2.0.0.1191")]
