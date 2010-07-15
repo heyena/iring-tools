4d9b23fce6fcd6b54abb69d46d3f3f165162cd30
strydom5607@googlemail.com <strydom5607@googlemail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-07-15 11:45:41 +0000

    added compositeKeys to Application editor

---
+++ trunk/NHibernateEditor/NHibernateEditor/CompositeKeys.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace ApplicationEditor
+{
+  public partial class CompositeKeys : ChildWindow
+  {
+    internal ObservableCollection<String> _keyItems;
+    internal ObservableCollection<String> _dataItems;
+    public CompositeKeys()
+    {
+      InitializeComponent();
+    }
+
+    private void OKButton_Click(object sender, RoutedEventArgs e)
+    {
+      this.DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+      this.DialogResult = false;
+    }
+
+    private void btnAddCompKey_Click(object sender, RoutedEventArgs e)
+    {
+      //for (int control = lbSourceProperties.SelectedItems.Count - 1; control >= 0; control--)
+      //{
+        String lbItem = lbSourceProperties.SelectedItem as String;
+        int index = lbSourceProperties.SelectedIndex;
+        if (lbItem != null)
+        {
+          if (!_keyItems.Contains(lbItem))
+          {
+            _dataItems.RemoveAt(index);
+            _keyItems.Add(lbItem);
+          }
+        }
+      //}
+    }
+
+
+    private void btnDelCompKey_Click(object sender, RoutedEventArgs e)
+    {
+     // for (int control = lbKeys.SelectedItems.Count - 1; control >= 0; control--)
+     // {
+        String lbItem = (String)lbKeys.SelectedItem;
+        int index = lbKeys.SelectedIndex;
+        if (lbItem != null)
+        {
+          if (!_dataItems.Contains(lbItem))
+          {
+            _keyItems.RemoveAt(index);
+            _dataItems.Add(lbItem);
+          }
+
+        }
+      //}
+    }
+  }
+}
+
+
+
+
