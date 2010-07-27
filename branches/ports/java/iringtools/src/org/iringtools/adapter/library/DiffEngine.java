2d222f1f095caf44603ec52181cfef5a939f4afb
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-07-27 20:07:13 +0000

    Re-structure projects.

--- branches/ports/java/iringtools/src/org/iringtools/adapter/library/DiffEngine.java
+++
@@ -1,9 +0,0 @@
-package org.iringtools.adapter.library;
-
-import org.iringtools.adapter.library.dto.DataTransferObjects;
-import org.iringtools.adapter.library.manifest.Graph;
- 
-public interface DiffEngine
-{
-  DataTransferObjects diff(Graph graphMap, String sendingXml, String receivingXml) throws Exception;
-}
