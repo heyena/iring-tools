ee3bc4aac2157d5cfa0a528147c07485d87316fe
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-08-02 14:46:11 +0000

    Fixed ReferenceDataService GetRepositoriesTest.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.1307")]
-[assembly: AssemblyFileVersion("2.0.0.1307")]
+[assembly: AssemblyVersion("2.0.0.1325")]
+[assembly: AssemblyFileVersion("2.0.0.1325")]
