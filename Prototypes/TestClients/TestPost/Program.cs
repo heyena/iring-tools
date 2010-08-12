7b6ebaa77a0d59defa8a7bd36a0c4ef65f98534b
rsagolsem <rsagolsem@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-08-12 13:26:53 +0000

    Changed UseDataContractSerializer to true

--- Prototypes/TestClients/TestPost/Program.cs
+++ Prototypes/TestClients/TestPost/Program.cs
@@ -35,12 +35,12 @@ namespace TestPost
 
           
           string qmxfPath = @"..\..\testing-QMXF.xml";
-          QMXF qmxf = Utility.Read<QMXF>(qmxfPath, false);
+          QMXF qmxf = Utility.Read<QMXF>(qmxfPath, true);
 
           string baseUri = "http://localhost:62460/";
           string relativeUri = "part8/templates";
           WebHttpClient client = new WebHttpClient(baseUri);
-          Response response = client.Post<QMXF, Response>(relativeUri, qmxf, false);
+          Response response = client.Post<QMXF, Response>(relativeUri, qmxf, true);
 
           foreach (Status status in response.StatusList)
           {
