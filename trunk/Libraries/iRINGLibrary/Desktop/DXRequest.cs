840e165bb6ee5c9d2661de12498c0efd3f9b0f08
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-08-16 15:26:49 +0000

    Forgot DXRequest.cs

---
+++ trunk/Libraries/iRINGLibrary/Desktop/DXRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using org.iringtools.library;
+using org.iringtools.library.manifest;
+
+namespace org.iringtools.library
+{
+  [CollectionDataContract]
+  public class DXRequest : Request
+  {
+    [DataMember]
+    public Manifest Manifest { get; set; }
+
+    [DataMember]
+    public Identifiers Identifiers { get; set; }
+  }
+}
