30a19b0ee1b0736965d61f2d5af61df097acc810
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-08-25 20:47:45 +0000

    clean up prototypes

--- trunk/Prototypes/iringtools/src/generate.cmd
+++
@@ -1,4 +0,0 @@
-setlocal
-set xsdFile=%1
-rem java -jar C:\Development\java\libraries\jaxb-ri-20100511\lib\jaxb-xjc.jar %xsdFile%
-C:\Development\java\libraries\jaxb-ri-20100511\bin\xjc -cp C:\Development\java\libraries\jaxb-ri-20100511\lib\collection-setter-injector.jar -Xcollection-setter-injector %xsdFile%
