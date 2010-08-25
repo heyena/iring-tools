afed3c1f334de9a4bc0bb2e2c7a3d64e2d8f6a26
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-08-25 01:31:18 +0000

    prototypes/iringtools.mvc update

--- Prototypes/iRINGTools.MVC/Models/ProjectTreeNode.cs
+++ Prototypes/iRINGTools.MVC/Models/ProjectTreeNode.cs
@@ -6,15 +6,17 @@ using org.iringtools.client.Models.Ext;
 
 namespace org.iringtools.client.Models
 {
-  public class ProjectTreeNode : TreeNode
+  public class ProjectTreeNode : TreeNode<ProjectTreeNode>
   {
     public ProjectTreeNode()
     {
-      children = new List<TreeNode>();
+      children = new List<ProjectTreeNode>();
+      expanded = false;
     }
 
-    public string name { get; set; }
+    public string name { get; set; }    
     public string description { get; set; }
+    public bool expanded { get; set; }
     public override string id
     {
       get
@@ -30,7 +32,7 @@ namespace org.iringtools.client.Models
     {
       get
       {
-        return name + ": " + description;
+        return name + " [" + description + "]";
       }      
     }
   }
