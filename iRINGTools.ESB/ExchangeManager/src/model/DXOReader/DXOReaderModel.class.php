b1c988e5745d0d78b22e17516e9dcd3806db2903
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-09-07 11:24:46 +0000

    class to fetch the DTO information from web-service

---
+++ iRINGTools.ESB/ExchangeManager/src/model/DXOReader/DXOReaderModel.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author Aswini Nayak (aknayak@bechtel.com)
+*/
+include_once('/model/RestfulService/curl.class.php');
+
+class DXOReaderModel{
+	private $dxoUrl;
+	function __construct(){
+		$this->dxoUrl = 'http://localhost:81/iRINGTools.ESB/ExchangeManager/src/dto.xml';
+	}
+
+function getDXOInfo($exchangeID,$postParams){
+	if(is_array($exchangeID)){
+		$this->dxoUrl = $this->dxoUrl.'?'.http_build_query($exchangeID);
+		// http://localhost:81/iRINGTools.ESB/ExchangeManager/src/dto.xml?exchangeID=1
+	}
+
+	$curlObj = new curl($this->dxoUrl);
+	$curlObj->setopt(CURLOPT_POST, 1);
+	$curlObj->setopt(CURLOPT_POSTFIELDS,'TagLists='.$postParams);
+	$fetchedData = $curlObj->exec();
+
+	print_r($fetchedData);
+	
+	
+	/*echo '<pre>';
+	var_dump($curlObj);
+	echo '</pre>';
+	*/
+	/*$xmlIterator = new SimpleXMLIterator($fetchedData);
+	$resultArr="" ;
+	foreach($xmlIterator  as $dataTransferIndices)
+	{
+		if($resultArr==''){
+		$resultArr=$dataTransferIndices->identifier;
+		}else{
+		$resultArr=$resultArr.','.$dataTransferIndices->identifier;
+		}
+	}
+	return $resultArr;
+	*/
+}
+}
+?>
