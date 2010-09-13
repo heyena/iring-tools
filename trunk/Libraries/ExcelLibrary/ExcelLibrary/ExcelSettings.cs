2a4ad9babf533ec675ebc0c9c50b562b20fcd24e
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-09-13 12:35:37 +0000

    Added the ExcelLibrary to the Library folder.

---
+++ trunk/Libraries/ExcelLibrary/ExcelLibrary/ExcelSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.library;
+
+namespace org.iringtools.excel
+{
+  public class ExcelSettings : ServiceSettings
+  {
+    public ExcelSettings() : base()
+    {      
+    }
+  }
+}
