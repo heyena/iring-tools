f90093c67c557a081e9f14450284c612b2dcb9c9
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-09-16 13:22:57 +0000

    Corrected directory.xml Added DTI and DTO files and pointed init at them.

--- iRINGTools.ESBServices/ExchangeManager/config/init.php
+++ iRINGTools.ESBServices/ExchangeManager/config/init.php
@@ -8,7 +8,7 @@ define('CONTROLLER_DIR', MVC_DIR . 'controller/');
 define('AJAX_HANDLER_DIR', MVC_DIR . 'AJAXhandlers/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
 
-define('DIRECTORY_REQUEST_URL','http://localhost:81/projects/iRINGTools.ESB/ExchangeManager/directory.xml');
-define('DXI_REQUEST_URL', 'http://localhost:81/projects/iRINGTools.ESB/ExchangeManager/dxo.xml');
-define('DXO_REQUEST_URL', 'http://localhost:81/projects/iRINGTools.ESB/ExchangeManager/dxi.xml');
+define('DIRECTORY_REQUEST_URL','http://localhost:8888/ExchangeManager/directory.xml');
+define('DXI_REQUEST_URL', 'http://localhost:8888/ExchangeManager/dto.xml');
+define('DXO_REQUEST_URL', 'http://localhost:8888/ExchangeManager/dti.xml');
 ?>
