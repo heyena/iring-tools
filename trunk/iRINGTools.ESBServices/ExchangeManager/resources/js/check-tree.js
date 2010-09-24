85ac1d46b3b76aff3fbfcfeb50364c715758bd52
ritu.dec27 <ritu.dec27@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-09-24 11:34:06 +0000

    Updated the Directory Tree with Loading Mask, save State and About pop-up mask

--- trunk/iRINGTools.ESBServices/ExchangeManager/resources/js/check-tree.js
+++ trunk/iRINGTools.ESBServices/ExchangeManager/resources/js/check-tree.js
@@ -1,48 +1,49 @@
-/*!
- * Ext JS Library 3.2.1
- * Copyright(c) 2006-2010 Ext JS, Inc.
- * licensing@extjs.com
- * http://www.extjs.com/license
+/*
+ * @File Name : check-tree.js
+ * @Path : resources/js
+ * @Using Lib : Ext JS Library 3.2.1(lib/ext-3.2.1)
+ * 
+ * This file intended to make Directory Tree Panel of Exchange Manager
+ * The data of tree nodes comes form the src/controller/ExchangeReader/ExchangeReader.class.php
+ * in the form of JSON String
+ * 
  */
-Ext.onReady(function(){
-	
-	var tBar = new Ext.Toolbar({ xtype: "toolbar",
-         
-	items: [
-                {xtype:"tbbutton",
-                icon:'resources/images/16x16/view-refresh.png',
-                qtip:'Refresh',
-                id: 'headRefresh', disabled: false,handler: function(){alert("Refresh Clicked")
+Ext.onReady(function(){	
+    var tBar = new Ext.Toolbar({ xtype: "toolbar",
+    items: [ {
+        xtype:"tbbutton",
+        icon:'resources/images/16x16/view-refresh.png',
+        qtip:'Refresh',
+        id: 'headRefresh', disabled: false,handler: function()
+            {alert("Refresh Clicked")}
+        },
+        {xtype:"tbbutton",text:"Exchange", id: 'headExchange', disabled: false,
+        handler: function(){
+        Ext.Msg.show({
+        title: ':: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ',
+        msg: 'Would you like to review the <br/>Data Exchange before starting?',
+        buttons: Ext.Msg.YESNO,
+        icon: Ext.Msg.QUESTION,//'profile', // &lt;- customized icon
+        fn: function(action){
+           if(action=='yes')
+           {
+                   if(tree.getSelectionModel().getSelectedNode().id!=null){
+                           Ext.getCmp('centerPanel').enable();
+                   }
+           }
+           else if(action=='no')
+           {
+                   alert('You clicked on No');
+           }
+       }});
+    }}
 
-                }},
-		{xtype:"tbbutton",text:"Exchange", id: 'headExchange', disabled: false,
-		handler: function(){
-		Ext.Msg.show({
-		title: ':: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: :: ',
-		msg: 'Would you like to review the <br/>Data Exchange before starting?',
-		buttons: Ext.Msg.YESNO,
-		icon: Ext.Msg.QUESTION,//'profile', // &lt;- customized icon
-		fn: function(action){
-		   if(action=='yes')
-		   {
-			   if(tree.getSelectionModel().getSelectedNode().id!=null){
-				   Ext.getCmp('centerPanel').enable();
-					//***** http://localhost:8080/iringtools/diffservice/12345_000/exchanges/1/index
-			   }
-		   }
-		   else if(action=='no')
-		   {
-			   alert('You clicked on No');
-		   }
-	   }});
-	}}
-								   
-	]});
+    ]});
 	
   var tree = new Ext.tree.TreePanel({
-      
+    id:'directory-tree',
     renderTo:'tree-div',
-    height: 460,
+    height: 494,    
     baseCls : 'x-plain',
     bodyBorder:false,
     border:true,
@@ -54,29 +55,36 @@ Ext.onReady(function(){
     margins: '0 0 0 0',
     cmargins: '100 100 100 100',
     lines :true,
-    //enableDD:true,
     containerScroll: true,
     rootVisible: true,
     frame: true,
-    //requestMethod:'GET', default is post
     root: {
       nodeType: 'async',
       icon: 'resources/images/16x16/internet-web-browser.png',
       text: 'Directory'
     },
     // auto create TreeLoader
-    //loader: new Ext.tree.TreeLoader(),
     dataUrl: 'ExchangeReader/exchnageList/1',
     tbar:tBar,
-
     listeners: {
+        BeforeLoad:{
+           fn:function(){
+             // Basic mask:
+              Ext.getCmp('directory-tree').el.mask('Loading...', 'x-mask-loading')            
+             }
+           },
+        load:{
+           fn:function(){
+             // Basic unmask:
+             Ext.getCmp('directory-tree').el.unmask()
+           }
+        },
         click: {
          fn: function(node){
              //get all the attributes of node
              obj = node.attributes
              var details_data = []
-               for(var key in obj){                 
-                 // val.push('"'+key+'":'+'"'+obj[key]+'"')
+               for(var key in obj){                
                  // restrict some of the properties to be displayed
                  if(key!='id' && key!='text' && key!='icon' && key!='children' && key!='loader' && key!='leaf' && key!='applicationId'){
                     details_data[key]=obj[key]
@@ -94,34 +102,29 @@ Ext.onReady(function(){
              // check the current state of Detail Grid panel
              if(Ext.getCmp('detail-grid').collapsed==true){
                  Ext.getCmp('detail-grid').expand();
-              }
-             
+              }             
           }
+        },
+        expandnode:{
+            fn : function (node){
+                Ext.state.Manager.set("treestate", node.getPath())
+            }
         }
-      }
+    }
   });
 
   var contextMenu = new Ext.menu.Menu({
     items: [
-    {
-      text: 'Sort',
-      handler: sortHandler
-    }
-    ,{
-      text: 'Exchange',
-      handler: exchangeHandler
-    }
+        {
+          text: 'Sort',
+          handler: sortHandler
+        }
     ]
   });
-  //tree.on('contextmenu', treeContextHandler);
-
-  function exchangeHandler(){
-    alert(tree.getSelectionModel().getSelectedNode().id);
-  }
   function sortHandler() {
     tree.getSelectionModel().getSelectedNode().sort(
       function (leftNode, rightNode) {
-        return 1;//(leftNode.text.toUpperCase() < rightNode.text.toUpperCase() ? 1 : -1);
+        return 1;
       }
       );
   }
@@ -132,17 +135,11 @@ Ext.onReady(function(){
     node.select();
     contextMenu.show(node.ui.getAnchor());
   });
-		
-  /*tree.on('expandnode', function (node){
-			//node.id
-			Ext.state.Manager.set("treestate", node.getPath());
-			//Ext.get('pg').collapse();
-		});*/
-		
+
+/* to maintain the state of the tree */
   var treeState = Ext.state.Manager.get("treestate");
   if (treeState){
     tree.expandPath(treeState);
   }
-/* to maintain the state of the tree */
 			
 });
