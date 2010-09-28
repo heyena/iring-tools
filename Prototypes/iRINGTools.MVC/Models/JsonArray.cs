23178d581c128f6430fc7d9a041f8c41be650e48
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-09-28 01:06:11 +0000

    General updates to the iRingTools.MVC ( Missing file)

---
+++ Prototypes/iRINGTools.MVC/Models/JsonArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace org.iringtools.client.Models
+{
+  public class JsonArrayItem<T1, T2>
+  {
+    public T1 Key { get; set; }
+    public T2 Value { get; set; }
+  }
+  
+  public class JsonArray : List<JsonArrayItem<string, string>>
+  {
+    public void Add(string key, string value)
+    {
+      this.Add(new JsonArrayItem<string, string>{ Key = key, Value = value });
+    }
+  }
+}
