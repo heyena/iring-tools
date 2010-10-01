d830e677cdfe5671e974cff9f90854782d810256
ritu.dec27 <ritu.dec27@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-10-01 13:25:32 +0000

    Rewrite ExchangeReader Model Class to parse new Directory.xml

--- trunk/iRINGTools.ESBServices/ExchangeManager/src/model/ExchangeReader/ExchangeReaderModel.class.php
+++ trunk/iRINGTools.ESBServices/ExchangeManager/src/model/ExchangeReader/ExchangeReaderModel.class.php
@@ -20,64 +20,6 @@ class ExchangeReaderModel{
 		print_r($fetchedData);
 	}
 
-        /**
-	 * Generate the array of application data graphs: applicationID wise grouping.
-	 *
-	 * @param one(Object)
-	 * @returns array
-	 * @access private
-	 */
-        private function getApplicationGraphArray($appGraphs){
-
-                $applicationGraphArray= array();
-
-                foreach($appGraphs  as $appGraph)
-                {
-                        $graphArray         =	array();
-
-                        $applicationId      = 	'appId_'.(string)$appGraph ->applicationId;
-                        $graphIdentifier    = 	(string)$appGraph ->identifier;
-                        $graphName          =	(string)$appGraph ->name;
-                        $graphCommodity     = 	(string)$appGraph ->commodity;
-                        $graphDescription   = 	(string)$appGraph ->description;
-
-                        $graphArray = array("text" =>$graphName, "id"=>$graphIdentifier, "applicationId"=>$applicationId,"Description"=>$graphDescription, "Commodity"=>$graphCommodity, "Title" =>$graphName, "icon" =>"resources/images/16x16/file-table.png", "leaf" => "true");
-
-                        $applicationGraphArray[$applicationId][]=$graphArray;
-
-                }
-                unset($graphArray);
-                return $applicationGraphArray;
-        }
-
-        /**
-	 * Generate the array of applications scope wise grouping
-	 *
-	 * @param one(Object)
-	 * @returns array
-	 * @access private
-	 */
-        private function getScopeDataExchangesArray($dataExchanges){
-
-                $exchangeScopeArray	=	array();
-
-                foreach($dataExchanges  as $dataExchange)
-                {  
-                        $exchangeArray 	=   array();
-                        $scopeVal 	=   (string)$dataExchange->scope;
-                        $dataExchangeID =   'dxId_'.(string)$dataExchange->identifier;
-                        $commodity 	=   (string)$dataExchange->commodity;
-
-                        $exchangeArray	=   array("Title"=>(string)$dataExchange->name, "text" =>(string)$dataExchange->name, "id"=>$dataExchangeID,"Scope"=>$scopeVal,"Description"=>(string)$dataExchange->description, "icon" =>"resources/images/16x16/file-table-diff.png", "leaf" => "true");
-
-                        $exchangeScopeArray[$scopeVal][$commodity]['Title']	=   $commodity;
-                        $exchangeScopeArray[$scopeVal][$commodity]['text']	=   $commodity;
-                        $exchangeScopeArray[$scopeVal][$commodity]['icon']	=   'resources/images/16x16/class-badge.png';
-                        $exchangeScopeArray[$scopeVal][$commodity]['children'][]=   $exchangeArray;;  // exchange Array should be inside this children
-                }
-                unset($exchangeArray);
-                return $exchangeScopeArray;
-        }
 
         /**
 	 * Convert XML to required JSON format for the Directory Tree.
@@ -88,56 +30,101 @@ class ExchangeReaderModel{
 	 */
 	function getDirectoryTreeJSONData(){
 
-            $resultArray    =	array();
-            $appScopeArray  =	array();
-
+            $resultArray = array();
             $curlObj     = new curl($this->exchangeUrl);
             $fetchedData = $curlObj->exec();
             $xmlIterator = new SimpleXMLIterator($fetchedData);
 
-            $applications       =   $xmlIterator->applications->application;
-            $graphArr           =   $this->getApplicationGraphArray($xmlIterator->applicationData->graph);
-            $dataExchangeArray  =   $this->getScopeDataExchangesArray($xmlIterator->dataExchanges->exchange);
+            // loop for scopes array
+            foreach($xmlIterator->scope  as $scope)
+            {
+                 $applicationArray = array();
+                // loop for applications array under each scope
+                foreach($scope->applicationData->children() as $application){
+               
+                    $graphArray = array();
+                    // loop for all graphs under each application
+                    foreach($application->graphs->children() as $graph){
+                       $graphArray[] = array(
+                        'id'=> 'gpId_'.(string)$graph->id,
+                        'uid'=> (string)$graph->id,
+                        'text'=> (string)$graph->name,
+                        'Title'=> (string)$graph->name,
+                        'Description'=> (string)$graph->description,
+                        'Commodity' => (string)$graph->commodity,
+                        'icon' =>'resources/images/16x16/file-table.png',
+                        'node_type' => 'graph',
+                        'leaf' => 'true'
+                      );
+                    }
+                    $applicationArray[]= array(
+                        'id'=>'appId_'.(string)$application->id,
+                        'uid'=>(string)$application->id,
+                        'text'=>(string)$application->name,
+                        'Title'=>(string)$application->name,                        
+                        'Desccription'=>(string)$application['description'],
+                        'node_type' => 'application',
+                        'icon'=>'resources/images/16x16/applications-internet.png',
+                        'children'=>$graphArray
+                       );
+                    
 
-                // make graphs array of Application Data along with Scope as Key of array
-                foreach($applications  as $applicaton)
-                {
-                        $appArray 	=   array();
-                        $scopeVal 	=   (string)$applicaton->scope;
-                        $applicationId  =   'appId_'.(string)$applicaton->id;
-
-                        $appArray = array("text" =>(string)$applicaton->name, "id"=>$applicationId,"Description"=>(string)$applicaton->description,'Scope'=>(string)$applicaton->scope,"Title" =>(string)$applicaton->name, "icon" => "resources/images/16x16/applications-internet.png", "children"=>$graphArr[$applicationId]);
-
-                        $appScopeArray[$scopeVal]["Title"]	=   "Application Data";
-                        $appScopeArray[$scopeVal]["text"]	=   "Application Data";
-                        $appScopeArray[$scopeVal]["icon"]	=   "resources/images/16x16/folder.png";
-                        $appScopeArray[$scopeVal]["children"][]	=   $appArray;
+                    unset($graphArray);
                 }
-                // get the scopes Exchange Manger
-                $scopeArray =   array_keys($appScopeArray);
 
-                // make complete directory tree array
-                foreach($scopeArray as $scope){
+                $commodityArray = array();
+                //loop for get all commodity array under each scope
+                foreach($scope->dataExchanges->children() as $commodity){
+                   $exchangeArray = array();
+                    // get all dataExchange array under each commodity of scope
+                    foreach($commodity->exchanges->children() as $exchange){
+                      
+                        $exchangeArray[] = array(
+                        'id'=> 'exID_'.(string)$exchange->id,
+                        'uid'=> (string)$exchange->id,
+                        'text'=> (string)$exchange->name,
+                        'Title'=> (string)$exchange->name,
+                        'Description'=> (string)$exchange->description,
+                        'Commodity' => (string)$exchange->commodity,
+                        'Scope' => (string)$scope->name,
+                        'icon' =>'resources/images/16x16/file-table-diff.png',
+                        'node_type' => 'exchange',
+                        'leaf' => 'true'
+                      );
+                    }
+                    $commodityArray[]= array(
+                        'text'=>(string)$commodity->name,
+                        'Title'=>(string)$commodity->name,
+                        'node_type' => 'commodity',
+                        'icon'=>'resources/images/16x16/class-badge.png',
+                        'children'=>$exchangeArray
+                       );
 
-                    $dataExchangeScopeArray  = $dataExchangeArray[$scope];
-
-                    // create the array as per the same length of $dataExchangeScopeArray
-                    $numericArray =  range ( 0, count($dataExchangeScopeArray)-1, 1 );
-
-                    // changed the keys(scope value) of array to keys(0,1,..). It is must for Tree Generation
-                    $dXArray  = array_combine($numericArray, $dataExchangeScopeArray);
-
-                    $resultArray []= array("Title"=>$scope, "text"=>$scope, "icon"=>"resources/images/16x16/system-file-manager.png", "children"=>array($appScopeArray [$scope], array("Title"=>"Data Exchange", "text"=>"Data Exchange","icon" =>"resources/images/16x16/folder.png","children"=>$dXArray)));
+                    unset($exchangeArray);
                 }
-                unset($dXArray);
-                unset($appArray);
-                unset($graphArr );
-                unset($scopeArray);
-                unset($numericArray);
-                unset($appScopeArray);
-                unset($dataExchangeArray);
-                unset($dataExchangeScopeArray);
-                return json_encode($resultArray);
+                    // combine Application Data and Data Exchange arrays
+                    $scopeArray[]=array(
+                        'text'=>(string)(string)$scope->name,
+                        'Title'=>(string)(string)$scope->name,
+                        'node_type' => 'scope',
+                        'icon'=>'resources/images/16x16/system-file-manager.png',
+                        'children'=>array(
+                             array(
+                             'text'=>'Application Data',
+                             'icon'=>'resources/images/16x16/folder.png',
+                             'children'=>$applicationArray),
+                            array(
+                             'text'=>'Data Exchange',
+                             'icon'=>'resources/images/16x16/folder.png',
+                             'children'=>$commodityArray),
+                            )
+                    );             
+              }
+
+             unset($applicationArray);
+             unset($commodityArray);
+             return json_encode($scopeArray);
+        
        }// end of function "getDirectoryTreeJSONData"
 }
 ?>
