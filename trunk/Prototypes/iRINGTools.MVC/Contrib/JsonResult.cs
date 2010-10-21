1b27975db0d2a2b68dfcd8ed70987b2d920df5df
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-10-21 02:26:19 +0000

    adapteramanger mvc missing files

---
+++ trunk/Prototypes/iRINGTools.MVC/Contrib/JsonResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace org.iringtools.client.Contrib
+{
+  public class MyJsonResult : JsonResult
+  {
+    private object _objectToSerialize = null;
+    private bool _useDataContract = false;
+
+    public MyJsonResult(object objectToSerialize)
+      : this(objectToSerialize, true)
+    {
+    }
+
+    public MyJsonResult(object objectToSerialize, bool useDataContract)
+    {
+      _objectToSerialize = objectToSerialize;
+      _useDataContract = useDataContract;
+    }
+
+    public override void ExecuteResult(ControllerContext context)
+    {
+      if (_objectToSerialize != null)
+      {
+
+        if (_useDataContract)
+        { 
+          
+          var xs = new DataContractJsonSerializer(_objectToSerialize.GetType());
+          context.HttpContext.Response.ContentType = "application/json";
+                    
+          var writer = XmlWriter.Create(context.HttpContext.Response.Output);
+          xs.WriteObject(writer, _objectToSerialize);
+          writer.Close();
+        }
+        else
+        {
+          //var xs = new JsonSerializer(_objectToSerialize.GetType());
+          //context.HttpContext.Response.ContentType = "application/json";
+          //xs.Serialize(context.HttpContext.Response.Output, _objectToSerialize);
+        }
+      }
+    }
+  }
+}
