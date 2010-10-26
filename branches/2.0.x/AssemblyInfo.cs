64b3fa15edf5aaffa32b2fd2457b3e519db7f06b
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-10-26 04:06:02 +0000

    Build tweaks and clean up regarding default configs and xmls, assemblyInfo and icons.

--- branches/2.0.x/AssemblyInfo.cs
+++ branches/2.0.x/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.1853")]
-[assembly: AssemblyFileVersion("2.0.0.1853")]
+[assembly: AssemblyVersion("2.0.0.1857")]
+[assembly: AssemblyFileVersion("2.0.0.1857")]
--- branches/2.0.x/Utilities/QMXFGenerator/Properties/AssemblyInfo.cs
+++ branches/2.0.x/Utilities/QMXFGenerator/Properties/AssemblyInfo.cs
@@ -6,13 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QMXFGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("QMXF Generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bechtel")]
-[assembly: AssemblyProduct("QMXFGenerator")]
-[assembly: AssemblyCopyright("Copyright © Bechtel 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6d963dc4-d6f6-4a4f-9d2f-055d4ff5dd73")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
