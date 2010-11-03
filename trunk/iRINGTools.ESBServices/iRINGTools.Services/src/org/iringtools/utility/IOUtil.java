01fa144e0e249828c36ffdd91be5be6493a8eadc
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-03 02:40:32 +0000

    Replace NetUtil with WebClient to support NT & proxy credentials and multipart messsage posting. Add sparql results schema and classes.

--- trunk/iRINGTools.ESBServices/iRINGTools.Services/src/org/iringtools/utility/IOUtil.java
+++ trunk/iRINGTools.ESBServices/iRINGTools.Services/src/org/iringtools/utility/IOUtil.java
@@ -1,15 +1,17 @@
 package org.iringtools.utility;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Properties;
 
 import javax.xml.stream.FactoryConfigurationError;
@@ -24,48 +26,59 @@ import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 
 public class IOUtil
 {
-	public static String readString(String filePath) throws IOException
-	{
-		byte[] buffer = new byte[(int) new File(filePath).length()];		
+  public static String toString(InputStream stream) throws IOException
+  {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+    StringWriter writer = new StringWriter();
+    char[] buffer = new char[2048];
+
+    int actualSize;
+    while ((actualSize = reader.read(buffer)) != -1)
+      writer.write(buffer, 0, actualSize);
+
+    return writer.toString();
+  }
+
+  public static String readString(String filePath) throws IOException
+  {
     BufferedInputStream stream = new BufferedInputStream(new FileInputStream(filePath));
-    stream.read(buffer);    
-    return new String(buffer);
-	}
-	
-	public static void writeString(String string, String filePath) throws IOException
-	{
-		BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
-		out.write(string);
-		out.close();		
-	}
-	
-	public static OMElement readXml(String filePath) throws FileNotFoundException, XMLStreamException, FactoryConfigurationError 
-	{
-	  XMLStreamReader parser = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(filePath));
+    return toString(stream);
+  }
+
+  public static void writeString(String string, String filePath) throws IOException
+  {
+    BufferedWriter out = new BufferedWriter(new FileWriter(filePath));
+    out.write(string);
+    out.close();
+  }
+
+  public static OMElement readXml(String filePath) throws FileNotFoundException, XMLStreamException, FactoryConfigurationError
+  {
+    XMLStreamReader parser = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(filePath));
     StAXOMBuilder builder = new StAXOMBuilder(parser);
     return builder.getDocumentElement();
-	}
-	
-	public static OMElement stringToXml(String text) throws XMLStreamException, FactoryConfigurationError 
-	{
-	  XMLStreamReader parser = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(text));
+  }
+
+  public static OMElement stringToXml(String text) throws XMLStreamException, FactoryConfigurationError
+  {
+    XMLStreamReader parser = XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(text));
     StAXOMBuilder builder = new StAXOMBuilder(parser);
     return builder.getDocumentElement();
-	}
-  
-  public static void writeXml(OMElement xml, String filePath) throws FileNotFoundException, XMLStreamException, FactoryConfigurationError 
+  }
+
+  public static void writeXml(OMElement xml, String filePath) throws FileNotFoundException, XMLStreamException, FactoryConfigurationError
   {
     XMLStreamWriter writer = XMLOutputFactory.newInstance().createXMLStreamWriter(new FileOutputStream(filePath));
-    xml.serialize(writer); 
+    xml.serialize(writer);
     writer.flush();
   }
-  
+
   public static Properties loadProperties(String filePath) throws IOException
   {
     Properties props = new Properties();
     InputStream fis = new FileInputStream(filePath);
     props.load(fis);
-    fis.close();    
+    fis.close();
     return props;
   }
 }
