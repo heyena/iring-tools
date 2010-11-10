a78904bf4129e808604b22caf46cf8081b7fa690
strydom5607@googlemail.com <strydom5607@googlemail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-10 11:48:34 +0000

    missed files

---
+++ branches/interop/Libraries/iRINGLibrary/Silverlight/DataRelationships.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using org.iringtools.utility;
+
+namespace org.iringtools.library
+{
+  [CollectionDataContract(Namespace = "http://iringtools.org/common", Name = "relationships", ItemName = "relationship")]
+  public class DataRelationships : List<RelationshipType>
+  {
+    public DataRelationships()
+    {
+#if !SILVERLIGHT
+      foreach (RelationshipType relationship in System.Enum.GetValues(typeof(RelationshipType)))
+      {
+        this.Add(relationship);
+      }
+#else
+      this.AddRange(Utility.GetEnumValues<RelationshipType>());
+
+#endif
+    }
+  }
+}
