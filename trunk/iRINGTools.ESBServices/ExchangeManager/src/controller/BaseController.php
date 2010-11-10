12f9debce317255a2025858c21b257253e5ade3e
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-10 11:11:19 +0000

    parseInt($string) function defined to validate the int value

--- trunk/iRINGTools.ESBServices/ExchangeManager/src/controller/BaseController.php
+++ trunk/iRINGTools.ESBServices/ExchangeManager/src/controller/BaseController.php
@@ -6,5 +6,15 @@ class BaseController{
 		$modelName = $className.'Model';
 		return new $modelName($parameter);
 	}
+
+	protected function parseInt($string) {
+		//	return intval($string);
+		if(preg_match('/(\d+)/', $string, $array)) {
+			return $array[1];
+		} else {
+			return 0;
+		}
+	}
+
 }
 ?>
