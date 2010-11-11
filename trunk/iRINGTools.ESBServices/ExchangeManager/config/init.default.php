4f4e26eb675472c346e570a850f4af55bd9ffee4
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-11 15:57:00 +0000

    add default.

---
+++ trunk/iRINGTools.ESBServices/ExchangeManager/config/init.default.php
@@ -0,0 +1,20 @@
+<?php
+/** Configuration Variables **/
+define('ROOT_DIR', dirname(getcwd()) . '/');
+define('MVC_DIR', ROOT_DIR . 'src/');
+define('VIEW_DIR', MVC_DIR . 'view/');
+define('MODEL_DIR', MVC_DIR . 'model/');
+define('CONTROLLER_DIR', MVC_DIR . 'controller/');
+define('AJAX_HANDLER_DIR', MVC_DIR . 'AJAXhandlers/');
+define('CONFIG_DIR', ROOT_DIR . 'config/');
+
+define('DXI_REQUEST_URL', 'http://localhost:8080/iringtools/services/esb');
+define('DXO_REQUEST_URL', 'http://localhost:8080/iringtools/services/esb');
+define('APP_REQUEST_URI','http://localhost/Services/dto');
+define('DIRECTORY_REQUEST_URL','http://localhost:8080/iringtools/services/esb/directory');
+define('RDL_REQUEST_URI','http://localhost/Services/RefDataService/Classes');
+define('PAGESIZE','5');
+/* Put username & Password required for authentication*/
+define('AUTH_USERID','*******');
+define('AUTH_USERPWD','*******');
+?>
