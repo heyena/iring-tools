517a4aa23df8a4526828451bf0e136da45e3fa80
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-17 03:52:04 +0000

    build verified (except unit tests) tweaked default web.config for services.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.1969")]
-[assembly: AssemblyFileVersion("2.0.0.1969")]
+[assembly: AssemblyVersion("2.0.0.1991")]
+[assembly: AssemblyFileVersion("2.0.0.1991")]
