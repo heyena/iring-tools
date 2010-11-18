5c39bd1a109923884c7f6953528937432aebab0a
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-18 12:53:50 +0000

    refactoring exhangemanager, run index1.html to see any changes

---
+++ iRINGTools.ESBServices/ExchangeManager/resources/js/ExchangeManager.js
@@ -0,0 +1,49 @@
+Ext.onReady(function () {
+	
+	Ext.BLANK_IMAGE_URL = 'resources/images/s.gif';
+	Ext.QuickTips.init();
+	
+	var navigationPanel = new iRINGTools.ExchangeManager.NavigationPanel({
+		id:'navigation-panel',
+		region:'west',
+		
+		//collapseMode: 'mini',
+	  collapsible: true,
+	  collapsed: false,
+	  
+	  border: true,
+	  split: true,
+	  
+		width: 250,
+  	minSize: 175,
+  	maxSize: 500,
+		navigationUrl: 'exchangereader/exchnagelist/1',		
+	});
+	
+	var contentPanel = new Ext.TabPanel({
+		region: 'center',
+		id: 'content-panel',
+		xtype: 'tabpanel',			
+		margins: '0 5 0 0',
+		enableTabScroll: true,
+		defaults: {
+			layout: 'fit'
+		}
+	});
+
+	var viewport = new Ext.Viewport({
+		layout: 'border',
+		renderTo: Ext.getBody(), 
+		items: [{ 
+				region: 'north',
+				baseCls : 'x-plain',
+				height: 65, // give north and south regions a height
+				margins: '-10 5 0 0',
+				contentEl:'header'
+			},
+			navigationPanel,
+			contentPanel
+		]
+	});	
+	
+});
