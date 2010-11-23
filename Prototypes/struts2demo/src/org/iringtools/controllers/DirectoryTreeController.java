25145b5de93cef3c5c35d9c72e40859fc15bf547
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-11-23 15:31:42 +0000

    Update struts2demo with page layout and directory tree.

---
+++ Prototypes/struts2demo/src/org/iringtools/controllers/DirectoryTreeController.java
@@ -0,0 +1,52 @@
+package org.iringtools.controllers;
+
+import org.iringtools.directory.Directory;
+import org.iringtools.utility.JaxbUtil;
+import org.iringtools.utility.WidgetsUtil;
+import org.iringtools.ui.widgets.tree.Tree;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DirectoryTreeController extends ActionSupport
+{
+  private static final long serialVersionUID = 1L;
+
+  private String directoryURL;
+  private Tree directoryTree;
+
+  public String execute() throws Exception
+  {
+    try
+    {
+      // Directory directory = NetUtil.get(Directory.class, directoryURL));
+      Directory directory = JaxbUtil.read(Directory.class, "C:\\Development\\java\\struts2demo\\WebContent\\WEB-INF\\data\\directory.xml");
+      directoryTree = WidgetsUtil.toTree(directory);
+    }
+    catch (Exception ex)
+    {
+      throw ex;
+    }
+
+    return SUCCESS;
+  }
+  
+  public void setDirectoryURL(String directoryURL)
+  {
+    this.directoryURL = directoryURL;
+  }
+
+  public String getDirectoryURL()
+  {
+    return directoryURL;
+  }
+
+  public void setDirectoryTree(Tree directoryTree)
+  {
+    this.directoryTree = directoryTree;
+  }
+
+  public Tree getDirectoryTree()
+  {
+    return directoryTree;
+  }
+}
