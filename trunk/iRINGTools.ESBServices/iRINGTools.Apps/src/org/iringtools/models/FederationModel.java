ec49f237cf6af47476ecb68e8bb0543c30dff0c0
rashmi.bechtel <rashmi.bechtel@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-12-13 08:14:30 +0000

    Federation Model 'save federation' changes

--- trunk/iRINGTools.ESBServices/iRINGTools.Apps/src/org/iringtools/models/FederationModel.java
+++ trunk/iRINGTools.ESBServices/iRINGTools.Apps/src/org/iringtools/models/FederationModel.java
@@ -1,6 +1,10 @@
 package org.iringtools.models;
 
+import java.lang.reflect.Method;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import org.iringtools.common.response.Response;
 import org.iringtools.refdata.federation.Federation;
@@ -15,6 +19,7 @@ import org.iringtools.ui.widgets.tree.Node;
 import org.iringtools.ui.widgets.tree.Property;
 import org.iringtools.ui.widgets.tree.Tree;
 import org.iringtools.ui.widgets.tree.TreeNode;
+import org.iringtools.utility.CommonUtility;
 import org.iringtools.utility.HttpClient;
 import org.iringtools.utility.WidgetUtil;
 import com.opensymphony.xwork2.ActionContext;
@@ -89,7 +94,8 @@ public class FederationModel
 
     Tree tree = new Tree();
     List<Node> treeNodes = tree.getTreeNodes();
-
+    //CommonUtility commUtil = new CommonUtility();
+    
     TreeNode generatorsNode = new TreeNode();
     generatorsNode.setText("ID Generators");
     generatorsNode.setId("idGenerator");
@@ -159,6 +165,18 @@ public class FederationModel
       repositoryNode.setLeaf(true);
 
       List<Property> properties = repositoryNode.getProperties();
+      /*Map map = commUtil.getRepoGetterMap();
+	  Iterator iterator = map.keySet().iterator();
+	  try{
+	      while(iterator.hasNext()){   
+	    	  String key = iterator.next().toString();
+	          properties.add(WidgetUtil.createProperty(key, 
+	        		  String.valueOf(commUtil.executeString(repository,(String) map.get(key)))));
+	      	}
+      }
+      catch(Exception e){
+    	  
+      }*/
       properties.add(WidgetUtil.createProperty("URI", repository.getUri()));
       properties.add(WidgetUtil.createProperty("Description", repository.getDescription()));
       properties.add(WidgetUtil.createProperty("Read Only", String.valueOf(repository.isIsReadOnly())));
@@ -178,39 +196,79 @@ public class FederationModel
 				//String method = httpRequest.getMethod() ; 
 				//System.out.println("Request reaching here :"+ method);
 				//String ParameterNames = "";
-				IDGenerator idgenerator;
-				Namespace namespace;
-				Repository repository;
+				//IDGenerator idgenerator;
+				//Namespace namespace;
+				//Repository repository;
 				
-				
+				Response response=null;
 				/*for(Enumeration e = httpRequest.getParameterNames();e.hasMoreElements();){
 					ParameterNames = (String)e.nextElement();
 					System.out.println(ParameterNames+":"+httpRequest.getParameter("ParameterNames"));
 				}*/
 				System.out.println("###"+httpRequest.getParameter("parentNodeID")+"###");
 				if("idGenerator".equalsIgnoreCase(httpRequest.getParameter("parentNodeID"))){
-					idgenerator = new IDGenerator();
+					IDGenerator idgenerator = new IDGenerator();
 					idgenerator.setName(httpRequest.getParameter("Name"));
 					idgenerator.setUri(httpRequest.getParameter("URI"));
 					idgenerator.setDescription(httpRequest.getParameter("Description"));
+					
+					response = httpClient.post(Response.class, "/idgenerator", idgenerator);
+					
 				}else if("namespace".equalsIgnoreCase(httpRequest.getParameter("parentNodeID"))){
-					namespace = new Namespace();
+					Namespace namespace = new Namespace();
 					namespace.setUri(httpRequest.getParameter("URI"));
 					namespace.setAlias(httpRequest.getParameter("Alias"));
 					namespace.setIsWritable(Boolean.parseBoolean(httpRequest.getParameter("Writable")));
 					namespace.setDescription(httpRequest.getParameter("Description"));
+					
+					response = httpClient.post(Response.class, "/namespace", namespace);
+					
 				}else if("repository".equalsIgnoreCase(httpRequest.getParameter("parentNodeID"))){
-					repository = new Repository();
+					Repository repository = new Repository();
 					repository.setDescription(httpRequest.getParameter("Description"));
 					repository.setUri(httpRequest.getParameter("URI"));
 					repository.setRepositoryType(httpRequest.getParameter("Repository Type"));
 					repository.setUpdateUri(httpRequest.getParameter("Update URI"));
 					repository.setIsReadOnly(Boolean.parseBoolean(httpRequest.getParameter("Read Only")));
+					
+					response = httpClient.post(Response.class, "/repository", repository);
 
+				}
+				if(response!=null){
+					System.out.println("response.getLevel().value() :" + response.getLevel().value());
+				}else{
+					System.out.println("response.getLevel().value() : null");
 				}
 		}catch(Exception e){
 			e.printStackTrace();
+			
 		}
    
   }
+  
+  private String hello="rashmi";
+/*  public String hello(){
+	  System.out.println("hello");
+	  return "elkdjdf";
+  }
+  
+  public static void main(String args[]) throws Exception{
+	  CommonUtility c = new CommonUtility();
+	  Map map = c.getRepoGetterMap();
+	  Iterator iterator = map.keySet().iterator();
+      while(iterator.hasNext()){   
+    	  String key = iterator.next().toString();
+          System.out.println(key+":"+(String) map.get(key));
+          properties.add(WidgetUtil.createProperty(key, (String) map.get(key)));
+      }
+
+	 }*/
+
+public String getHello() {
+	return hello;
+}
+
+public void setHello(String hello) {
+	this.hello = hello;
+}
 }
