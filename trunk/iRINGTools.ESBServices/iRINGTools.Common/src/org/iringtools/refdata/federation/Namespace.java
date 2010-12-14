fc0465f298555058a2add439aa3c9f750491e6f1
rashmi.bechtel <rashmi.bechtel@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-12-14 07:51:42 +0000

    Added 'id' in federation.xsd

--- trunk/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/refdata/federation/Namespace.java
+++ trunk/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/refdata/federation/Namespace.java
@@ -17,6 +17,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="uri" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="alias" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="isWritable" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
@@ -32,6 +33,7 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "Namespace", propOrder = {
+    "id",
     "uri",
     "alias",
     "isWritable",
@@ -41,6 +43,8 @@ import javax.xml.bind.annotation.XmlType;
 public class Namespace {
 
     @XmlElement(required = true)
+    protected String id;
+    @XmlElement(required = true)
     protected String uri;
     @XmlElement(required = true)
     protected String alias;
@@ -48,6 +52,30 @@ public class Namespace {
     @XmlElement(required = true)
     protected String description;
     protected int idGenerator;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setId(String value) {
+        this.id = value;
+    }
 
     /**
      * Gets the value of the uri property.
