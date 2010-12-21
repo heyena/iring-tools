c958f42fa2375b4d5d4f691fec8dd670f6c962a2
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-12-21 12:50:52 +0000

    Related Items displayed on the new Tab

--- trunk/iRINGTools.ESBServices/ExchangeManager/resources/js/NavigationPanel.js
+++ trunk/iRINGTools.ESBServices/ExchangeManager/resources/js/NavigationPanel.js
@@ -12,15 +12,14 @@ dataGrid: null,
 url: null,
 relatedClassArr:null,
 enableTabScroll: true,
-listView:null,
-
+identifier:0,
+refClassIdentifier:0,
   /**
   * initComponent
   * @protected
   */
 initComponent: function () {
-
-		   this.addEvents({
+this.addEvents({
 next: true,
 prev: true
 		   });
@@ -32,45 +31,44 @@ prev: true
 		   var filterSet = eval(this.configData.filterSet);
 		   var pageSize = parseInt(this.configData.pageSize); 
 		   var sortBy = this.configData.sortBy
-						var sortOrder = this.configData.sortOrder
-
-										var filters = new Ext.ux.grid.GridFilters({
+		   var sortOrder = this.configData.sortOrder
+		   var filters = new Ext.ux.grid.GridFilters({
 			// encode and local configuration options defined previously for easier reuse
-encode: true, // json encode the filter query
-remotesort: true, // json encode the filter query
-local: false,   // defaults to false (remote filtering)
-filters: filterSet
+			encode: true, // json encode the filter query
+			remotesort: true, // json encode the filter query
+			local: false,   // defaults to false (remote filtering)
+			filters: filterSet
 		   });
 
+		   /*
 		   this.relatedClassArr = new Array();
-
 		   if (this.configData.relatedClasses != undefined) {			
 			   for(var i=0; i < this.configData.relatedClasses.length; i++) {
 				   var key = this.configData.relatedClasses[i].identifier;
 				   var text = this.configData.relatedClasses[i].text;
 				   this.relatedClassArr[i] = text;
 			   }
-		   }
+		   }*/
 
 		// build the header first
-	// send the request to generate the arraystore
-		   var proxy = new Ext.data.HttpProxy({
+// send the request to generate the arraystore
+var proxy = new Ext.data.HttpProxy({
 api: {
 read: new Ext.data.Connection({ url: this.url, method: 'POST', timeout: 120000 }),
 create: null,
 update: null,
 destroy: null
-	 }
-		   });
+}
+});
 
-		   var reader = new Ext.data.JsonReader({
+var reader = new Ext.data.JsonReader({
 totalProperty: 'total',
 successProperty: 'success',
 root: 'data',
 fields: fieldList
-		   });
+});
 
-		   var store = new Ext.data.Store({
+var store = new Ext.data.Store({
 autoLoad: true,
 proxy: proxy,
 remoteSort: true,
@@ -79,21 +77,17 @@ sortInfo: { field: sortBy, direction: sortOrder },
 autoLoad: {
 params: {
 start: 0, 
-limit: pageSize/*,
-      		identifier:identifier,
-      		refClassIdentifier:refClassIdentifier
-      		*/
-		}
-		  },
+limit: pageSize,
+identifier:this.identifier,
+refClassIdentifier:this.refClassIdentifier
+}},
 baseParams: {
-		/*
-        'identifier':identifier,
-        'refClassIdentifier':refClassIdentifier
-        */
-		  }
-		   });
+        'identifier':this.identifier,
+        'refClassIdentifier':this.refClassIdentifier
+}
+});
 
-		   this.dataGrid = new Ext.grid.GridPanel({
+this.dataGrid = new Ext.grid.GridPanel({
 store: store,
 columns: headerList,
 stripeRows: true,      
@@ -108,26 +102,50 @@ minColumnWidth: 100,
 columnLines: true,
 classObjName: classObjName,
 enableColumnMove: false,
-bbar: new Ext.PagingToolbar({
+bbar:new Ext.PagingToolbar({
 pageSize: pageSize,
 store: store,
 displayInfo: true,
 autoScroll: true,
 plugins: [filters]
-	   })
-		   });
+})
+});
 
 
+/*if(!this.dataGrid.getBottomToolbar()){
+var ptb = new Ext.PagingToolbar({
+pageSize: pageSize,
+store: store,
+displayInfo: true,
+autoScroll: true,
+plugins: [filters]
+});
+this.dataGrid.add(ptb);
+this.dataGrid.doLayout();
+}else{
+	alert(1)
+			
+	this.dataGrid.remove(this.dataGrid.bbar);
+			
+}
+*/
 
-		   this.dataGrid.on('beforerender',this.beforeRender,this);
-		   this.dataGrid.on('cellclick',this.onCellClick,this);
-
-
-		   this.items = [{
+   this.dataGrid.on('beforerender',this.beforeRender,this);
+   this.dataGrid.on('cellclick',this.onCellClick,this);
+   //alert(this.dataGrid.classObjName)
+// for related items we don't require the bbar
+   if(this.identifier!=0 && this.refClassIdentifier!=0){
+	   this.dataGrid.getBottomToolbar().hide();
+	   //this.dataGrid.classObjName=this.identifier;
+   }
+this.items = [{
+//title:this.dataGrid.classObjName,
 title:'Detail Grid View',
 items:[this.dataGrid],
 layout:'fit'
-		   }];
+}];
+
+
 
 	//this.tbar = this.buildToolbar();
 
@@ -171,8 +189,8 @@ onCellClick: function (grid,rowIndex,columnIndex,e) {
 			   for(var i=3; i<cm.getColumnCount();i++){
 				   fieldHeader= grid.getColumnModel().getColumnHeader(i); // Get field name
 				   fieldValue= record.get(grid.getColumnModel().getDataIndex(i))
-							   tempArr= Array(fieldHeader,fieldValue)
-										rowDataArr.push(tempArr)
+				   tempArr= Array(fieldHeader,fieldValue)
+				   rowDataArr.push(tempArr)
 			   }
 			   var filedsVal_ = '[{"name":"Property"},{"name":"Value"}]';
 			   var columnsData_ ='[{"id":"Property","header":"Property","width":144,"sortable":"true","dataIndex":"Property"},{"id":"Value","header":"Value","width":144,"sortable":"true","dataIndex":"Value"}]';
@@ -181,115 +199,145 @@ onCellClick: function (grid,rowIndex,columnIndex,e) {
 			   var pColumnData = eval(columnsData_);
 		  // create the data store
 			   var pStore = new Ext.data.ArrayStore({
-fields: pfiledsVal
+				fields: pfiledsVal
 			   });
 			   pStore.loadData(prowData);
 
-		// create the Grid
-			   var grid_class_properties = new Ext.grid.GridPanel({
-store: pStore,
-columns: pColumnData,
-stripeRows: true,
-height: 460,
-autoSizeColumns: true,
-autoSizeGrid: true,
-AllowScroll : true,
-minColumnWidth: 100,
-columnLines: true,
-enableColumnMove: false
-});
+				// create the Grid
+				var grid_class_properties = new Ext.grid.GridPanel({
+				store: pStore,
+				columns: pColumnData,
+				stripeRows: true,
+				height: 460,
+				autoSizeColumns: true,
+				autoSizeGrid: true,
+				AllowScroll : true,
+				minColumnWidth: 100,
+				columnLines: true,
+				enableColumnMove: false
+				});
 
 
 
-var store = new Ext.data.JsonStore({
-data: eval(this.configData.relatedClasses[rowIndex].label),
-fields: ['name','reference','identifier']
-});
+			var store = new Ext.data.JsonStore({
+			data: eval(this.configData.relatedClasses[rowIndex].label),
+			fields: ['name','reference','identifier','scopeId','exchangeID']
+			});
 
-var listView = new Ext.list.ListView({
-	store: store,
-	hideHeaders:true,
-	singleSelect: true,
-	emptyText: 'No Items to display',
-	reserveScrollOffset: true,
-	columns: [{header: 'Class Name',dataIndex:'name'},{header: 'reference',hidden: true},{header: 'identifier',hidden:true}]
-});
+			var navPanel = this;
+			var listView = new Ext.list.ListView({
+				store: store,
+				hideHeaders:true,
+				singleSelect: true,
+				emptyText: 'No Items to display',
+				reserveScrollOffset: true,
+				columns: [{header: 'Class Name',dataIndex:'name'},{header: 'reference',hidden: true},{header: 'scopeId',hidden:true},{header: 'identifier',hidden:true},{header: 'exchangeID',hidden:true}]
+			});
 
-listView.on('click', function( dataView, index, node, e ) {
-	//**** alert(this.configData.relatedClasses[rowIndex].identifier)
-	alert(dataView.store.data.items[index].data.identifier)
-	alert(dataView.store.data.items[index].data.reference)
-	alert(dataView.store.data.items[index].data.name)
+			listView.on('click', function( dataView, index, node, e ) {
 
-	/*
-		check-tree.js file: function displayRleatedClassGrid(refClassIdentifier,dtoIdentifier,relatedClassName) {}
-		Send the request to get the grid details
-		built the gridPanel
-		add this gp to new Panel
-		add this Panel to the new tab
-	*/
+			//**** alert(this.configData.relatedClasses[rowIndex].identifier)
+			var dtoIdentifier = dataView.store.data.items[index].data.identifier;
+			var refClassIdentifier = dataView.store.data.items[index].data.reference;
+			var relatedClassName = dataView.store.data.items[index].data.name;
+			var scopeId = dataView.store.data.items[index].data.scopeId;
+			var exchangeId = dataView.store.data.items[index].data.exchangeID;
 
+			/*
+				check-tree.js file: function displayRleatedClassGrid(refClassIdentifier,dtoIdentifier,relatedClassName) {}
+				built the URI http://localhost:8888/dataObjects/getRelatedDataObjects/exchanges/12345_000/1/90003-SL/90
+				Send the request to get the grid details
+				built the gridPanel
+				add this gp to new Panel
+				add this Panel to the new tab
+			*/
+			var requestURL = 'dataObjects/getRelatedDataObjects/exchanges/'+scopeId+'/'+exchangeId+'/'+dtoIdentifier+'/'+refClassIdentifier;
 
-	/*var newTab = {
-			title: dataView.store.data.items[index].data.identifier,
-			id:this.title+'_'+dataView.store.data.items[index].data.identifier,
+			//alert('Get related list '+requestURL)
+			Ext.Ajax.request({
+			url: requestURL,
+			method: 'POST',
+			params: {},
+			success: function(result, request) {
+					var responseData = Ext.util.JSON.decode(result.responseText);
+					var pageURL='/dataObjects/getPageData/exchanges/'+scopeId+'/'+exchangeId+'/'+dtoIdentifier+'/'+refClassIdentifier;
+					//alert(refClassIdentifier+'_'+relatedClassName+'      '+dtoIdentifier)
+
+					var newTab = new ExchangeManager.NavigationPanel({
+					title: relatedClassName,
+					id:'tab_'+dtoIdentifier,
+					configData: responseData,
+					url: pageURL,
+					closable: true,
+					identifier:dtoIdentifier,
+					refClassIdentifier:refClassIdentifier
+					});
+
+					if(navPanel.get(newTab.id)==undefined){
+					navPanel.add(newTab);
+					navPanel.setActiveTab(navPanel.items.length-1);
+					}else{
+						navPanel.setActiveTab(navPanel.items.length-1);
+					}
+				 }});
+			
+	var newRelatedClassTab = {
+			title: relatedClassName,
 			//items:[classPanel],
 			html:'aaaaaaaa',
 			closable : true
 			};
-		this.add(newTab);*/
+		//navPanel.add(newRelatedClassTab);
 	 });
 
 
 
-var classPanel = new Ext.Panel({
-autoWidth: true,
-height: 500,
-forceFit:true,
-layout: 'border',
-defaults: {
-collapsible: true,
-split: true
-		   },
-items: [
-			 {
-height:100,				 
-region: 'north',
-collapsible: false,
-split: true,
-html:'<div style="float:left; width:110px;"><img src="resources/images/class-badge.png"/></div><div style="padding-top:20px;" id="identifier"><b>'+this.removeHTMLTags(IdentificationByTag_value)+'</b><br/>'+grid.classObjName+'<br/>Transfer Type : '+transferType_value+'</div>'
-			 },{
-title: 'Properties',
-region:'west',
-split: true,
-margins: '0 1 3 3',
-width: 250,
-height:900,
-minSize: 100,
-items:[grid_class_properties]
-			 },{
-title: 'Related Items',
-layout:'fit',
-collapsible: false,
-split: true,
-width: 220,
-region: 'center',
-margins: '0 3 3 0',
-layoutConfig: {animate: true,fill : false},
-					//html:this.relatedClassArr[rowIndex]
-					// html: this.configData.relatedClasses[rowIndex].text
-					//html: this.configData.relatedClasses[rowIndex].label
-items:listView
-	   }
-	   ]
-			   });
 
-			   var newTab = {
-title: IdentificationByTag_value,
-id:this.title+'_'+IdentificationByTag_value,
-items:[classPanel],
-closable : true
-			   };
+	var classPanel = new Ext.Panel({
+	autoWidth: true,
+	height: 500,
+	forceFit:true,
+	layout: 'border',
+	defaults: {
+				collapsible: true,
+				split: true
+	},
+	items: [
+				{
+				height:100,				 
+				region: 'north',
+				collapsible: false,
+				split: true,
+				html:'<div style="float:left; width:110px;"><img src="resources/images/class-badge.png"/></div><div style="padding-top:20px;" id="identifier"><b>'+this.removeHTMLTags(IdentificationByTag_value)+'</b><br/>'+grid.classObjName+'<br/>Transfer Type : '+transferType_value+'</div>'
+				},{
+				title: 'Properties',
+				region:'west',
+				split: true,
+				margins: '0 1 3 3',
+				width: 250,
+				height:900,
+				minSize: 100,
+				items:[grid_class_properties]
+				},{
+				title: 'Related Items',
+				layout:'fit',
+				collapsible: false,
+				split: true,
+				width: 220,
+				region: 'center',
+				margins: '0 3 3 0',
+				layoutConfig: {animate: true,fill : false},
+				items:listView
+				}
+		   ]
+	   });
+
+		var newTab = {
+						title: IdentificationByTag_value,
+						id:this.title+'_'+IdentificationByTag_value,
+						items:[classPanel],
+						closable : true
+					};
 
 			   if(this.get(newTab.id)==undefined){
 				   this.add(newTab);
@@ -300,7 +348,7 @@ closable : true
 		   }
 	   },
 buildToolbar: function () {
-		   return [{
+return [{
 id: "card-1",
 xtype:"tbbutton",
 tooltip:'Crum 1',
@@ -308,8 +356,8 @@ text:'1...',
 disabled: false,
 handler: this.onOpen,
 scope: this
-		   }]
-	   },
+}]
+},
 removeHTMLTags:function(strInputCode) {
   /*
 	This line is optional, it replaces escaped brackets with real ones,
@@ -330,7 +378,7 @@ onOpen: function (btn, ev) {
 		l.setActiveItem(next);
 		var t = this.getTopToolbar(); 
 
-		t.add([{
+t.add([{
 id: "card-btn-"+i,
 xtype: "tbbutton",
 tooltip: 'Crum '+i,
@@ -338,10 +386,9 @@ text: i+'...',
 disabled: false,
 handler: this.onOpen,
 scope: this
-		}]);
+}]);
 
-		t.doLayout();
-		this.fireEvent('next', this, i);
-	}  
+t.doLayout();
+	this.fireEvent('next', this, i);
+}  
 });
-
