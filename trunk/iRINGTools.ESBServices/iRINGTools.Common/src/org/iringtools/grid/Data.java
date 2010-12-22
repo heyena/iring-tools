e9365c8e3eebc70bfb33474bbecb500a767cd946
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-12-22 21:03:53 +0000

    Fixed the bug and added relatedClassName in C# code Show Exchange data in Grid Modified entity class in response class Changed the url name in xml file for exchange data

---
+++ trunk/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/grid/Data.java
@@ -0,0 +1,88 @@
+
+package org.iringtools.grid;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlType;
+import org.w3c.dom.Element;
+
+
+/**
+ * <p>Java class for Data complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="Data">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;any processContents='lax' maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Data", propOrder = {
+    "anies"
+})
+public class Data {
+
+    @SuppressWarnings("rawtypes")
+	@XmlAnyElement
+    protected HashMap hdata;
+
+    /**
+     * Gets the value of the anies property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the anies property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getAnies().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Element }
+     * 
+     * 
+     */
+    public HashMap getHdata() {
+        if (hdata == null) {
+        	hdata = new HashMap();
+        }
+        return this.hdata;
+    }
+
+    /**
+     * Sets the value of the anies property.
+     * 
+     * @param anies
+     *     allowed object is
+     *     {@link Element }
+     *     
+     */
+    public void setHdata(HashMap hdata) {
+        this.hdata = hdata;
+    }
+
+    public void setHdata(String key, String value)
+    {
+    	this.hdata.put(key, value);
+    }
+}
