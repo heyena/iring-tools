891a360fd29b8745335778cf695b038e1b099e69
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2010-12-22 21:26:26 +0000

    Fix port number in build.cmd and build script exclusion list.

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ call "%VS100COMNTOOLS%\vsvars32.bat"
 cd %~dp0%
 svn update build.xml
 taskkill /IM WebDev.WebServer40.exe
-start /B WebDev.WebServer40 /port:54321 /path:%~dp0%iRINGTools.Services
+start /B WebDev.WebServer40 /port:65432 /path:%~dp0%iRINGTools.Services
 msbuild build.xml /v:n /t:all /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
 taskkill /IM WebDev.WebServer40.exe
 pause
