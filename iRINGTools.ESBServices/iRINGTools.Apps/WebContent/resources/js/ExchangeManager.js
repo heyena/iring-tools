63d6da9c8abb69a29e418a380884e85713aebbb9
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-04 14:00:09 +0000

    directory request URI changed

--- iRINGTools.ESBServices/iRINGTools.Apps/WebContent/resources/js/ExchangeManager.js
+++ iRINGTools.ESBServices/iRINGTools.Apps/WebContent/resources/js/ExchangeManager.js
@@ -18,7 +18,7 @@ Ext.onReady(function () {
 	  width: 250,
   	  minSize: 175,
   	  maxSize: 500,
-	  url: 'exchangereader/exchangelist/1'
+	  url: 'directory'
 	});
 	
 	var contentPanel = new Ext.TabPanel({
