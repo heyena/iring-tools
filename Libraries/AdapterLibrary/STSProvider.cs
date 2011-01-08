1daabdae87693cbcde117d346fb0771c9657ddcd
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-08 15:00:04 +0000

    Various fixes from testing on Thursday

--- Libraries/AdapterLibrary/STSProvider.cs
+++ Libraries/AdapterLibrary/STSProvider.cs
@@ -1,55 +1,50 @@
 ﻿using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Web;
 using Ninject;
-using System;
-using System.Configuration;
-using System.Runtime.Serialization.Json;
-using System.Net;
-using System.IO;
-using System.Text;
 
 namespace org.iringtools.adapter.identity
 {
-    public class STSProvider : IIdentityLayer
+  public class STSProvider : IIdentityLayer
+  {
+    /// <summary>
+    /// Classes for dealing with STS for web services.
+    /// 
+    /// Requires a web operation context...wcf use only
+    /// To enable add app settings:
+    /// <add key="restTokenAddress" value will be listed shortly/>
+    /// </summary>
+    /// 
+
+    private AdapterSettings _settings = null;
+
+    [Inject]
+    public STSProvider(AdapterSettings settings)
     {
-        /// <summary>
-        /// Classes for dealing with STS for web services.
-        /// 
-        /// Requires a web operation context...wcf use only
-        /// To enable add app settings:
-        /// <add key="sso_enabled" value="true"/>
-        /// <add key="restTokenAddress" value will be listed shortly/>
-        /// </summary>
-        /// 
-
-        private AdapterSettings _settings = null;
-
-        [Inject]
-        public STSProvider(AdapterSettings settings)
-        {
-            _settings = settings;
-        }
-
-        public void Initialize()
-        {
-            bool sso_enabled = Convert.ToBoolean(ConfigurationManager.AppSettings["sso_enabled"]);
-            HttpContext context = System.Web.HttpContext.Current;
-
-            if (sso_enabled)
-            {
-                string header = context.Request.Headers["Authorization"];
-                string restTokenAddress = ConfigurationManager.AppSettings["restTokenAddress"];
-
-                DataContractJsonSerializer dictionarySerializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>));
-                WebClient client = new WebClient();
-                client.UseDefaultCredentials = true;
-
-                string xmlToken = client.DownloadString(restTokenAddress + "/JSON/DecodeOAuthHeader?header=" + header);
-                MemoryStream str = new MemoryStream(Encoding.Unicode.GetBytes(xmlToken));
-                str.Position = 0;
-                Dictionary<string, string> result = (Dictionary<string, string>)dictionarySerializer.ReadObject(str);
-                _settings["UserName"] = result.Values.ToString();
-            }
-        }
+      _settings = settings;
     }
+
+    public void Initialize()
+    {
+      HttpContext context = System.Web.HttpContext.Current;
+
+      string header = context.Request.Headers["Authorization"];
+      string restTokenAddress = _settings["STSAddress"];
+
+      DataContractJsonSerializer dictionarySerializer = new DataContractJsonSerializer(typeof(Dictionary<string, string>));
+      WebClient client = new WebClient();
+      client.UseDefaultCredentials = true;
+
+      string xmlToken = client.DownloadString(restTokenAddress + "/JSON/DecodeOAuthHeader?header=" + header);
+      MemoryStream str = new MemoryStream(Encoding.Unicode.GetBytes(xmlToken));
+      str.Position = 0;
+
+      Dictionary<string, string> result = (Dictionary<string, string>)dictionarySerializer.ReadObject(str);
+      _settings["UserName"] = result.Values.ToString();
+    }
+  }
 }
