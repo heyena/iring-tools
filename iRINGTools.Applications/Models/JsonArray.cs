3f9dc403f9edaf664f067addc299180d2eddceb6
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-18 12:21:19 +0000

    more missing files

---
+++ iRINGTools.Applications/Models/JsonArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iRINGTools.Web.Models
+{  
+  public class JsonArrayItem : Dictionary<string, string>
+  { 
+  }
+
+  public class JsonArray : List<JsonArrayItem>
+  {    
+  }
+}
