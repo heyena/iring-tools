c9ed6f9ec791961c4870b7b9f55533442aef7ac0
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-18 04:50:54 +0000

    initial move of the adapter manager classes and ext jscripts for Robertson and Neha

---
+++ iRINGTools.Applications/Scripts/iringtools/AdapterManager/ScopePanel.js
@@ -0,0 +1,19 @@
+﻿Ext.ns('AdapterManager');
+/**
+* @class AdapterManager.ScopePanel
+* @extends Panel
+* @author by Gert Jansen van Rensburg
+*/
+AdapterManager.ScopePanel = Ext.extend(Ext.Panel, {
+
+    /**
+    * initComponent
+    * @protected
+    */
+    initComponent: function () {
+
+        // super
+        AdapterManager.ScopePanel.superclass.initComponent.call(this);
+    }
+
+});
