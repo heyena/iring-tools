986ccd89541da0161a2ca8254c2a58488f53614e
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-26 21:03:32 +0000

    Build 2.00.01.2552

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.1.2544")]
-[assembly: AssemblyFileVersion("2.0.1.2544")]
+[assembly: AssemblyVersion("2.0.1.2552")]
+[assembly: AssemblyFileVersion("2.0.1.2552")]
