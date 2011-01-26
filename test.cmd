d2049b72ecd4fe170a83965415815a362f4550f3
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-01-26 18:01:33 +0000

    Added CSVDataLayer to the DataLayers branch as an example.

--- test.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-call "%VS100COMNTOOLS%\vsvars32.bat"
-cd %~dp0%
-svn update build.xml
-taskkill /IM WebDev.WebServer40.exe >> null
-start /B WebDev.WebServer40 /port:54321 /path:%~dp0%iRINGTools.Services
-msbuild build.xml /t:Test /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
-taskkill /IM WebDev.WebServer40.exe >> null
-pause
