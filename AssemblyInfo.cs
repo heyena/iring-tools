6a3633f16b7a26494b82adfa0a6b0dcf72dc93a0
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-02-14 23:34:25 +0000

    ported IDataLayer change from 2.0.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.2577")]
-[assembly: AssemblyFileVersion("2.0.0.2577")]
+[assembly: AssemblyVersion("2.1.0.2640")]
+[assembly: AssemblyFileVersion("2.1.0.2640")]
