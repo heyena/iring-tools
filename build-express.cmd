3b07b6de552c65d4a43e598149bc5375541b6f18
rpdecarl@bechtel.com <rpdecarl@bechtel.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-02-24 22:48:08 +0000

    Added Configuration Tools to express solution and build.

--- build-express.cmd
+++ build-express.cmd
@@ -1,8 +1,5 @@
 @echo off
 cd %~dp0%
 svn update build-express.xml
-taskkill /IM WebDev.WebServer40.exe
-start /B WebDev.WebServer40 /port:65432 /path:%~dp0%iRINGTools.Services
 msbuild build-express.xml /v:n /t:all /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
-taskkill /IM WebDev.WebServer40.exe
 pause
