cf08369b22bb882583fb0b8d79d589b013b9d8ce
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-01 13:28:04 +0000

    Added the ReferenceDataServices functions that we are calling in iRINGTools.Services/src/org/iringtools/services/RefDataService.java file

--- trunk/iRINGTools.ESBServices/iRINGTools.Services/src/org/iringtools/services/RefDataService.java
+++ trunk/iRINGTools.ESBServices/iRINGTools.Services/src/org/iringtools/services/RefDataService.java
@@ -1,20 +1,24 @@
 package org.iringtools.services;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.xml.bind.JAXBException;
 
 import org.apache.log4j.Logger;
+import org.ids_adi.ns.qxf.model.Qmxf;
 import org.iringtools.common.response.Response;
 import org.iringtools.refdata.federation.Federation;
 import org.iringtools.refdata.federation.IDGenerator;
 import org.iringtools.refdata.federation.Namespace;
 import org.iringtools.refdata.federation.Repository;
+import org.iringtools.refdata.response.Entity;
 import org.iringtools.services.core.RefDataProvider;
 
 @Path("/")
@@ -183,4 +187,201 @@ public class RefDataService extends AbstractService
 
     return response;
   }
+  
+  @GET
+  @Path("/classes/{id}/label")
+  public String getClassLabel(@PathParam("id") String id)
+ {
+	  String classLabel = "";
+	  try
+	    {
+	      initService();
+		  RefDataProvider refDataProvider = new RefDataProvider(settings);
+		  classLabel = refDataProvider.getClassLabel(id);	      
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	    return classLabel;
+   }
+
+  @GET
+  @Path("/classes/{id}/{namespace}")
+
+  public Qmxf getClass(@PathParam("id") String id,@PathParam("namespace") String namespace)
+  {
+   
+	  Qmxf qmxf= null;
+	    try
+	    {
+	      initService();
+	      RefDataProvider refDataProvider = new RefDataProvider(settings);
+	      qmxf = refDataProvider.GetClass(id,namespace);
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	    return qmxf;
+  }
+  
+  @GET
+  @Path("/templates/{id}")
+  public Qmxf getTemplate(@PathParam("id") String id)
+  {
+	  Qmxf qmxf= null;
+	    try
+	    {
+	      initService();
+	      RefDataProvider refDataProvider = new RefDataProvider(settings);
+	      qmxf = refDataProvider.GetTemplate(id);
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	    return qmxf;
+  }
+
+  @POST
+  @Path("/templates")
+  public Response PostTemplate(Qmxf qmxf)
+  {
+	  Response response=null;
+	    try
+	    {
+	      initService();
+	      RefDataProvider refDataProvider = new RefDataProvider(settings);
+	      response= refDataProvider.PostTemplate(qmxf);
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	  return response;
+  }
+
+  @POST
+  @Path("/classes")
+  public Response PostClass(Qmxf qmxf)
+  {
+	  Response response=null;
+	    try
+	    {
+	      initService();
+	      RefDataProvider refDataProvider = new RefDataProvider(settings);
+	      response= refDataProvider.PostClass(qmxf);
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	  return response;
+  }
+
+  /*public List<Entity> getSuperClasses(String id)
+  {
+  }
+
+  public List<Entity> getAllSuperClasses(String id)
+  {
+  }
+
+  public List<Entity> getSubClasses(String id)
+  {
+  }
+
+  public List<Entity> getClassTemplates(String id)
+  {
+	  	  
+  }  
+
+  @GET
+  @Path("/search/{query}/{start}/{limit}/reset")
+
+  public RefDataEntities SearchPageReset(String query, String start, String limit)
+   {
+     OutgoingWebResponseContext context = WebOperationContext.Current.OutgoingResponse;
+     context.ContentType = "application/xml";
+
+     int startIdx = 0;
+     int pageLimit = 0;
+     int.TryParse(start, out startIdx);
+     int.TryParse(limit, out pageLimit);
+
+     return _referenceDataProvider.SearchPageReset(query, startIdx, pageLimit);
+   }
+
+  @GET
+  @Path("/repositories")
+  public List<Repository> getRepositories()
+  {
+	  try
+	    {
+	      initService();
+		  RefDataProvider refDataProvider = new RefDataProvider(settings);
+		  return refDataProvider.getRepositories();	      
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+   }
+  
+  @GET
+  @Path("/repositories/{query}")
+  public List<Entity> find(@PathParam("query") String query)
+  {
+	  String classLabel = "";
+	  try
+	    {
+	      initService();
+		  RefDataProvider refDataProvider = new RefDataProvider(settings);
+		  classLabel = refDataProvider.find(query);
+	    }
+	    catch (Exception ex)
+	    {
+	      logger.error("Error getting federation information: " + ex);
+	    }
+	    return classLabel;
+	    
+  }
+
+  @GET
+  @Path("/search/{query}")
+  public RefDataEntities search(String query)
+  {
+     OutgoingWebResponseContext context = WebOperationContext.Current.OutgoingResponse;
+     context.ContentType = "application/xml";
+     return _referenceDataProvider.Search(query);
+   }
+
+  @GET
+  @Path("/search/{query}/{start}/{limit}")
+  public RefDataEntities SearchPage(String query, String start, String limit)
+   {
+     OutgoingWebResponseContext context = WebOperationContext.Current.OutgoingResponse;
+     context.ContentType = "application/xml";
+
+
+     int startIdx = 0;
+     int pageLimit = 0;
+     int.TryParse(start, out startIdx);
+     int.TryParse(limit, out pageLimit);
+
+     return _referenceDataProvider.SearchPage(query, startIdx, pageLimit);
+   }
+
+  @GET
+  @Path("/search/{query}/reset")
+  public RefDataEntities SearchReset(String query)
+   {
+     OutgoingWebResponseContext context = WebOperationContext.Current.OutgoingResponse;
+     context.ContentType = "application/xml";
+
+     return _referenceDataProvider.SearchReset(query);
+   }*/
+  
 }
+
