c5b791988ff6b98755fdd85d659ffb4616468dbd
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-03 19:56:26 +0000

    Switching to express builds and cleaning up.

--- build-express.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd %~dp0%
-@set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
-svn update build-express.xml
-msbuild build-express.xml /t:All /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
-pause
