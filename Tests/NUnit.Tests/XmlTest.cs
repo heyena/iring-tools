40766c17622dbcf9f30b3ca2b64f3186a44032af
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-03 10:04:06 +0000

    NUnit benchmark files

--- Tests/NUnit.Tests/XmlTest.cs
+++ Tests/NUnit.Tests/XmlTest.cs
@@ -27,7 +27,7 @@ namespace NUnit.Tests
       _settings["GraphName"] = "Lines";
       _settings["Identifier"] = "66015-O";
       _settings["TestMode"] = "UseFiles"; //UseFiles/WriteFiles
-
+      _settings["GraphBaseUri"] = "http://www.example.com/"; 
       _baseDirectory = Directory.GetCurrentDirectory();
       _baseDirectory = _baseDirectory.Substring(0, _baseDirectory.LastIndexOf("\\Bin"));
       _settings["BaseDirectoryPath"] = _baseDirectory;
