b0e7d049464e6ba8fdf2349358d037b424006a41
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-08 17:57:34 +0000

    Build script tweaks.

--- javabuild.cmd
+++ javabuild.cmd
@@ -3,15 +3,10 @@ setlocal
 set basedir=%~dp0
 set javabasedir=%basedir%iRINGTools.ESBServices\
 set deploymentdir=%basedir%Deployment\
-set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
 
 echo Updating build script ...
-cd %basedir%
+cd %javabasedir%
 svn update build.xml
-pause
-
-echo Building C# projects ...
-msbuild build.xml /t:All /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
 pause
 
 echo Building Java projects ...
