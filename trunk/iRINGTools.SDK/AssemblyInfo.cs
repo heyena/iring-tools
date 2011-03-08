3a3e4b1bb673d870d6da3d9456d86b7ff09d5e78
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-08 01:53:03 +0000

    Build 2.1.0.2765 (uses express style build)

--- trunk/iRINGTools.SDK/AssemblyInfo.cs
+++ trunk/iRINGTools.SDK/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.3.2759")]
-[assembly: AssemblyFileVersion("2.0.3.2759")]
+[assembly: AssemblyVersion("2.1.0.2765")]
+[assembly: AssemblyFileVersion("2.1.0.2765")]
