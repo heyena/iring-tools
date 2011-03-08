ad1eb09c8406a8450b3666f0f7e5fa9cec47c23c
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-08 17:57:34 +0000

    Build script tweaks.

--- trunk/test.cmd
+++ trunk/test.cmd
@@ -1,6 +1,13 @@
 @echo off
-cd %~dp0%
-@set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
-svn update build-express.xml
-msbuild build-express.xml /t:Test /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
+setlocal
+set basedir=%~dp0
+set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
+
+echo Updating build script ...
+cd %basedir%
+svn update build.xml
+pause
+
+echo Running C# tests ...
+msbuild build.xml /t:Test /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
 pause
