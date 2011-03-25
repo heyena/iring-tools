cbb11be431133a9208c6500b4062cd619ae67e61
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-25 14:02:57 +0000

    Build 2.0.4.0

--- branches/2.0.x/iRINGTools.SDK/AssemblyInfo.cs
+++ branches/2.0.x/iRINGTools.SDK/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.3.2875")]
-[assembly: AssemblyFileVersion("2.0.3.2875")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
