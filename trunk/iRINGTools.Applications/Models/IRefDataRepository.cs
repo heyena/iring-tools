ea0126285e9a87da88c90f043c813a2094966415
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-29 22:28:41 +0000

    added missed files

---
+++ trunk/iRINGTools.Applications/Models/IRefDataRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using org.iringtools.library;
+
+namespace iRINGTools.Web.Models
+{
+  public interface IRefDataRepository
+  {
+
+    RefDataEntities Search(string query);
+
+    RefDataEntities Search(string query, string start, string limit);
+
+    RefDataEntities SearchReset(string query);
+
+    Entity GetClassLabel(string classId);
+
+    Entities GetSuperClasses(string classId);
+
+    Entities GetSubClasses(string classId);
+
+    Entities GetClassTemplates(string classId);
+  }
+}
