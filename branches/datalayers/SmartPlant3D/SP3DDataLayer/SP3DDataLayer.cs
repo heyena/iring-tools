7b212e75bda24b73edb3b67dfe0c9ea8b3a74c4f
neha11.b@tcs.com <neha11.b@tcs.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-30 07:07:11 +0000

    Project added for SmartPlant-3D

---
+++ branches/datalayers/SmartPlant3D/SP3DDataLayer/SP3DDataLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+using Ciloci.Flee;
+using log4net;
+using Ninject;
+using org.iringtools.adapter;
+using org.iringtools.library;
+using org.iringtools.utility;
+
+namespace SmartPlant3D
+{
+    public class Class1
+    {
+    }
+}
---
+++ branches/datalayers/SmartPlant3D/SPP3DDataLayer.NUnit/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using org.iringtools.adapter;
+using org.iringtools.library;
+using org.iringtools.utility;
+using StaticDust.Configuration;
+using Ninject;
+using Ninject.Extensions.Xml;
+
+namespace SPP3DDataLayer.NUnit
+{
+    public class Test
+    {
+    }
+}
