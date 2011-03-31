932b30ebdcf709e1efc8323940a5bfa5b6ab6295
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-31 15:48:24 +0000

    added version to SDK.

---
+++ web/sdk.iringsandbox.org/js/sendRequest.js
@@ -0,0 +1,44 @@
+function getXmlHttpConnection() {
+  var xmlHttp = null;
+
+  try {
+    xmlHttp = new XMLHttpRequest();  // Firefox, Opera 8.0+, Safari
+  }
+  catch(e) {
+    try {
+      xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');  // IE
+    }
+    catch(e) {
+      try {
+        xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+      }
+      catch(e) {
+        return false;
+      }
+    }
+  }
+
+  return xmlHttp;
+}
+
+function sendRequest(url, callback) {
+  var i = url.indexOf('?');
+  var action = url.substring(0, i);
+  var params = url.substring(i + 1);    
+  var xmlHttp = getXmlHttpConnection();
+    
+  if (xmlHttp != null) {
+    xmlHttp.open('GET', url, true);
+    xmlHttp.send();
+    
+    var interval = window.setInterval(
+      function() {
+        if (xmlHttp.readyState == 4) {
+          window.clearInterval(interval);
+		  callback(xmlHttp.responseText);
+          xmlHttp = null;
+        }
+      }
+    );
+  }
+}
