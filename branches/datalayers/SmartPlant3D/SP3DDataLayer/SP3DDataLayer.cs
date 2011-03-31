84018d0bcecc7b3348b8c7875345b6e84273b558
neha11.b@tcs.com <neha11.b@tcs.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-03-31 11:48:01 +0000

    Add Create and Read Method.

--- branches/datalayers/SmartPlant3D/SP3DDataLayer/SP3DDataLayer.cs
+++ branches/datalayers/SmartPlant3D/SP3DDataLayer/SP3DDataLayer.cs
@@ -16,6 +16,8 @@ namespace iRINGTools.SDK.SP3DDataLayer
 {
     public class SP3DDataLayer : BaseDataLayer, IDataLayer2
     {
+        private List<IDataObject> _dataObjects = null;
+
         [Inject]
         public SP3DDataLayer(AdapterSettings settings, IKernel kernel)
         {
@@ -47,6 +49,15 @@ namespace iRINGTools.SDK.SP3DDataLayer
             try
             {
                 List<string> identifiers = new List<string>();
+
+                //NOTE: pageSize of 0 indicates that all rows should be returned.
+                IList<IDataObject> dataObjects = Get(objectType, filter, 0, 0);
+
+                foreach (IDataObject dataObject in dataObjects)
+                {
+                    identifiers.Add((string)dataObject.GetPropertyValue("Tag"));
+                }
+
                 return identifiers;
             }
             catch (Exception ex)
@@ -68,17 +79,68 @@ namespace iRINGTools.SDK.SP3DDataLayer
         public override Response Post(IList<IDataObject> dataObjects)
         {
             Response response = new Response();
+            string objectType = String.Empty;
 
-            return response;
+            if (dataObjects == null || dataObjects.Count == 0)
+            {
+                Status status = new Status();
+                status.Level = StatusLevel.Warning;
+                status.Messages.Add("Nothing to update.");
+                response.Append(status);
+                return response;
+            }
 
+            try
+            {
+                objectType = ((GenericDataObject)dataObjects.FirstOrDefault()).ObjectType;
 
+                LoadDataDictionary(objectType);
+
+                IList<IDataObject> existingDataObjects = LoadDataObjects(objectType);
+
+                foreach (IDataObject dataObject in dataObjects)
+                {
+                    IDataObject existingDataObject = null;
+
+                    string identifier = GetIdentifier(dataObject);
+                    var predicate = FormKeyPredicate(identifier);
+
+                    if (predicate != null)
+                    {
+                        existingDataObject = existingDataObjects.AsQueryable().Where(predicate).FirstOrDefault();
+                    }
+
+                    if (existingDataObject != null)
+                    {
+                        existingDataObjects.Remove(existingDataObject);
+                    }
+
+                    existingDataObjects.Add(dataObject);
+                }
+
+                response = SaveDataObjects(objectType, existingDataObjects);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error in Post: " + ex);
+
+                throw new Exception(
+                  "Error while posting dataObjects of type [" + objectType + "].",
+                  ex
+                );
+            }
         }
 
         public override Response Delete(string objectType, IList<string> identifiers)
         {
             // Not gonna do it. Wouldn't be prudent.
             Response response = new Response();
-
+            Status status = new Status();
+            status.Level = StatusLevel.Error;
+            status.Messages.Add("Delete not supported by the SP3D DataLayer.");
+            response.Append(status);
             return response;
         }
 
@@ -86,7 +148,10 @@ namespace iRINGTools.SDK.SP3DDataLayer
         {
             // Not gonna do it. Wouldn't be prudent with a filter either.
             Response response = new Response();
-
+            Status status = new Status();
+            status.Level = StatusLevel.Error;
+            status.Messages.Add("Delete not supported by the SP3D DataLayer.");
+            response.Append(status);
             return response;
         }
 
@@ -94,14 +159,314 @@ namespace iRINGTools.SDK.SP3DDataLayer
         {
             DataDictionary dataDictionary = new DataDictionary();
 
+            LoadConfiguration();
+
+            List<DataObject> dataObjects = new List<DataObject>();
+            foreach (XElement commodity in _configuration.Elements("commodity"))
+            {
+                string name = commodity.Element("name").Value;
+
+                DataObject dataObject = new DataObject
+                {
+                    objectName = name,
+                    keyDelimeter = "_",
+                };
+
+                List<KeyProperty> keyProperties = new List<KeyProperty>();
+                List<DataProperty> dataProperties = new List<DataProperty>();
+
+                foreach (XElement attribute in commodity.Element("attributes").Elements("attribute"))
+                {
+                    bool isKey = false;
+                    if (attribute.Attribute("isKey") != null)
+                    {
+                        Boolean.TryParse(attribute.Attribute("isKey").Value, out isKey);
+                    }
+
+                    string attributeName = attribute.Attribute("name").Value;
+
+                    DataType dataType = DataType.String;
+                    Enum.TryParse<DataType>(attribute.Attribute("dataType").Value, out dataType);
+
+                    int dataLength = 0;
+                    if (DataDictionary.IsNumeric(dataType))
+                    {
+                        dataLength = 16;
+                    }
+                    else
+                    {
+                        dataLength = 255;
+                    }
+
+                    DataProperty dataProperty = new DataProperty
+                    {
+                        propertyName = attributeName,
+                        dataType = dataType,
+                        dataLength = dataLength,
+                        isNullable = true,
+                        showOnIndex = false,
+                    };
+
+                    if (isKey)
+                    {
+                        dataProperty.isNullable = false;
+                        dataProperty.showOnIndex = true;
+
+                        KeyProperty keyProperty = new KeyProperty
+                        {
+                            keyPropertyName = attributeName,
+                        };
+
+                        keyProperties.Add(keyProperty);
+                    }
+
+                    dataProperties.Add(dataProperty);
+                }
+
+                dataObject.keyProperties = keyProperties;
+                dataObject.dataProperties = dataProperties;
+
+                dataObjects.Add(dataObject);
+            }
+
+            dataDictionary.dataObjects = dataObjects;
+
             return dataDictionary;
+        }
+
+        private void LoadConfiguration()
+        {
+            if (_configuration == null)
+            {
+                string uri = String.Format(
+                    "{0}Configuration.{1}.xml",
+                    _settings["XmlPath"],
+                    _settings["ApplicationName"]
+                );
+
+                XDocument configDocument = XDocument.Load(uri);
+                _configuration = configDocument.Element("configuration");
+            }
+        }
+
+        private IList<IDataObject> LoadDataObjects(string objectType)
+        {
+            try
+            {
+                IList<IDataObject> dataObjects = new List<IDataObject>();
+
+                //Get Path from Scope.config ({project}.{app}.config)
+                string path = String.Format(
+                    "{0}{1}\\{2}.csv",
+                     _settings["BaseDirectoryPath"],
+                    _settings["SP3DFolderPath"],
+                    objectType
+                );
+
+                IDataObject dataObject = null;
+                TextReader reader = new StreamReader(path);
+                while (reader.Peek() >= 0)
+                {
+                    string csvRow = reader.ReadLine();
+
+                    dataObject = FormDataObject(objectType, csvRow);
+
+                    if (dataObject != null)
+                        dataObjects.Add(dataObject);
+                }
+                reader.Close();
+
+                return dataObjects;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error in LoadDataObjects: " + ex);
+                throw new Exception("Error while loading data objects of type [" + objectType + "].", ex);
+            }
+        }
+
+        private IDataObject FormDataObject(string objectType, string csvRow)
+        {
+            try
+            {
+                IDataObject dataObject = new GenericDataObject
+                {
+                    ObjectType = objectType,
+                };
+
+                XElement commodityElement = GetCommodityConfig(objectType);
+
+                if (!String.IsNullOrEmpty(csvRow))
+                {
+                    IEnumerable<XElement> attributeElements = commodityElement.Element("attributes").Elements("attribute");
+
+                    string[] csvValues = csvRow.Split(',');
+
+                    int index = 0;
+                    foreach (var attributeElement in attributeElements)
+                    {
+                        string name = attributeElement.Attribute("name").Value;
+                        string dataType = attributeElement.Attribute("dataType").Value.ToLower();
+                        string value = csvValues[index++].Trim();
+
+                        // if data type is not nullable, make sure it has a value
+                        if (!(dataType.EndsWith("?") && value == String.Empty))
+                        {
+                            if (dataType.Contains("bool"))
+                            {
+                                if (value.ToUpper() == "TRUE" || value.ToUpper() == "YES")
+                                {
+                                    value = "1";
+                                }
+                                else
+                                {
+                                    value = "0";
+                                }
+                            }
+                            else if (value == String.Empty && (
+                                     dataType.StartsWith("int") ||
+                                     dataType == "double" ||
+                                     dataType == "single" ||
+                                     dataType == "float" ||
+                                     dataType == "decimal"))
+                            {
+                                value = "0";
+                            }
+                        }
+
+                        dataObject.SetPropertyValue(name, value);
+                    }
+                }
+
+                return dataObject;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error in FormDataObject: " + ex);
+
+                throw new Exception(
+                  "Error while forming a dataObject of type [" + objectType + "] from SPPID.",
+                  ex
+                );
+            }
+        }
+
+        private XElement GetCommodityConfig(string objectType)
+        {
+            if (_configuration == null)
+            {
+                LoadConfiguration();
+            }
+
+            XElement commodityConfig = _configuration.Elements("commodity").Where(o => o.Element("name").Value == objectType).First();
+
+            return commodityConfig;
+        }
+
+        private Response SaveDataObjects(string objectType, IList<IDataObject> dataObjects)
+        {
+            try
+            {
+                Response response = new Response();
+
+                // Create data object directory in case it does not exist
+                Directory.CreateDirectory(_settings["SP3DFolderPath"]);
+
+                string path = String.Format(
+                 "{0}{1}\\{2}.csv",
+                   _settings["BaseDirectoryPath"],
+                  _settings["SP3DFolderPath"],
+                  objectType
+                );
+
+                //TODO: Need to update file, not replace it!
+                TextWriter writer = new StreamWriter(path);
+
+                foreach (IDataObject dataObject in dataObjects)
+                {
+                    Status status = new Status();
+
+                    try
+                    {
+                        string identifier = GetIdentifier(dataObject);
+                        status.Identifier = identifier;
+
+                        List<string> csvRow = FormCSVRow(objectType, dataObject);
+
+                        writer.WriteLine(String.Join(", ", csvRow.ToArray()));
+                        status.Messages.Add("Record [" + identifier + "] has been saved successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        status.Level = StatusLevel.Error;
+
+                        string message = String.Format(
+                          "Error while posting dataObject [{0}]. {1}",
+                          dataObject.GetPropertyValue("Tag"),
+                          ex.ToString()
+                        );
+
+                        status.Messages.Add(message);
+                    }
+
+                    response.Append(status);
+                }
+
+                writer.Close();
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error in LoadDataObjects: " + ex);
+                throw new Exception("Error while loading data objects of type [" + objectType + "].", ex);
+            }
+        }
+
+        private List<string> FormCSVRow(string objectType, IDataObject dataObject)
+        {
+            try
+            {
+                List<string> csvRow = new List<string>();
+
+                XElement commodityElement = GetCommodityConfig(objectType);
+
+                IEnumerable<XElement> attributeElements = commodityElement.Element("attributes").Elements("attribute");
+
+                foreach (var attributeElement in attributeElements)
+                {
+                    string name = attributeElement.Attribute("name").Value;
+                    string value = Convert.ToString(dataObject.GetPropertyValue(name));
+                    csvRow.Add(value);
+                }
+
+                return csvRow;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error in FormSPPIDRow: " + ex);
+
+                throw new Exception(
+                  "Error while forming a CSV row of type [" + objectType + "] from a DataObject.",
+                  ex
+                );
+            }
         }
 
         public override IList<IDataObject> Get(string objectType, IList<string> identifiers)
         {
             try
             {
-                IList<IDataObject> _dataObjects = new List<IDataObject>();
+                LoadDataDictionary(objectType);
+
+                IList<IDataObject> allDataObjects = LoadDataObjects(objectType);
+
+                var expressions = FormMultipleKeysPredicate(identifiers);
+
+                if (expressions != null)
+                {
+                    _dataObjects = allDataObjects.AsQueryable().Where(expressions).ToList();
+                }
 
                 return _dataObjects;
             }
@@ -116,7 +481,30 @@ namespace iRINGTools.SDK.SP3DDataLayer
         {
             try
             {
-                IList<IDataObject> _dataObjects = new List<IDataObject>();
+                LoadDataDictionary(objectType);
+
+                IList<IDataObject> allDataObjects = LoadDataObjects(objectType);
+
+                // Apply filter
+                if (filter != null && filter.Expressions != null && filter.Expressions.Count > 0)
+                {
+                    var predicate = filter.ToPredicate(_dataObjectDefinition);
+
+                    if (predicate != null)
+                    {
+                        _dataObjects = allDataObjects.AsQueryable().Where(predicate).ToList();
+                    }
+                }
+
+                if (filter != null && filter.OrderExpressions != null && filter.OrderExpressions.Count > 0)
+                {
+                    throw new NotImplementedException("OrderExpressions are not supported by the SPPID DataLayer.");
+                }
+
+                //Page and Sort The Data
+                if (pageSize > _dataObjects.Count())
+                    pageSize = _dataObjects.Count();
+                _dataObjects = _dataObjects.GetRange(startIndex, pageSize);
 
                 return _dataObjects;
             }
--- branches/datalayers/SmartPlant3D/SPP3DDataLayer.NUnit/Test.cs
+++ branches/datalayers/SmartPlant3D/SPP3DDataLayer.NUnit/Test.cs
@@ -20,7 +20,7 @@ namespace iRINGTools.SDK.SP3DDataLayer
         private IKernel _kernel = null;
         private NameValueCollection _settings;
         private AdapterSettings _adapterSettings;
-        private IDataLayer2 _sp3dDataLayer;
+        private SP3DDataLayer _sp3dDataLayer;
 
         public SP3DDataLayerTest()
         {
@@ -29,7 +29,7 @@ namespace iRINGTools.SDK.SP3DDataLayer
             _settings["XmlPath"] = @".\12345_000\";
             _settings["ProjectName"] = "12345_000";
             _settings["ApplicationName"] = "SP3D";
-
+            
             _baseDirectory = Directory.GetCurrentDirectory();
             _baseDirectory = _baseDirectory.Substring(0, _baseDirectory.LastIndexOf("\\bin"));
             _settings["BaseDirectoryPath"] = _baseDirectory;
@@ -38,7 +38,7 @@ namespace iRINGTools.SDK.SP3DDataLayer
             _adapterSettings = new AdapterSettings();
             _adapterSettings.AppendSettings(_settings);
 
-            string appSettingsPath = String.Format("{0}12345_000.SPP3D.config",
+            string appSettingsPath = String.Format("{0}12345_000.SP3D.config",
                 _adapterSettings["XmlPath"]
             );
 
@@ -67,12 +67,72 @@ namespace iRINGTools.SDK.SP3DDataLayer
 
             _kernel.Load(bindingConfigurationPath);
 
-            _sp3dDataLayer = _kernel.Get<IDataLayer2>();
+           // _sp3dDataLayer = _kernel.Get<SP3DDataLayer>(); This will reset the new updated adaptersettings with default values.
+            
+            _sp3dDataLayer = new SP3DDataLayer(_adapterSettings, _kernel);
         }
 
-       // [Test]
+        [Test]
         public void Create()
         {
+            IList<string> identifiers = new List<string>() { 
+                "Equip-001", 
+                "Equip-002",
+                "Equip-00323", 
+                "Equip-004"
+            };
+
+            Random random = new Random();
+
+            IList<IDataObject> dataObjects = _sp3dDataLayer.Create("Equipment", identifiers);
+            foreach (IDataObject dataObject in dataObjects)
+            {
+                dataObject.SetPropertyValue("PumpType", "PT-" + random.Next(2, 10));
+                dataObject.SetPropertyValue("PumpDriverType", "PDT-" + random.Next(2, 10));
+                dataObject.SetPropertyValue("DesignTemp", (double)random.Next(2, 10));
+                dataObject.SetPropertyValue("DesignPressure", (double)random.Next(2, 10));
+                dataObject.SetPropertyValue("Capacity", (double)random.Next(2, 10));
+                dataObject.SetPropertyValue("SpecificGravity", (double)random.Next(2, 10));
+                dataObject.SetPropertyValue("DifferentialPressure", (double)random.Next(2, 10));
+            }
+            Response actual = _sp3dDataLayer.Post(dataObjects);
+
+            if (actual.Level != StatusLevel.Success)
+            {
+                throw new AssertionException(Utility.SerializeDataContract<Response>(actual));
+            }
+
+            Assert.IsTrue(actual.Level == StatusLevel.Success);
+        }
+
+        [Test]
+        public void Read()
+        {
+            IList<string> identifiers = new List<string>() 
+            { 
+                "Equip-001", 
+                "Equip-002", 
+                "Equip-003", 
+                "Equip-004" 
+            };
+
+            IList<IDataObject> dataObjects = _sp3dDataLayer.Get("Equipment", identifiers);
+
+            if (!(dataObjects.Count() > 0))
+            {
+                throw new AssertionException("No Rows returned.");
+            }
+
+            foreach (IDataObject dataObject in dataObjects)
+            {
+                Assert.IsNotNull(dataObject.GetPropertyValue("PumpType"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("PumpDriverType"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("DesignTemp"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("DesignPressure"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("Capacity"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("SpecificGravity"));
+                Assert.IsNotNull(dataObject.GetPropertyValue("DifferentialPressure"));
+            }
         }
 
     }
