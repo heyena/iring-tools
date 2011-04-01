03a27c38024bfcfebf51e4bb3472ebc501d7a92b
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-01 09:35:44 +0000

    Working on it... searchPage()

---
+++ trunk/iRINGTools.ESBServices/iRINGTools.Apps/src/org/iringtools/controllers/RefDataController.java
@@ -0,0 +1,51 @@
+package org.iringtools.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.iringtools.models.RefDataModel;
+import org.iringtools.refdata.response.Response;
+import org.iringtools.widgets.tree.Tree;
+
+import com.opensymphony.xwork2.Action;
+
+
+public class RefDataController implements ServletRequestAware{
+	
+	private RefDataModel refdata;
+	private Tree tree;
+	private Response response;
+	private HttpServletRequest httpRequest = null;
+	
+	public RefDataController()
+	{
+		refdata = new RefDataModel();
+	}
+
+	public void setTree(Tree tree) {
+		this.tree = tree;
+	}
+
+	public Tree getTree() {
+		return tree;
+	}
+	
+	public void setServletRequest(HttpServletRequest request) {
+		this.httpRequest = request;  
+		} 
+	
+	public String searchPage() {
+		//System.out.println("callled.........");
+		String query=httpRequest.getParameter("query");
+    	if(query!=null){
+    		response = refdata.populate(httpRequest);
+    		//	tree = refdata.toTree();
+    	       
+    	}else{
+    		//System.out.println('{"items":null,"message":null,"success":false,"total":0,"errors":null}');
+    	}
+    	//System.out.println("Finished........."+response.getEntities().getItems().size());
+    	 return Action.SUCCESS;
+    	}
+	
+}
