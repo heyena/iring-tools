3a048dcf53286fd931d174478dade30a8b172b26
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-04 20:17:48 +0000

    removed SDK and updated build script.  CSVDataLayer has moved to IIP project.

--- trunk/bamboo.cmd
+++ trunk/bamboo.cmd
@@ -1,21 +1,7 @@
 @echo off
 setlocal
-rem set basedir=%~dp0
-rem set javabasedir=%basedir%iRINGTools.ESBServices\
-rem set deploymentdir=%basedir%Deployment\
 set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
-
-rem echo Updating build script ...
-rem cd %basedir%
-rem svn update build.xml
-rem pause
 
 echo Building C# projects ...
 msbuild bamboo.xml /t:All
 pause
-
-rem echo Building Java projects ...
-rem cd %javabasedir%
-rem call ant
-rem if %ERRORLEVEL% equ 0 copy /y %javabasedir%dist\*.* %deploymentdir%
-rem pause
