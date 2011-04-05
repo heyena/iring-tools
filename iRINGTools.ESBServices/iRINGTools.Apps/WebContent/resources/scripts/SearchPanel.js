54321ed697e7eb802fd82284d51bd04ab4ba3f15
aswini.nayak <aswini.nayak@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-05 08:50:23 +0000

    Reference Data Search Panel

--- iRINGTools.ESBServices/iRINGTools.Apps/WebContent/resources/scripts/SearchPanel.js
+++ iRINGTools.ESBServices/iRINGTools.Apps/WebContent/resources/scripts/SearchPanel.js
@@ -1,8 +1,7 @@
 Ext.ns('FederationManager');
 /**
 * @class FederationManager.SearchPanel
-* @extends TreePanel
-* @author by Gert Jansen van Rensburg
+* @author by Aswini Nayak
 */
 
 //image path
@@ -22,82 +21,126 @@ function renderIcon(value, p, record) {
 
 FederationManager.SearchPanel = Ext.extend(Ext.Panel, {
     title: 'Reference Data Search',
-    layout: 'fit',
+    layout: 'border',
     border: true,
     split: true,
-    searchStore: null,
-    searchExpander: null,
-    searchColumnmodel: null,
     searchUrl: null,
     limit: 100,
+    refClassTabPanel:null,
+    propertyPanel:null,
 
     /**
     * initComponent
     * @protected
     */
     initComponent: function () {
+        this.tbar = this.buildToolbar();
+        this.propertyPanel = new Ext.grid.PropertyGrid( {
+            id : 'class-property-panel',
+            title : 'Details',
+            region : 'east',
+            // layout: 'fit',
+            stripeRows : true,
+            collapsible : true,
+            autoScroll : true,
+            width:350,
+        	split: true,
+        	bodyBorder:true,
+        	collapsed : false,
+            border : true,
+            frame : true,
+            //bodyStyle: 'padding-bottom:15px;background:#eee;',
+            source : {},
+            listeners : {
+              // to disable editable option of the property grid
+              beforeedit : function(e) {
+                e.cancel = true;
+              }
+            }
+          });
 
-        this.searchStore = new Ext.data.JsonStore({
-            root: 'items',
-            totalProperty: 'total',
-            idProperty: 'Label',
-            fields: [
-              { name: 'Uri', allowBlank: false },
-              { name: 'Label', allowBlank: false },
-              { name: 'Repository', allowBlank: false }
-            ],
-            proxy: new Ext.data.HttpProxy({
-                url: this.searchUrl,
-                timeout: 12000
-            }),
-            baseParams: { limit: this.limit }
-        });
-
-        this.searchExpander = new FederationManager.AjaxRowExpander({
-            tpl: new Ext.Template(
-            '<p><b>Uri:</b> {Uri}</p><br>',
-            '<p><b>Description:</b> {Desc}</p>'
-          )
-        },'');
-
-        this.searchExpander.on('beforeexpand', function (record, body, rowIndex) {
-            alert(record);
-        });
-
-        this.tbar = [
-          'Search: ', ' ',
-          new Ext.ux.form.SearchField({
-              store: this.searchStore,
-              width: 320
-          }, ' ', { text: '' })
-        ];
-
-        this.bbar = new Ext.PagingToolbar({
-            store: this.searchStore,
-            pageSize: this.limit,
-            displayInfo: true,
-            displayMsg: 'Results {0} - {1} of {2}',
-            emptyMsg: "No results to display"
-        });
-
-        this.items = new Ext.grid.GridPanel({
-            border: false,
-            store: this.searchStore,
-            plugins: this.searchExpander,
-            cm: new Ext.grid.ColumnModel([
-        this.searchExpander,
-        { header: "Label", width: 400, sortable: true, dataIndex: 'Label', renderer: renderIcon },
-        { header: "Repository", width: 150, sortable: true, dataIndex: 'Repository' },
-        { header: "Uri", width: 400, sortable: true, dataIndex: 'Uri', hidden: true }
-      ])
-        });
+        this.refClassTabPanel = new Ext.TabPanel({
+            id: 'content-pane',
+            region: 'center',
+            deferredRender: false,
+            enableTabScroll: true,
+            border: true,
+            activeItem: 0
+          });
+        
+        
+        this.items =[this.refClassTabPanel,this.propertyPanel];
 
         // super
         FederationManager.SearchPanel.superclass.initComponent.call(this);
     },
 
+    buildToolbar: function () {
+        return [ ' Search:  ',' ',
+                 {
+        			xtype: 'textfield',
+        			allowBlank:false,
+        			blankText :'This field can not be blank',
+        			name: 'referencesearch',
+        			id:'referencesearch',
+        			listeners: {
+        	              specialkey: function(f,e){
+        	                if (e.getKey() == e.ENTER) {
+        	                    alert("about to search: "+Ext.get('referencesearch').getValue());
+        	                }
+        	              }
+        	            }
+            	},
+                 {
+                	xtype: 'checkbox',
+               	  	boxLabel:'Reset',
+               	  	text: 'Reset',
+               	  	name: 'reset'
+               	  },
+               	  {
+                     xtype : "tbbutton",
+                     text : 'Promote',
+                     //icon : 'resources/images/16x16/view-refresh.png',
+                     tooltip : 'Promote',
+                     disabled : false,
+                     handler: this.onPromote,
+                     scope : this
+                   },
+                   {
+                     xtype : "tbbutton",
+                     text : 'Add Class',
+                     tooltip : 'Add Class',
+                     disabled : false,
+                     handler: this.onClassAdd,
+                     scope : this
+                   },
+                   {
+                     xtype : "tbbutton",
+                     text : 'Edit Class',
+                     tooltip : 'Edit Class',
+                     disabled : true,
+                     handler: this.onClassEdit,
+                     scope : this
+                   },
+                   {
+                     xtype : "tbbutton",
+                     text : 'Add Template',
+                     tooltip : 'Add Template',
+                     disabled : false,
+                     handler: this.onTemplateAdd,
+                     scope : this
+                   },
+                   {
+                     xtype : "tbbutton",
+                     text : 'Edit Template',
+                     tooltip : 'Edit Template',
+                     disabled : true,
+                     handler: this.onTemplateEdit,
+                     scope : this
+                   }];
+      },
     load: function () {
-        this.searchStore.load({ params: { start: 0, limit: this.limit} });
+        //this.searchStore.load({ params: { start: 0, limit: this.limit} });
         return;
     }
 
