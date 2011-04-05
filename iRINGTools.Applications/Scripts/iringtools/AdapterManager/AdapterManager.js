90d44d86a7c5637c8dc60dd464b1c8c291eb65af
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-05 19:16:47 +0000

    Added Icons in Scope Tab and Application Tab

--- iRINGTools.Applications/Scripts/iringtools/AdapterManager/AdapterManager.js
+++ iRINGTools.Applications/Scripts/iringtools/AdapterManager/AdapterManager.js
@@ -6,36 +6,36 @@
 var App = new Ext.App({});
 
 Ext.onReady(function () {
-    Ext.QuickTips.init();
+  Ext.QuickTips.init();
 
-    Ext.get('about-link').on('click', function () {
-        var win = new Ext.Window({
-            title: 'About Adapter Manager',
-            bodyStyle: 'background-color:white;padding:5px',
-            width: 700,
-            height: 500,
-            closable: true,
-            resizable: false,
-            autoScroll: true,
-            buttons: [{
-                text: 'Close',
-                handler: function () {
-                    Ext.getBody().unmask();
-                    win.close();
-                }
-            }],
-            autoLoad: 'about.html',
-            listeners: {
-                close: {
-                    fn: function () {
-                        Ext.getBody().unmask();
-                    }
-                }
-            }
-        });
+  Ext.get('about-link').on('click', function () {
+    var win = new Ext.Window({
+      title: 'About Adapter Manager',
+      bodyStyle: 'background-color:white;padding:5px',
+      width: 700,
+      height: 500,
+      closable: true,
+      resizable: false,
+      autoScroll: true,
+      buttons: [{
+        text: 'Close',
+        handler: function () {
+          Ext.getBody().unmask();
+          win.close();
+        }
+      }],
+      autoLoad: 'about.html',
+      listeners: {
+        close: {
+          fn: function () {
+              Ext.getBody().unmask();
+          }
+        }
+      }
+    });
 
-        Ext.getBody().mask();
-        win.show();
+      Ext.getBody().mask();
+      win.show();
     });
 
     /*
@@ -53,209 +53,213 @@ Ext.onReady(function () {
 
 
     var searchPanel = new AdapterManager.SearchPanel({
-        id: 'search-panel',
-        title: 'Reference Data Search',
-        region: 'south',
-        height: 300,
+      id: 'search-panel',
+      title: 'Reference Data Search',
+      region: 'south',
+      height: 300,
 
-        collapseMode: 'mini',
-        collapsible: true,
-        collapsed: false,
+      collapseMode: 'mini',
+      collapsible: true,
+      collapsed: false,
 
-        searchUrl: 'refdata',
-        limit: 100
+      searchUrl: 'refdata',
+      limit: 100
     });
 
     var contentPanel = new Ext.TabPanel({
-        id: 'content-panel',
-        region: 'center',
+      id: 'content-panel',
+      region: 'center',
 
-        collapsible: false,
-        closable: true,
-        enableTabScroll: true,
-        border: true,
-        split: true
+      collapsible: false,
+      closable: true,
+      enableTabScroll: true,
+      border: true,
+      split: true
     });
 
     var directoryPanel = new AdapterManager.DirectoryPanel({
-        id: 'nav-panel',
-        title: 'Directory',
-        region: 'west',
-        width: 260,
+      id: 'nav-panel',
+      title: 'Directory',
+      region: 'west',
+      width: 260,
 
-        collapseMode: 'mini',
-        collapsible: true,
-        collapsed: false,
+      collapseMode: 'mini',
+      collapsible: true,
+      collapsed: false,
 
-        navigationUrl: 'directory/getnode'
+      navigationUrl: 'directory/getnode'
     });
 
     directoryPanel.on('newscope', function (npanel, node) {
 
-        if (node.attributes.type == "ScopeNode") {
-            node = node.parentNode;
-        }
+      if (node.attributes.type == "ScopeNode") {
+        node = node.parentNode;
+      }
 
-        var newTab = new AdapterManager.ScopePanel({
-            id: 'tab-' + node.id,
-            title: 'Scope - (New)',
-            record: node.attributes.record,
-            url: 'directory/scope'
-        });
+      var newTab = new AdapterManager.ScopePanel({
+        id: 'tab-' + node.id,
+        title: 'Scope - (New)',
+        record: node.attributes.record,
+        url: 'directory/scope',
+        iconCls: 'tabsScope'
+      });
 
-        newTab.on('save', function (panel) {
-            contentPanel.remove(panel);
-            directoryPanel.reload();
-        }, this);
+      newTab.on('save', function (panel) {
+        contentPanel.remove(panel);
+        directoryPanel.reload();
+      }, this);
 
-        contentPanel.add(newTab);
-        contentPanel.activate(newTab);
+      contentPanel.add(newTab);
+      contentPanel.activate(newTab);
 
     }, this);
 
     directoryPanel.on('editscope', function (npanel, node) {
 
-        var newTab = new AdapterManager.ScopePanel({
-            id: 'tab-' + node.id,
-            title: 'Scope - ' + node.text,
-            record: node.attributes.record,
-            url: 'directory/scope'
-        });
+      var newTab = new AdapterManager.ScopePanel({
+        id: 'tab-' + node.id,
+        title: 'Scope - ' + node.text,
+        record: node.attributes.record,
+        url: 'directory/scope',
+        iconCls: 'tabsScope'
+      });
 
-        newTab.on('save', function (panel) {
-            contentPanel.removeAll(true);
-           directoryPanel.reload();
-        }, this);
+      newTab.on('save', function (panel) {
+        contentPanel.removeAll(true);
+        directoryPanel.reload();
+      }, this);
 
-        contentPanel.add(newTab);
-        contentPanel.activate(newTab);
+      contentPanel.add(newTab);
+      contentPanel.activate(newTab);
 
     }, this);
 
     directoryPanel.on('deletescope', function (npanel, node) {
 
-        Ext.Ajax.request({
-            url: 'directory/deletescope',
-            method: 'POST',
-            params: {
-                'nodeid': node.attributes.id
-            },
-            success: function (o) {
-                contentPanel.removeAll(true);
-                directoryPanel.reload();
-                Ext.Msg.alert('Sucess', 'Scope [' + node.attributes.id + '] has been deleted');
-            },
-            failure: function (f, a) {
-                Ext.Msg.alert('Warning', 'Error!!!');
-            }
-        });
+      Ext.Ajax.request({
+        url: 'directory/deletescope',
+        method: 'POST',
+        params: {
+          'nodeid': node.attributes.id
+        },
+        success: function (o) {
+          contentPanel.removeAll(true);
+          directoryPanel.reload();
+          Ext.Msg.alert('Sucess', 'Scope [' + node.attributes.id + '] has been deleted');
+        },
+        failure: function (f, a) {
+          Ext.Msg.alert('Warning', 'Error!!!');
+        }
+      });
 
     }, this);
 
 
     directoryPanel.on('newapplication', function (npanel, node) {
 
-        if (node.attributes.type == "ApplicationNode") {
-            node = node.parentNode;
-        }
+      if (node.attributes.type == "ApplicationNode") {
+        node = node.parentNode;
+      }
 
-        var newTab = new AdapterManager.ApplicationPanel({
-            id: 'tab-' + node.id,
-            title: 'Application - ' + node.parentNode.text + '.(new)',
-            scope: node.attributes.record,
-            record: null,
-            url: 'directory/application',
-            closable: true
-        });
+      var newTab = new AdapterManager.ApplicationPanel({
+        id: 'tab-' + node.id,
+        title: 'Application - ' + node.parentNode.text + '.(new)',
+        scope: node.attributes.record,
+        record: null,
+        url: 'directory/application',
+        closable: true,
+        iconCls: 'tabsApplication'
+      });
 
-        newTab.on('save', function (panel) {
-            contentPanel.remove(panel);
-            directoryPanel.reload();
-        }, this);
+      newTab.on('save', function (panel) {
+        contentPanel.remove(panel);
+        directoryPanel.reload();
+      }, this);
 
-        contentPanel.add(newTab);
-        contentPanel.activate(newTab);
+      contentPanel.add(newTab);
+      contentPanel.activate(newTab);
 
     }, this);
 
     directoryPanel.on('editapplication', function (npanel, node) {
-        if (node == undefined || node == null) return;
+      if (node == undefined || node == null) return;
 
-        var newTab = new AdapterManager.ApplicationPanel({
-            id: 'tab-' + node.id,
-            title: 'Application - ' + node.parentNode.text + '.' + node.text,
-            scope: node.parentNode.attributes.record,
-            record: node.attributes.record,
-            url: 'directory/application',
+      var newTab = new AdapterManager.ApplicationPanel({
+        id: 'tab-' + node.id,
+        title: 'Application - ' + node.parentNode.text + '.' + node.text,
+        scope: node.parentNode.attributes.record,
+        record: node.attributes.record,
+        url: 'directory/application',
+        closable: true,
+        iconCls: 'tabsApplication'
+      });
+
+      newTab.on('save', function (panel) {
+        contentPanel.remove(panel);
+        directoryPanel.reload();
+      }, this);
+
+      newTab.on('configure', function (panel, scope, application) {
+
+        if (application.DataLayer == 'ExcelLibrary') {
+
+          var newConfig = new AdapterManager.ExcelLibraryPanel({
+            id: 'tab-c.' + scope.Name + '.' + application.Name,
+            title: 'Configure - ' + scope.Name + '.' + application.Name,
+            scope: scope,
+            application: application,
+            url: 'excel/configure',
             closable: true
-        });
+          });
 
-        newTab.on('save', function (panel) {
-            contentPanel.remove(panel);
-            directoryPanel.reload();
-        }, this);
+          contentPanel.add(newConfig);
+          contentPanel.activate(newConfig);
 
-        newTab.on('configure', function (panel, scope, application) {
+        } else if (application.DataLayer == 'NHibernateLibrary') {
 
-            if (application.DataLayer == 'ExcelLibrary') {
+        } else {
 
-                var newConfig = new AdapterManager.ExcelLibraryPanel({
-                    id: 'tab-c.' + scope.Name + '.' + application.Name,
-                    title: 'Configure - ' + scope.Name + '.' + application.Name,
-                    scope: scope,
-                    application: application,
-                    url: 'excel/configure',
-                    closable: true
-                });
+        }
 
-                contentPanel.add(newConfig);
-                contentPanel.activate(newConfig);
+      }, this);
 
-            } else if (application.DataLayer == 'NHibernateLibrary') {
-
-            } else {
-
-            }
-
-        }, this);
-
-        contentPanel.add(newTab);
-        contentPanel.activate(newTab);
+      contentPanel.add(newTab);
+      contentPanel.activate(newTab);
 
     }, this);
 
     directoryPanel.on('deleteapplication', function (npanel, node) {
-        Ext.Ajax.request({
-            url: 'directory/deleteapplication',
-            method: 'POST',
-            params: {
-                'nodeid': node.attributes.id
-            },
-            success: function (o) {
-                contentPanel.removeAll(true);
-                directoryPanel.reload();
-                Ext.Msg.alert('Sucess', 'Application [' + node.attributes.id + '] has been deleted');
-            },
-            failure: function (f, a) {
-                Ext.Msg.alert('Warning', 'Error!!!');
-            }
-        });
+      Ext.Ajax.request({
+        url: 'directory/deleteapplication',
+        method: 'POST',
+        params: {
+          'nodeid': node.attributes.id
+        },
+        success: function (o) {
+          contentPanel.removeAll(true);
+          directoryPanel.reload();
+          Ext.Msg.alert('Sucess', 'Application [' + node.attributes.id + '] has been deleted');
+        },
+        failure: function (f, a) {
+          Ext.Msg.alert('Warning', 'Error!!!');
+        }
+      });
     }, this);
 
 
     directoryPanel.on('openmapping', function (npanel, node) {
 
-        var scope = node.parentNode;
-        var application = node;
+      var scope = node.parentNode;
+      var application = node;
 
-        var newTab = new AdapterManager.MappingPanel({
-            title: 'Mapping - ' + scope.text + "." + application.text,
-            scope: scope.attributes.record,
-            application: application.attributes.record
-        });
+      var newTab = new AdapterManager.MappingPanel({
+        title: 'Mapping - ' + scope.text + "." + application.text,
+        scope: scope.attributes.record,
+        application: application.attributes.record
+      });
 
-        contentPanel.add(newTab);
-        contentPanel.activate(newTab);
+      contentPanel.add(newTab);
+      contentPanel.activate(newTab);
 
     }, this);
 
@@ -265,28 +269,28 @@ Ext.onReady(function () {
     // Finally, build the main layout once all the pieces are ready.  This is also a good
     // example of putting together a full-screen BorderLayout within a Viewport.
     var viewPort = new Ext.Viewport({
-        layout: 'border',
-        title: 'Scope Editor',
-        border: false,
-        items: [
-      {
-          xtype: 'box',
-          region: 'north',
-          applyTo: 'header',
-          border: false,
-          height: 55
+      layout: 'border',
+      title: 'Scope Editor',
+      border: false,
+      items: [
+				{
+					xtype: 'box',
+					region: 'north',
+					applyTo: 'header',
+					border: false,
+					height: 55
+				},
+				directoryPanel,
+				contentPanel,
+				searchPanel
+			],
+      listeners: {
+        render: function () {
+          // After the component has been rendered, disable the default browser context menu
+          Ext.getBody().on("contextmenu", Ext.emptyFn, null, { preventDefault: true });
+        }
       },
-      directoryPanel,
-      contentPanel,
-      searchPanel
-    ],
-        listeners: {
-            render: function () {
-                // After the component has been rendered, disable the default browser context menu
-                Ext.getBody().on("contextmenu", Ext.emptyFn, null, { preventDefault: true });
-            }
-        },
-        renderTo: Ext.getBody()
+      renderTo: Ext.getBody()
     });
 
 });
