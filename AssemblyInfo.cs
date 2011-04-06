c3c923f239bba405cfaf1b723ef684d3a031fe0e
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-06 04:11:33 +0000

    changed version to 2.0.5

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.4.2965")]
-[assembly: AssemblyFileVersion("2.0.4.2965")]
+[assembly: AssemblyVersion("2.0.5.3133")]
+[assembly: AssemblyFileVersion("2.0.5.3133")]
