fc57fcb94e2e21c44b42d08e696d476c8540c663
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-07 23:13:32 +0000

    added mapping repository also can now represent mapping in MappingPanel.js

--- trunk/iRINGTools.Applications/Scripts/iringtools/AdapterManager/MappingPanel.js
+++ trunk/iRINGTools.Applications/Scripts/iringtools/AdapterManager/MappingPanel.js
@@ -9,105 +9,106 @@ Ext.ns('AdapterManager');
 */
 AdapterManager.MappingPanel = Ext.extend(Ext.Panel, {
 
-    height: 300,
-    minSize: 150,
+  height: 300,
+  minSize: 150,
 
-    autoScroll: true,
-    layout: 'border',
-    
-    split: true,
-    closable: true,
+  autoScroll: true,
+  layout: 'border',
 
-    navigationUrl: null,
-    propertyPanel: null,
-    mappingPanel: null,
-    rootNode: null,
-    treeLoader: null,
-    scope: null,
-    application: null,
-    graph: null,
+  split: true,
+  closable: true,
 
-    /**
-    * initComponent
-    * @protected
-    */
-    initComponent: function () {
+  navigationUrl: null,
+  propertyPanel: null,
+  mappingPanel: null,
+  rootNode: null,
+  treeLoader: null,
+  scope: null,
+  application: null,
+  graph: null,
 
-        this.treeLoader = new Ext.tree.TreeLoader({
-            baseParams: {
-                type: null,
-                graphMap: null
-            },
-            url: this.navigationUrl
-        });
+  /**
+  * initComponent
+  * @protected
+  */
+  initComponent: function () {
 
-        this.treeLoader.on("beforeload", function (treeLoader, node) {
-            treeLoader.baseParams.type = node.attributes.type;
-        }, this);
-                
-        this.rootNode = new Ext.tree.AsyncTreeNode({
-            id: this.scope + "/" + this.application + "/" + this.graph,
-            text: this.graph,
-            expanded: true,
-            draggable: false,
-            icon: 'Content/img/internet-web-browser.png',
-            type: 'GraphNode'
-        });
+    this.treeLoader = new Ext.tree.TreeLoader({
+      baseParams: {
+        type: null,
+        record: null
+      },
+      url: this.navigationUrl
+    });
 
-        this.mappingPanel = new Ext.tree.TreePanel({
-            region: 'center',
-            split: true,
-            border: true,
-            collapseMode: 'mini',
-            height: 300,
-            layout: 'fit',              
-            expandAll: true,
-            rootVisible: true,
-            lines: true,
-            autoScroll: true,
-            //singleExpand: true,
-            useArrows: true,
-            loader: this.treeLoader,
-            root: this.rootNode,
-            tbar: [{ text: 'Save', icon: 'Content/img/document-save.png'}],
-            bbar: new Ext.ux.StatusBar({
-                defaultText: 'Ready',
-                statusAlign: 'right'
-            })          
-        });
+    this.treeLoader.on("beforeload", function (treeLoader, node) {
+      treeLoader.baseParams.type = node.attributes.type;
+      treeLoader.baseParams.record = node.attributes.record;
+    }, this);
 
-        this.propertyPanel = new Ext.Panel({
-            title: 'Properties',
-            region: 'east',
-            collapseMode: 'mini',
-            width: 200,
-            collapsible: true,
-            collapsed: false,
-            split: true,
-            border: true
-        });
+    this.rootNode = new Ext.tree.AsyncTreeNode({
+      id: this.scope.Name + "/" + this.application.Name,
+      text: 'mapping',
+      expanded: true,
+      draggable: false,
+      icon: 'Content/img/internet-web-browser.png',
+      type: 'MappingNode'
+    });
 
-        this.items = [
+    this.mappingPanel = new Ext.tree.TreePanel({
+      region: 'center',
+      split: true,
+      border: true,
+      collapseMode: 'mini',
+      height: 300,
+      layout: 'fit',
+      expandAll: true,
+      rootVisible: true,
+      lines: true,
+      autoScroll: true,
+      //singleExpand: true,
+      useArrows: true,
+      loader: this.treeLoader,
+      root: this.rootNode,
+      tbar: [{ text: 'Save', icon: 'Content/img/document-save.png'}],
+      bbar: new Ext.ux.StatusBar({
+        defaultText: 'Ready',
+        statusAlign: 'right'
+      })
+    });
+
+    this.propertyPanel = new Ext.Panel({
+      title: 'Properties',
+      region: 'east',
+      collapseMode: 'mini',
+      width: 200,
+      collapsible: true,
+      collapsed: false,
+      split: true,
+      border: true
+    });
+
+    this.items = [
           this.propertyPanel,
           this.mappingPanel
         ];
 
-        // super
-        AdapterManager.MappingPanel.superclass.initComponent.call(this);
+    // super
+    AdapterManager.MappingPanel.superclass.initComponent.call(this);
 
-    },
+  },
 
-    /**
-    * buildUI
-    * @private
-    */
-    buildUI: function () {
-        return [{
-            text: 'Save',
-            //iconCls: 'icon-save',
-            //handler: this.onUpdate,
-            scope: this
-        }];
-    }
+  /**
+  * buildUI
+  * @private
+  */
+  buildUI: function () {
+    return [{
+      text: 'Save',
+      //iconCls: 'icon-save',
+      //handler: this.onUpdate,
+      scope: this
+    }];
+  }
 
 });
