1a8956e958b06777f48375f3cd73498f31104a0f
neha11.b@tcs.com <neha11.b@tcs.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-08 13:28:37 +0000

    Fix Failed Build

--- trunk/iRINGTools.Applications/Models/MappingRepository.cs
+++ trunk/iRINGTools.Applications/Models/MappingRepository.cs
@@ -14,41 +14,45 @@ using org.iringtools.mapping;
 
 namespace iRINGTools.Web.Models
 {
-  public class MappingRepository : IMappingRepository
-  {
-
-    private NameValueCollection _settings = null;
-    private WebHttpClient _client = null;
-    private string _refDataServiceURI = string.Empty;
-    
-    [Inject]
-    public MappingRepository()
+    public class MappingRepository : IMappingRepository
     {
-      _settings = ConfigurationManager.AppSettings;
-      _client = new WebHttpClient(_settings["AdapterServiceUri"]);
+
+        private NameValueCollection _settings = null;
+        private WebHttpClient _client = null;
+        private string _refDataServiceURI = string.Empty;
+
+        [Inject]
+        public MappingRepository()
+        {
+            _settings = ConfigurationManager.AppSettings;
+            _client = new WebHttpClient(_settings["AdapterServiceUri"]);
+        }
+
+        public Mapping GetMapping(string scopeName, string applicationName)
+        {
+            Mapping obj = null;
+
+            try
+            {
+                obj = _client.Get<Mapping>(String.Format("/{0}/{1}/mapping", scopeName, applicationName), true);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return obj;
+        }
+
+        public Mapping UpdateMapping(string scopeName, string applicationName)
+        {
+            Mapping obj = null;
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+            }
+            return obj;
+        }
     }
-
-    public Mapping GetMapping(string scopeName, string applicationName)
-    {
-      Mapping obj = null;
-
-      try
-      {
-        obj = _client.Get<Mapping>(String.Format("/{0}/{1}/mapping", scopeName, applicationName), true);
-      }
-      catch (Exception ex)
-      {
-      }
-
-      return obj;
-    }
-
-    public Mapping UpdateMapping(string scopeName, string applicationName)
-    {
-      Mapping obj = null;
-      try
-      {
-
-    }
-  }
 }
