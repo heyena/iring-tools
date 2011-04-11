39399d2201ac616faac60d0b08d4b3da43bb39c3
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-11 19:42:06 +0000

    Fixed bamboo to remove testing change. Fixed package.cmd to work with bamboo to test.

--- trunk/package.cmd
+++ trunk/package.cmd
@@ -3,11 +3,6 @@ setlocal
 set basedir=%~dp0
 set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
 
-echo Updating build script ...
-cd %basedir%
-svn update build.xml
-pause
-
 echo Creating Deployment Packages ...
-msbuild build.xml /t:CreatePackages /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
+msbuild bamboo.xml /t:CreatePackages
 pause
