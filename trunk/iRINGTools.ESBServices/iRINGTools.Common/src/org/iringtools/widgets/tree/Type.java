109d2ad31f8d4fa770f17f794e5c5a657749a597
rashmi.bechtel <rashmi.bechtel@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-13 12:57:52 +0000

    Added record in Node and Type enum

---
+++ trunk/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/widgets/tree/Type.java
@@ -0,0 +1,31 @@
+package org.iringtools.widgets.tree;
+
+public enum Type {
+
+	SEARCH("SearchNode"),
+    CLASS("ClassNode"),
+    TEMPLATE("TemplateNode"),
+    CLASSIFICATION("ClassificationsNode"),
+    SUPERCLASS("SuperclassesNode"),
+    SUBCLASS("SubclassesNode"),
+    CLASSTEMPLATE("ClassTemplatesNode");
+    private final String value;
+
+    Type(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static Type fromValue(String v) {
+        for (Type c: Type.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
