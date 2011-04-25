781c98c1644b889eead3ce30807cb98b453477d5
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-04-25 08:47:23 +0000

    fixed some bugs

--- iRINGTools.Applications/Scripts/iringtools/AdapterManager/DirectoryPanel.js
+++ iRINGTools.Applications/Scripts/iringtools/AdapterManager/DirectoryPanel.js
@@ -138,6 +138,7 @@ AdapterManager.DirectoryPanel = Ext.extend(Ext.Panel, {
     this.directoryPanel.on('contextmenu', this.showContextMenu, this);
     this.directoryPanel.on('click', this.onClick, this);
     this.directoryPanel.on('dblclick', this.onDoubleClick, this);
+    this.directoryPanel.on('addgraphmap', this.onAddGraphmap, this);
 
 
 
@@ -325,6 +326,140 @@ AdapterManager.DirectoryPanel = Ext.extend(Ext.Panel, {
     this.fireEvent('addgraphmap', this, node);
   },
 
+  onAddGraphMap: function (node) {
+    var dirnode = this.directoryPanel.getSelectionModel().getSelectedNode();
+    var formid = 'graphtarget-' + dirnode.parentNode.parentNode.text + '-' + dirnode.parentNode.text;
+    var form = new Ext.form.FormPanel({
+      id: formid,
+      layout: 'form',
+      method: 'POST',
+      border: false,
+      frame: false,
+      bbar: [
+        { text: 'Submit', scope: this, handler: this.onSubmitGraphMap },
+        { text: 'Close', scope: this, handler: this.onClose }
+        ],
+      url: 'mapping/addgraphmap',
+      items: [{ xtype: 'textfield', name: 'graphName', id: 'graphName', fieldLabel: 'Graph Name', width: 120, required: true },
+              { xtype: 'hidden', name: 'objectName', id: 'objectName' },
+              { xtype: 'hidden', name: 'classLabel', id: 'classLabel' },
+              { xtype: 'hidden', name: 'classUrl', id: 'classUrl' },
+              { xtype: 'hidden', name: 'mappingNode', id: 'mappingNode', value: this.rootNode }
+             ],
+      html: '<div class="property-target' + formid + '" '
+          + 'style="border:1px silver solid;margin:5px;padding:8px;height:20px">'
+          + 'Drop a Key Property Node here.</div>'
+          + '<div class="class-target' + formid + '" '
+          + 'style="border:1px silver solid;margin:5px;padding:8px;height:20px">'
+          + 'Drop a Class Node here. </div>',
+
+      afterRender: function (cmp) {
+        Ext.FormPanel.prototype.afterRender.apply(this, arguments);
+
+        var propertyTarget = this.body.child('div.property-target' + formid);
+        var propertydd = new Ext.dd.DropTarget(propertyTarget, {
+          ddGroup: 'propertyGroup',
+          notifyEnter: function (dd, e, data) {
+            if (data.node.attributes.type != 'KeyDataPropertyNode')
+              return this.dropNotAllowed;
+            else
+              return this.dropAllowed;
+          },
+          notifyOver: function (dd, e, data) {
+            if (data.node.attributes.type != 'KeyDataPropertyNode')
+              return this.dropNotAllowed;
+            else
+              return this.dropAllowed;
+          },
+          notifyDrop: function (dd, e, data) {
+            if (data.node.attributes.type != 'KeyDataPropertyNode') {
+              return false;
+            }
+            else {
+              Ext.get('objectName').dom.value = data.node.id;
+              var msg = '<table style="font-size:13px"><tr><td>Property:</td><td><b>' + data.node.id.split('/')[5] + '</b></td></tr>'
+              msg += '</table>'
+              Ext.getCmp(formid).body.child('div.property-target' + formid).update(msg)
+              return true;
+            }
+          } //eo notifyDrop
+        }); //eo propertydd
+        var classTarget = this.body.child('div.class-target' + formid);
+        var classdd = new Ext.dd.DropTarget(classTarget, {
+          ddGroup: 'refdataGroup',
+          notifyDrop: function (classdd, e, data) {
+            if (data.node.attributes.type != 'ClassNode') {
+              Ext.Msg.show({
+                title: 'Invalid Selection',
+                msg: 'Please slect a RDL Class...',
+                icon: Ext.MessageBox.ERROR,
+                buttons: Ext.Msg.CANCEL
+              });
+              return false;
+            }
+            Ext.get('classLabel').dom.value = data.node.attributes.record.Label;
+            Ext.get('classUrl').dom.value = data.node.attributes.record.Uri;
+            var msg = '<table style="font-size:13px"><tr><td>Class Label:</td><td><b>' + data.node.attributes.record.Label + '</b></td></tr>'
+            msg += '</table>'
+            Ext.getCmp(formid).body.child('div.class-target' + formid).update(msg)
+            return true;
+
+          } //eo notifyDrop
+        }); //eo propertydd
+      }
+    });
+
+    var win = new Ext.Window({
+      closable: true,
+      modal: false,
+      layout: 'form',
+      title: 'Add new GraphMap to Mapping',
+      items: form,
+      height: 180,
+      width: 430,
+      plain: true,
+      scope: this
+    });
+
+    win.show();
+  },
+
+  onClose: function (btn, e) {
+    if (btn != undefined) {
+      var win = btn.findParentByType('window');
+      if (win != undefined)
+        win.close();
+    }
+  },
+
+  onSubmitGraphMap: function (btn, e) {
+    var form = btn.findParentByType('form');
+    var win = btn.findParentByType('window');
+    var objectname = Ext.get('objectName').dom.value;
+    var classlabel = Ext.get('classLabel').dom.value;
+    var classuri = Ext.get('classUrl').dom.value;
+    var graphname = Ext.get('graphName').dom.value;
+    var that = this;
+    if (form.getForm().isValid())
+      Ext.Ajax.request({
+        url: 'mapping/addgraphmap',
+        method: 'POST',
+        params: {
+          objectName: objectname,
+          classLabel: classlabel,
+          classUrl: classuri,
+          graphName: graphname
+        },
+        success: function (result, request) {
+          that.onReload();
+          win.close();
+          Ext.Msg.show({ title: 'Success', msg: 'Added GraphMap to mapping', icon: Ext.MessageBox.INFO, buttons: Ext.Msg.OK });
+        },
+        failure: function (result, request) {
+          Ext.Msg.show({ title: 'Failure', msg: 'Failed to Add GraphMap to mapping', icon: Ext.MessageBox.ERROR, buttons: Ext.Msg.CANCEL });
+        }
+      })
+  },
 
   onEditGraphMap: function (btn, e) {
     var node = this.directoryPanel.getSelectionModel().getSelectedNode();
