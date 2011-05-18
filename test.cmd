a427c115d3cde36cdd0027787976e944525fe24c
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-05-18 16:59:18 +0000

    more unit test XMLs to fix the build.

--- test.cmd
+++ test.cmd
@@ -3,11 +3,8 @@ setlocal
 set basedir=%~dp0
 set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
 
-echo Updating build script ...
-cd %basedir%
-svn update build.xml
-pause
-
 echo Running C# tests ...
-msbuild build.xml /t:Test /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
+cd %basedir%
+
+msbuild build.xml /t:Test
 pause
