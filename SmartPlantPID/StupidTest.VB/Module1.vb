821ee49c7a88917c09da4288a16a0c593ed28a13
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-05-23 20:17:44 +0000

    Adding Stupid Test to demonstrate/test COM stupidity.

---
+++ SmartPlantPID/StupidTest.VB/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports Llama
+
+Module Module1
+
+  Sub Main()
+
+    Dim stupidThingy As LMADataSource = New LMADataSource()
+
+  End Sub
+
+End Module
---
+++ SmartPlantPID/StupidTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Llama;
+
+namespace StupidTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      LMADataSource stupidThingy = new LMADataSource();
+    }
+  }
+}
