76a7947114bfd8a1b3852c93df2b9c45e879c819
andersholmefjord <andersholmefjord@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-06-01 07:48:34 +0000

    added VB project with sample code and reference in SPPIDDataLayer project

---
+++ SPPIDDataLayerVB/LamaFactory.vb
@@ -0,0 +1,31 @@
+﻿'Imports Llama;
+
+
+Public Class LamaFactory
+
+    'Public Shared Function Llama.LMADataSource CreateDataSource(string siteNode, string projectStr)
+    '    Dim ds As Llama.LMADataSource = New Llama.LMADataSource()
+    '    ds.ProjectNumber = projectStr;
+    '    ds.set_SiteNode(siteNode);
+    '    Return ds
+
+    Public Shared Function CreateDataSource(ByVal siteNode As String, ByVal projectStr As String) As LMADataSourceSubstitute
+        Dim ds As LMADataSourceSubstitute = New LMADataSourceSubstitute()
+        ds.ProjectNumber = projectStr
+        ds.set_SiteNode(siteNode)
+        Return ds
+    End Function
+
+End Class
+
+'Had to use this as I did not have the Llama.dll available
+'Should be the Llama.LMADataSource instead
+Public Class LMADataSourceSubstitute
+
+    Public Property ProjectNumber() As String
+
+    Public Sub set_SiteNode(ByVal siteNode As String)
+
+    End Sub
+
+End Class
