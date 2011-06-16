1edb9b38ba9f34fc8d7c1272153e1c5ec8d1b377
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-06-16 22:12:21 +0000

    Added Access Control List xsd and classes.

--- iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/directory/ObjectFactory.java
+++ iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/directory/ObjectFactory.java
@@ -30,19 +30,115 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link User }
+     * Create an instance of {@link Commodity }
      * 
      */
-    public User createUser() {
-        return new User();
+    public Commodity createCommodity() {
+        return new Commodity();
     }
 
     /**
-     * Create an instance of {@link Groups }
+     * Create an instance of {@link Members }
      * 
      */
-    public Groups createGroups() {
-        return new Groups();
+    public Members createMembers() {
+        return new Members();
+    }
+
+    /**
+     * Create an instance of {@link Scope }
+     * 
+     */
+    public Scope createScope() {
+        return new Scope();
+    }
+
+    /**
+     * Create an instance of {@link Graph }
+     * 
+     */
+    public Graph createGraph() {
+        return new Graph();
+    }
+
+    /**
+     * Create an instance of {@link DataExchanges }
+     * 
+     */
+    public DataExchanges createDataExchanges() {
+        return new DataExchanges();
+    }
+
+    /**
+     * Create an instance of {@link Graphs }
+     * 
+     */
+    public Graphs createGraphs() {
+        return new Graphs();
+    }
+
+    /**
+     * Create an instance of {@link ApplicationData }
+     * 
+     */
+    public ApplicationData createApplicationData() {
+        return new ApplicationData();
+    }
+
+    /**
+     * Create an instance of {@link Exchanges }
+     * 
+     */
+    public Exchanges createExchanges() {
+        return new Exchanges();
+    }
+
+    /**
+     * Create an instance of {@link Application }
+     * 
+     */
+    public Application createApplication() {
+        return new Application();
+    }
+
+    /**
+     * Create an instance of {@link Aci }
+     * 
+     */
+    public Aci createAci() {
+        return new Aci();
+    }
+
+    /**
+     * Create an instance of {@link ExchangeDefinition }
+     * 
+     */
+    public ExchangeDefinition createExchangeDefinition() {
+        return new ExchangeDefinition();
+    }
+
+    /**
+     * Create an instance of {@link Exchange }
+     * 
+     */
+    public Exchange createExchange() {
+        return new Exchange();
+    }
+
+    /**
+     * Create an instance of {@link Directory }
+     * 
+     */
+    public Directory createDirectory() {
+        return new Directory();
+    }
+
+    /**
+     * Create an instance of {@link Acl }
+     * 
+     */
+    public Acl createAcl() {
+        return new Acl();
     }
 
 }
