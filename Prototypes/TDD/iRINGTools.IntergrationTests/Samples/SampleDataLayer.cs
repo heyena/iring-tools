89466a694b055255ff98e89791bb18ac59f4c54a
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-06-17 01:10:09 +0000

    iRINGTools Refactored

---
+++ Prototypes/TDD/iRINGTools.IntergrationTests/Samples/SampleDataLayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject;
+
+using iRINGTools.Data;
+
+namespace iRINGTools.IntergrationTests
+{
+  public class SampleDataLayer : IDataLayer
+  {
+    public string Name { get; private set; }
+
+    public SampleDataLayer()
+    {
+      Name = "Sample DataLayer";
+    }
+
+    public IList<IDataObject> Create(string objectType, IList<string> identifiers)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IList<IDataObject> Get(string objectType, IList<string> identifiers)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Response Post(IList<IDataObject> dataObjects)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Response Delete(string objectType, IList<string> identifiers)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IDictionary GetDictionary()
+    {
+      throw new NotImplementedException();
+    }
+
+    public IList<IDataObject> GetRelatedObjects(IDataObject dataObject, string relatedObjectType)
+    {
+      throw new NotImplementedException();
+    }
+
+
+    public IQueryable<IDataObject> DataObjects
+    {
+      get { throw new NotImplementedException(); }
+    }
+  }
+}
---
+++ Prototypes/TDD/iRINGTools.Tests/TestRepositories/TestProjectionEngineRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Web;
+
+using iRINGTools.Data;
+
+namespace iRINGTools.Tests
+{
+
+  public class TestProjectionEngineRepository: IProjectionEngineRepository  
+  {
+    private List<IProjectionEngine> _projectionEngineList;
+
+    public TestProjectionEngineRepository()
+    {
+      _projectionEngineList = new List<IProjectionEngine>();
+
+      for (int i = 1; i <= 5; i++)
+      {
+        var d = new ProjectionEngine
+        { 
+          Format = "ProjectionEngine" + i.ToString()
+        };
+
+        _projectionEngineList.Add(d);
+      }
+    }
+
+    public IQueryable<IProjectionEngine> GetProjectionEngines()
+    {
+      return _projectionEngineList.AsQueryable();
+    }
+  }
+}
