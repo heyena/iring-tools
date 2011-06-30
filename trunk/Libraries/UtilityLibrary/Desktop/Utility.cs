b3405bf3b0e5df6812b6cec196a099cc2f2c7fcc
gertjvr <gertjvr@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-06-30 01:00:26 +0000

    Updated NHibernate Configuration and Service Post methods instead of Gets

--- trunk/Libraries/UtilityLibrary/Desktop/Utility.cs
+++ trunk/Libraries/UtilityLibrary/Desktop/Utility.cs
@@ -1211,5 +1211,20 @@ namespace org.iringtools.utility
         mimeType = regKey.GetValue("Content Type").ToString();
       return mimeType;
     }
+
+    public static string EncodeTo64(string toEncode)
+    {
+      byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+      string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+      return returnValue;
+    }
+
+    public static string DecodeFrom64(string encodedData)
+    {
+      byte[] encodedDataAsBytes = System.Convert.FromBase64String(encodedData);
+      string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+      return returnValue;
+    }
+
   }
 }
