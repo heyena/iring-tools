80dc923d315540f6b7fba420fcb93c950f9333ba
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-07-07 19:27:58 +0000

    Oops! Missed some Security and Identity modifications to support SSO as well as Windows/Anonymous.

--- Libraries/iRINGLibrary/Desktop/IAuthenticationLayer.cs
+++ Libraries/iRINGLibrary/Desktop/IAuthenticationLayer.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace org.iringtools.adapter.security
 {
   interface IAuthenticationLayer
   {
+    string Authenticate(ref IDictionary allClaims, ref string OAuthtoken);
   }
 }
