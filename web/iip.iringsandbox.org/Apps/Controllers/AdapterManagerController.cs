2f600d65bf5978680733b492baac88964228d9b7
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-07-18 22:18:52 +0000

    adding iip environment.

---
+++ web/iip.iringsandbox.org/Apps/Controllers/AdapterManagerController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using iRINGTools.Web.Models;
+using org.iringtools.library;
+using org.iringtools.adapter.security;
+using iRINGTools.Web.Helpers;
+using System;
+using System.IO;
+using log4net;
+using System.Configuration;
+using System.Collections;
+using org.iringtools.utility;
+
+namespace iRINGTools.Web.Controllers
+{
+  public abstract class BaseController : Controller
+  {
+    protected IAuthenticationLayer _authenticationLayer = new OAuthProvider();
+    protected IDictionary _allClaims = new Dictionary<string, string>();
+    protected string _oAuthToken = String.Empty;
+    protected IAuthorizationLayer _authorizationLayer = new LdapAuthorizationProvider();
+    private static readonly ILog _logger = LogManager.GetLogger(typeof(BaseController));
+
+    public BaseController()
+		{
+      string enableOAuth = ConfigurationManager.AppSettings["EnableOAuth"];
+
+      if (!String.IsNullOrEmpty(enableOAuth) && enableOAuth.ToUpper() == "TRUE")
+      {
+        _authenticationLayer.Authenticate(ref _allClaims, ref _oAuthToken);
+
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string ldapConfigFilePath = baseDirectory + @"App_Data\ldap.conf";
+
+        if (System.IO.File.Exists(ldapConfigFilePath))
+        {
+          Properties ldapConfig = new Properties();
+          ldapConfig.Load(ldapConfigFilePath);
+          ldapConfig["authorizedGroup"] = "adapterAdmins";
+          _authorizationLayer.Init(ldapConfig);
+
+          if (!_authorizationLayer.IsAuthorized(_allClaims))
+          {
+            throw new UnauthorizedAccessException("User not authorized to access AdapterManager.");
+          }
+        }
+        else
+        {
+          _logger.Warn("LDAP Configuration is missing!");
+        }
+      }
+		}
+  }
+
+	public class AdapterManagerController : BaseController 
+	{
+		private AdapterRepository _repository;
+    private static readonly ILog _logger = LogManager.GetLogger(typeof(AdapterManagerController));
+    
+		public AdapterManagerController() : this(new AdapterRepository()) { }
+
+		public AdapterManagerController(AdapterRepository repository) : base()
+		{
+			_repository = repository;
+		}
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult DBProviders()
+		{
+			DataProviders dataProviders = _repository.GetDBProviders();
+
+			List<DBProvider> providers = new List<DBProvider>();
+			foreach (Provider dataProvider in dataProviders)
+			{
+				providers.Add(new DBProvider() { Provider = dataProvider.ToString() });
+			}
+
+			JsonContainer<List<DBProvider>> container = new JsonContainer<List<DBProvider>>();
+			container.items = providers;
+			container.success = true;
+			container.total = dataProviders.Count;
+
+			return Json(container, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult DBDictionary(FormCollection form)
+		{
+			DatabaseDictionary dbDict = _repository.GetDBDictionary(form["scope"], form["app"]);
+			return Json(dbDict, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult TableNames(FormCollection form)
+		{
+			List<string> dataObjects = _repository.GetTableNames(
+				form["scope"], form["app"], form["dbProvider"], form["dbServer"], form["dbInstance"],
+				form["dbName"], form["dbSchema"], form["dbUserName"], form["dbPassword"], form["portNumber"]);
+
+			JsonContainer<List<string>> container = new JsonContainer<List<string>>();
+			container.items = dataObjects;
+			container.success = true;
+			container.total = dataObjects.Count;
+
+			return Json(container, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult DBObjects(FormCollection form)
+		{
+			List<JsonTreeNode> dbObjects = _repository.GetDBObjects(
+				form["scope"], form["app"], form["dbProvider"], form["dbServer"], form["dbInstance"],
+				form["dbName"], form["dbSchema"], form["dbUserName"], form["dbPassword"], form["tableNames"], form["portNumber"]);
+
+			return Json(dbObjects, JsonRequestBehavior.AllowGet);
+		}
+
+
+		public ActionResult Trees(FormCollection form)
+		{
+			string response = string.Empty;
+			
+			response = _repository.SaveDBDictionary(form["scope"], form["app"], form["tree"]);
+
+			if (response != null && response.ToUpper().Contains("ERROR"))
+			{
+				int inds = response.ToUpper().IndexOf("<MESSAGE>");
+				int inde = response.ToUpper().IndexOf(";");
+				string msg = response.Substring(inds + 9, inde - inds - 13);
+				return Json(new { success = false } + msg, JsonRequestBehavior.AllowGet);
+			}
+			return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+		}
+
+
+		public class DBProvider
+		{
+			public string Provider { get; set; }
+		}
+	}
+}
---
+++ web/iip.iringsandbox.org/Apps/Models/MappingRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Xml.Linq;
+using System.Web;
+using Ninject;
+
+
+using org.iringtools.library;
+using org.iringtools.utility;
+using org.iringtools.mapping;
+
+namespace iRINGTools.Web.Models
+{
+    public class MappingRepository : IMappingRepository
+    {
+
+        private NameValueCollection _settings = null;
+        private WebHttpClient _client = null;
+        private string _refDataServiceURI = string.Empty;
+
+        [Inject]
+        public MappingRepository()
+        {
+            _settings = ConfigurationManager.AppSettings;
+            _client = new WebHttpClient(_settings["AdapterServiceUri"]);
+        }
+
+        public Mapping GetMapping(string scopeName, string applicationName)
+        {
+            Mapping obj = null;
+
+            try
+            {
+                obj = _client.Get<Mapping>(String.Format("/{0}/{1}/mapping", scopeName, applicationName), true);
+            }
+            catch  
+            {
+            }
+
+            return obj;
+        }
+
+        public void UpdateMapping(string scopeName, string applicationName, Mapping mapping)
+        {
+          XElement mappingXml = XElement.Parse(Utility.SerializeDataContract<Mapping>(mapping));
+            try
+            {
+              _client.Post<XElement>(String.Format("/{0}/{1}/mapping", scopeName, applicationName), mappingXml, true);
+            }
+            catch  
+            {
+            }         
+        }
+    }
+}
