b8d92d9d99c8cb43fd690420045223b871045a50
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-08-10 19:46:47 +0000

    Back port changes from trunk

--- branches/2.1.x/Libraries/NHibernateLibrary/NHibernateProvider.cs
+++ branches/2.1.x/Libraries/NHibernateLibrary/NHibernateProvider.cs
@@ -674,8 +674,16 @@ namespace org.iringtools.nhibernate
 					string dataType = Utility.SqlTypeToCSharpType(Convert.ToString(metadata[1]));
 					int dataLength = Convert.ToInt32(metadata[2]);
 					bool isIdentity = Convert.ToBoolean(metadata[3]);
-					string nullable = Convert.ToString(metadata[4]).ToUpper();
-					bool isNullable = (nullable == "Y" || nullable == "TRUE");
+					bool isNullable;
+					try
+					{
+						isNullable = Convert.ToBoolean(metadata[4]);
+					}
+					catch
+					{
+						string nullable = Convert.ToString(metadata[4]).ToUpper();
+						isNullable = (nullable == "Y" || nullable == "TRUE");
+					}
 					string constraint = Convert.ToString(metadata[5]);
 
 					if (String.IsNullOrEmpty(constraint)) // process columns
