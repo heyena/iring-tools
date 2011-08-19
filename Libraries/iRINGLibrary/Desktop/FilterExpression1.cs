dd90e77e8bda3d655497ad99e83dc890e441e652
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-08-19 02:35:42 +0000

    Grid in adapter manager

---
+++ Libraries/iRINGLibrary/Desktop/FilterExpression1.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace org.iringtools.library
+{
+	[DataContract(Name = "FilterExpression1", Namespace = "http://www.iringtools.org/library")]
+	public class FilterExpression1
+	{
+		[DataMember(Name = "type", IsRequired = true, Order = 0)]
+		public string type { get; set; }
+
+		[DataMember(Name = "value", IsRequired = true, Order = 1)]
+		public string value { get; set; }
+
+		[DataMember(Name = "field", IsRequired = true, Order = 2)]
+		public string field { get; set; }
+	}
+}
