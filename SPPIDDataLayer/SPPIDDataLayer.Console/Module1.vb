63aa0096119ab04786ce77c3b2c0328d8b34c399
calislnb@gmail.com <calislnb@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-08-23 20:34:58 +0000

    Adrian first checkin

--- SPPIDDataLayer/SPPIDDataLayer.Console/Module1.vb
+++ SPPIDDataLayer/SPPIDDataLayer.Console/Module1.vb
@@ -1,8 +1,11 @@
-﻿Imports org.iringtools.adapter
+﻿
+Imports org.iringtools.adapter
 Imports Ninject
 Imports System.Collections.Specialized
 Imports org.iringtools.library
+Imports SD = StaticDust.Configuration
 Imports Ninject.Extensions.Xml
+
 Imports System.IO
 Imports System.Configuration
 
@@ -15,48 +18,101 @@ Module Module1
 
   Sub Main()
 
-    'Dim dataLayer As iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer = New iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer(New AdapterSettings(), Nothing)
+        'Dim dataLayer As iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer = New iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer(New AdapterSettings(), Nothing)
 
-    Dim ninjectSettings = New NinjectSettings() With {.LoadExtensions = False}
-    _kernel = New StandardKernel(ninjectSettings)
+        Dim ninjectSettings = New NinjectSettings() With {.LoadExtensions = False}
+        Dim spdl As iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer
+        Dim tmp As String
 
-    _kernel.Load(New XmlExtensionModule())
+        _kernel = New StandardKernel(ninjectSettings)
 
-    _kernel.Bind(Of AdapterSettings)().ToSelf().InSingletonScope()
-    _adapterSettings = _kernel.[Get](Of AdapterSettings)()
+        _kernel.Load(New XmlExtensionModule())
 
-    ' Start with some generic settings
-    _settings = New NameValueCollection()
+        _kernel.Bind(Of AdapterSettings)().ToSelf().InSingletonScope()
+        _adapterSettings = _kernel.[Get](Of AdapterSettings)()
 
-    _settings("XmlPath") = ".\12345_000\"
-    _settings("ProjectName") = "12345_000"
-    _settings("ApplicationName") = "SPPID"
+        ' Start with some generic settings
+        _settings = New NameValueCollection()
 
-    _baseDirectory = Directory.GetCurrentDirectory()
-    _baseDirectory = _baseDirectory.Substring(0, _baseDirectory.LastIndexOf("\bin"))
-    _settings("BaseDirectoryPath") = _baseDirectory
-    Directory.SetCurrentDirectory(_baseDirectory)
+        _settings("XmlPath") = ".\12345_000\"
+        _settings("ProjectDirName") = "12345_000\"
+        _settings("ProjectName") = "12345_000"
+        _settings("ApplicationName") = "SPPID"
+        _settings("BaseConfigurationPath") = _settings("XmlPath") & _settings("ProjectName")
+        _settings("BaseConcatPath") = _settings("ProjectDirName") & _settings("ProjectName")
 
-    _adapterSettings.AppendSettings(_settings)
 
-    ' Add our specific settings
-    Dim appSettingsPath As String = [String].Format("{0}12345_000.SPPID.config", _adapterSettings("XmlPath"))
 
-    If File.Exists(appSettingsPath) Then
-      'Dim appSettings As New AppSettingsReader(appSettingsPath)
-      '_adapterSettings.AppendSettings(appSettings)
-    End If
+        _baseDirectory = Directory.GetCurrentDirectory()
+        _baseDirectory = _baseDirectory.Substring(0, _baseDirectory.LastIndexOf("\bin"))
+        _settings("BaseDirectoryPath") = _baseDirectory
+        Directory.SetCurrentDirectory(_baseDirectory)
 
-    ' and run the thing
-    Dim relativePath As String = [String].Format("{0}BindingConfiguration.{1}.{2}.xml", _settings("XmlPath"), _settings("ProjectName"), _settings("ApplicationName"))
+        ' NOTE - for testing and to separate from other testers, the string '_Adrian' is inserted into the project config path
+        ' Please create a new file (replacing 'Adrian' with your name) and customize the connection strings for your own testing
+        'tmp = [String].Format("{0}_<YOUR NAME HERE>.{1}.config", _settings("BaseConcatPath"), _settings("ApplicationName"))
+        tmp = [String].Format("{0}_Adrian.{1}.config", _settings("BaseConcatPath"), _settings("ApplicationName"))
+        _settings("ProjectConfigurationPath") = Path.Combine(_baseDirectory, tmp)
 
-    ' Ninject Extension requires fully qualified path.
-    Dim bindingConfigurationPath As String = Path.Combine(_settings("BaseDirectoryPath"), relativePath)
+        tmp = [String].Format("{0}.StagingConfiguration.{1}.xml", _settings("BaseConcatPath"), _settings("ApplicationName"))
+        _settings("StagingConfigurationPath") = Path.Combine(_baseDirectory, tmp)
 
-    _kernel.Load(bindingConfigurationPath)
+        tmp = [String].Format("{0}BindingConfiguration.{1}.{2}.xml", _
+                      _settings("ProjectDirName"), _settings("ProjectName"), _settings("ApplicationName"))
 
-    _sppidDataLayer = _kernel.[Get](Of IDataLayer2)()
+        _adapterSettings.AppendSettings(_settings)
 
-  End Sub
+        ' Add our specific settings
+        Dim appSettingsPath As String = [String].Format("{0}12345_000.SPPID.config", _adapterSettings("XmlPath"))
+
+        'Testing()
+
+        If File.Exists(_settings("ProjectConfigurationPath")) Then
+            _adapterSettings.AppendSettings(New SD.AppSettingsReader(_settings("ProjectConfigurationPath")))
+        End If
+
+        ' Ninject Extension requires fully qualified path.
+
+        _kernel.Load(Path.Combine(_baseDirectory, tmp))
+        spdl = New iRINGTools.SDK.SPPIDDataLayer.SPPIDDataLayer(_adapterSettings)
+
+        '_sppidDataLayer = _kernel.[Get](Of IDataLayer2)()
+
+    End Sub
+
+    Private Sub Testing()
+
+        Dim ProjConfigPath As String = ""
+        Dim x As Xml.XmlReader
+        Dim nvc As New NameValueCollection
+
+        ' *** debug
+        ProjConfigPath = "C:\Users\dabshere\Documents\_Projects\iRing\DataLayers\SPPIDDataLayer\SPPIDDataLayer.NUnit\12345_000\12345_000_Adrian.SPPID.config"
+
+        If File.Exists(ProjConfigPath) Then
+
+            x = Xml.XmlReader.Create(ProjConfigPath)
+            x.ReadToFollowing("appSettings")
+            x.ReadToDescendant("add")
+
+            Do
+                nvc.Add(x.GetAttribute("key"), x.GetAttribute("value"))
+            Loop While x.ReadToNextSibling("add")
+
+        End If
+
+        _adapterSettings.Add(nvc)
+
+
+
+
+        'If File.Exists(ProjConfigPath) Then
+        '    Dim asr As New StaticDust.Configuration.AppSettingsReader(ProjConfigPath)
+        '    _adapterSettings.AppendSettings(asr)
+        '    Debug.Print(asr.Item(0).ToString)
+        'End If
+
+
+    End Sub
 
 End Module
---
+++ SPPIDDataLayer/SPPIDDataLayer/SPPIDWorkingSet.vb
@@ -0,0 +1,234 @@
+﻿Option Explicit On
+Option Compare Text
+
+Imports System.Data.SqlClient
+
+Public Class SPPIDWorkingSet
+
+#Region " Variables "
+
+    Private _conn As SqlConnection
+
+    Private _siteDataDT As SQLSchemaDS.SiteDataDataTable
+    Private _siteDataTA As SQLSchemaDSTableAdapters.SiteDataTableAdapter
+    Private _siteDataDV As DataView
+    Private _tablesDT As SQLSchemaDS.TablesDataTable
+    Private _tablesTA As SQLSchemaDSTableAdapters.TablesTableAdapter
+    Private _tablesDV As DataView
+    Private _columnsDT As SQLSchemaDS.ColumnsDataTable
+    Private _columnsTA As SQLSchemaDSTableAdapters.ColumnsTableAdapter
+    Private _columnsDV As DataView
+    Private _SPAPLANTSchemaName As String
+    Private _DATA_DICTIONARYSchemaName As String
+    Private _SPPIDSchemaName As String
+    Private _SPPIDDATA_DICTIONARYSchemaName As String
+    Private _SITESchemaName As String
+    Private SPQueries As SmartPlantDBQueries
+    Private _SchemaSubstitutions As Dictionary(Of String, String)
+
+#End Region
+
+#Region " Properties "
+
+    Public ReadOnly Property Connection As SqlConnection
+        Get
+            Return _conn
+        End Get
+    End Property
+
+    Public ReadOnly Property Tables As SQLSchemaDS.TablesDataTable
+        Get
+            Return _tablesDT
+        End Get
+    End Property
+
+    Public ReadOnly Property Columns As SQLSchemaDS.ColumnsDataTable
+        Get
+            Return _columnsDT
+        End Get
+    End Property
+
+    Public ReadOnly Property TablesView As DataView
+        Get
+            Return _tablesDV
+        End Get
+    End Property
+
+    Public ReadOnly Property ColumnsView(Schema As String, Table As String) As DataView
+        Get
+            Return New DataView(_columnsDT _
+                                , "TableSchema='" & Schema & "' and TableName='" & Table & "'" _
+                                , "ColumnName" _
+                                , DataViewRowState.CurrentRows)
+
+        End Get
+    End Property
+
+    Public ReadOnly Property ColumnsView() As DataView
+        Get
+            Return _columnsDT.DefaultView
+        End Get
+    End Property
+
+    Public ReadOnly Property SchemaName(SchemaType As SPSchemaType) As String
+        Get
+            Select Case SchemaType
+                Case SPSchemaType.SPAPLANT
+                    Return _SPAPLANTSchemaName
+                Case SPSchemaType.DATA_DICTIONARY
+                    Return _DATA_DICTIONARYSchemaName
+                Case SPSchemaType.SPPID
+                    Return _SPPIDSchemaName
+                Case SPSchemaType.SPPIDDATA_DICTIONARY
+                    Return _SPPIDDATA_DICTIONARYSchemaName
+                Case SPSchemaType.SITE
+                    Return _SITESchemaName
+                Case Else : Return ""
+            End Select
+        End Get
+    End Property
+
+    Public ReadOnly Property SchemaSubstitutions() As Dictionary(Of String, String)
+        Get
+            Return _SchemaSubstitutions
+        End Get
+    End Property
+
+#End Region
+
+#Region " Constructors "
+
+    Public Sub New(ProjectConnection As SqlConnection, SiteConnection As SqlConnection, SiteDataQuery As XElement)
+
+        SPQueries = New SmartPlantDBQueries
+        _SchemaSubstitutions = New Dictionary(Of String, String)
+        _siteDataTA = New SQLSchemaDSTableAdapters.SiteDataTableAdapter
+        _tablesTA = New SQLSchemaDSTableAdapters.TablesTableAdapter
+        _columnsTA = New SQLSchemaDSTableAdapters.ColumnsTableAdapter
+
+        GetBaselineSchema(SiteConnection, ProjectConnection.Database, SiteDataQuery)
+
+        _tablesTA.Connection = ProjectConnection
+        _columnsTA.Connection = ProjectConnection
+        _tablesDT.Clear()
+        _tablesDT = _tablesTA.GetData
+        _tablesDV = _tablesDT.DefaultView
+        _columnsDT = _columnsTA.GetData
+        _columnsDV = _columnsDT.DefaultView
+
+    End Sub
+
+#End Region
+
+
+#Region " Public Methods "
+
+#End Region
+
+#Region " Private Methods "
+
+    Private Sub SetSchemaType(SchemaType As SPSchemaType, Value As String)
+
+        Select Case SchemaType
+            Case SPSchemaType.SPAPLANT
+                _SPAPLANTSchemaName = Value
+            Case SPSchemaType.DATA_DICTIONARY
+                _DATA_DICTIONARYSchemaName = Value
+            Case SPSchemaType.SPPID
+                _SPPIDSchemaName = Value
+            Case SPSchemaType.SPPIDDATA_DICTIONARY
+                _SPPIDDATA_DICTIONARYSchemaName = Value
+            Case SPSchemaType.SITE
+                _SITESchemaName = Value
+        End Select
+
+    End Sub
+
+    ''' <summary>
+    ''' Derive the site schema, and from that, query the site to get the schema for the given database schema information
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub GetBaselineSchema(SiteConnection As SqlConnection, ProjectDBname As String, SiteDataQuery As XElement)
+
+        Dim SiteDR As SQLSchemaDS.SiteDataRow
+        Dim TablesDR As SQLSchemaDS.TablesRow
+        Dim drv As DataRowView
+        Dim q As String
+        Dim schemaTp As SPSchemaType
+        Dim rVal As Boolean
+        Dim queryParts As New Dictionary(Of SQLClause, String)
+        Dim replacements As IEnumerable(Of XElement) = Nothing
+        Dim declarations As IEnumerable(Of XElement) = Nothing
+        Dim setClause As String
+
+        Try
+
+            ' fetch the table information for the SITE database
+            _tablesTA.Connection = SiteConnection
+            _tablesDT = _tablesTA.GetData
+            _tablesDV = _tablesDT.DefaultView
+
+            ' fetch the column information fro the SITE database
+            _columnsTA.Connection = SiteConnection
+            _columnsDT = _columnsTA.GetData
+            _columnsDV = _columnsDT.DefaultView
+
+            ' fetch the site data specifying information about all site projects
+            _siteDataTA.Connection = SiteConnection
+            _siteDataDT = _siteDataTA.GetData
+            _siteDataDV = _siteDataDT.DefaultView
+
+        Catch ex As InvalidOperationException
+            Throw ex
+        Catch ex As Exception
+            Throw ex
+            ' to do: Exit ungracefully if the connection string does
+        End Try
+
+        ' exit if the table is not found
+        Try
+            TablesDR = _tablesDT.Select("Name='T_DB_Data'").First
+        Catch ex As Exception
+            Throw New KeyNotFoundException("The table 'T_DB_Data' cannot be found in the SPPID site database using " & _
+                                       "connection string '" & SiteConnection.ConnectionString & "'")
+        End Try
+
+        ' otherwise use the schema to form the correct query to get the SPPID schema types
+        _SITESchemaName = TablesDR.Schema
+        _SchemaSubstitutions.Clear()
+        _SchemaSubstitutions.Add(SPSchemaType.SITE.ToString, _SITESchemaName)
+        GetQueryParts(SiteDataQuery, ColumnsView, TablesView, _SchemaSubstitutions, queryParts, replacements, declarations)
+
+        ' the only SET necessary should be the database name
+        setClause = "SET @ProjectDBName='" & ProjectDBname & "'" & nltb
+        queryParts.Add(SQLClause.Set, setClause)
+        q = queryParts.BuildQuery
+
+        ' fetch the site data
+        Try
+            _siteDataTA.Adapter.SelectCommand = New SqlCommand(q, SiteConnection)
+        Catch ex As Exception
+            Throw New InvalidExpressionException("The site data query '" & q & "' is malformed")
+        End Try
+
+        _siteDataDT = _siteDataTA.GetData
+        _siteDataDV = New DataView(_siteDataDT, "SP_Schema_Type='SPAPLANT'", "SP_Schema_Type", DataViewRowState.CurrentRows)
+
+        ' set the project-specific schema set for this project
+        For Each drv In _siteDataDV
+
+            SiteDR = drv.Row
+            rVal = [Enum].TryParse(SiteDR.SP_Schema_Type, True, schemaTp)
+            SetSchemaType(schemaTp, SiteDR.Username)
+            SchemaSubstitutions.Add(SiteDR.SP_Schema_Type, SiteDR.Username)
+
+        Next
+
+    End Sub
+
+#End Region
+
+
+
+
+End Class
