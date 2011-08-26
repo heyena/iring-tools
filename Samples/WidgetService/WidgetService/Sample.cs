89e1b94022d222cd7c58ad44b555bfa3d07e1b00
neha11.b@tcs.com <neha11.b@tcs.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-08-26 12:45:40 +0000

    Restful Widget Service

---
+++ Samples/WidgetService/WidgetService/Sample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using org.iringtools.library;
+
+namespace WidgetService
+{
+    public class Sample
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
