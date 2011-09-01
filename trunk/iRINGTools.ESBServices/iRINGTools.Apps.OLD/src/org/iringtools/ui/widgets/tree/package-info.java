cd86c16a6947549c46888f9c1ecb296cc7cdeb1c
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-01 01:54:55 +0000

    a little more cleanup. java code can be found here: http://iringtools.googlecode.com/svn/trunk

--- trunk/iRINGTools.ESBServices/iRINGTools.Apps.OLD/src/org/iringtools/ui/widgets/tree/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.iringtools.org/ui/widgets/tree", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.iringtools.ui.widgets.tree;
--- trunk/iRINGTools.ESBServices/iRINGTools.Common/src/com/apigee/api/wadl/_2010/_07/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.apigee.com/wadl/2010/07/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.apigee.api.wadl._2010._07;
