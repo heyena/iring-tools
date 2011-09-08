edf91c415f00a050ddcd4b4b67f57212fa6cd14e
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-08 02:56:35 +0000

    Moved directory prototype to java branch.

--- trunk/Prototypes/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/mapping/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.iringtools.org/mapping", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.iringtools.mapping;
