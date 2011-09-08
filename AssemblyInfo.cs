b3113e56f1e1598178ba6948e820920913e5bdf5
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-08 23:11:20 +0000

    some updates to 3.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.0.0.4131")]
-[assembly: AssemblyFileVersion("3.0.0.4131")]
+[assembly: AssemblyVersion("3.0.0.4169")]
+[assembly: AssemblyFileVersion("3.0.0.4169")]
