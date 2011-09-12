aff9c39523597c233e3bd52af4eb0c3ec46d31f4
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-12 13:25:37 +0000

    Updated assembly info and model file.

--- branches/2.2.x/AssemblyInfo.cs
+++ branches/2.2.x/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.0.3107")]
-[assembly: AssemblyFileVersion("2.2.0.3107")]
+[assembly: AssemblyVersion("2.2.0.4167")]
+[assembly: AssemblyFileVersion("2.2.0.4167")]
