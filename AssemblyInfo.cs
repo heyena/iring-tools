065e37915ba19f58dc37c9b03c1dda004007b624
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-12 15:34:38 +0000

    Updated bamboo.xml.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.0.4189")]
-[assembly: AssemblyFileVersion("2.2.0.4189")]
+[assembly: AssemblyVersion("2.2.0.4191")]
+[assembly: AssemblyFileVersion("2.2.0.4191")]
