86607e001a8d83302d2f69839f077c1132822e28
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-13 18:52:00 +0000

    Revert name validation for application

--- iRINGTools.Applications/Scripts/iringtools/AdapterManager1/ApplicationPanel.js
+++ iRINGTools.Applications/Scripts/iringtools/AdapterManager1/ApplicationPanel.js
@@ -163,28 +163,25 @@ AdapterManager.ApplicationPanel = Ext.extend(Ext.Panel, {
     },
 
     onSave: function () {
-        var that = this;    // consists the main/prappNameclass object
-       
-            if (this.form.getForm().getFieldValues().Scope != this.form.getForm().getFieldValues().Name) {
-                this.form.getForm().submit({
-                    waitMsg: 'Saving Data...',
-                    success: function (f, a) {
-                        that.fireEvent('Save', that);
-                    },
-                    failure: function (f, a) {
-                        //Ext.Msg.alert('Warning', 'Error saving changes!')
-                        var message = 'Error saving changes!';
-                        showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
-                    }
-                });
-            }
-            else {
-                var message = 'Scope & Application name cannot be same!';
-                showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
-            }
-       
-    }
-   
+        var that = this;    // consists the main/prappNameclass object       
+		if (this.form.getForm().getFieldValues().Scope != this.form.getForm().getFieldValues().Name) {
+			this.form.getForm().submit({
+				waitMsg: 'Saving Data...',
+				success: function (f, a) {
+					that.fireEvent('Save', that);
+				},
+				failure: function (f, a) {
+					//Ext.Msg.alert('Warning', 'Error saving changes!')
+					var message = 'Error saving changes!';
+					showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
+				}
+			});
+		}
+		else {
+			var message = 'Scope & Application name cannot be same!';
+			showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
+		}       
+    }   
 
 });
 
