203c255b36c3a9fc70077b12922058ed337795c8
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-15 14:58:25 +0000

    Added modular demo.

---
+++ Prototypes/ModularDemo/TestModule/TestModule/ConcreteTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLibrary;
+
+namespace TestModule
+{
+  public class ConcreteTest : BaseTest
+  {
+    public override String SayHello(String name)
+    {
+      return "ConcreteTest says hello " + name;
+    }
+  }
+}
