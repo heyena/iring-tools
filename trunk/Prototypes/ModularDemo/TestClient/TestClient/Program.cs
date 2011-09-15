69251961d657655aeb1fa8106c53c5ac5ed09466
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-15 14:58:25 +0000

    Added modular demo.

---
+++ trunk/Prototypes/ModularDemo/TestClient/TestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestLibrary;
+
+namespace TestClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Type testType = Type.GetType("TestModule.ConcreteTest, TestModule");
+      ITest test = (ITest) Activator.CreateInstance(testType);
+      Console.WriteLine(test.SayHello("iRING"));
+
+      Console.ReadKey();
+    }
+  }
+}
