0c7bdad7f759f7e671194c527bd8e7deb2da7dd0
Strydom5607 <Strydom5607@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-09-26 09:03:11 +0000

    one more attempt to fix bamboo build errors

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -2,11 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
+
+
+
+
+
+
 [assembly: AssemblyCompany("iringug.org")]
 [assembly: AssemblyProduct("iRINGTools")]
 [assembly: AssemblyCopyright("Copyright © 2011, iringug.org")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("3.0.0.4263")]
-[assembly: AssemblyFileVersion("3.0.0.4263")]
+[assembly: AssemblyVersion("3.0.0.4264")]
+[assembly: AssemblyFileVersion("3.0.0.4264")]
