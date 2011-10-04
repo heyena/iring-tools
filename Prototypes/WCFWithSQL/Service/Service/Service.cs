11484f281ba90dd176958c6f7be2f594a504f114
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-10-04 21:04:26 +0000

    Just using svn to transfer files to iringsandbox.

---
+++ Prototypes/WCFWithSQL/Service/Service/Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace WCFWithSQL
+{
+  [ServiceContract]
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+  public class Service
+  {
+    [WebGet(UriTemplate = "/reset")]
+    public string Reset()
+    {
+      OutgoingWebResponseContext context = WebOperationContext.Current.OutgoingResponse;
+      context.ContentType = "application/xml";
+
+      Provider provider = new Provider();
+      bool success = provider.ResetDB();
+
+      return success ? "DB reset successfully" : "Error reseting DB";
+    }
+  }
+}
