4a72f5050eb19ae9d047cc6dc3aadd6a31abc89c
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-10-12 17:49:46 +0000

    Adapter manager show directory from java service, and fixed mapping controller so that it can use info from new directory

---
+++ trunk/Prototypes/iRINGTools.Applications/Controllers/GridManagerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Xml;
+
+using org.iringtools.library;
+using org.iringtools.utility;
+using org.iringtools.mapping;
+
+using iRINGTools.Web.Helpers;
+using iRINGTools.Web.Models;
+using org.ids_adi.qmxf;
+using VDS.RDF;
+using System.Text;
+using log4net;
+
+namespace org.iringtools.web.controllers
+{
+	public class GridManagerController : BaseController
+	{
+		private static readonly ILog _logger = LogManager.GetLogger(typeof(GridManagerController));
+		private IGridRepository _repository { get; set; }
+		private Grid pageDtoGrid;		
+
+		public GridManagerController() : this(new GridRepository()) { }
+
+		public GridManagerController(IGridRepository repository)
+		{
+			_repository = repository;			
+		}
+
+		public ActionResult Pages(FormCollection form)
+		{
+			_logger.Error("GridManagerController -- Pages");
+			JsonContainer<Grid> container = new JsonContainer<Grid>();
+			pageDtoGrid = _repository.getGrid(form["scope"], form["app"], form["graph"], form["filter"], form["sort"], form["dir"], form["start"], form["limit"]);
+
+			string response = _repository.getResponse();
+			if (response != "")
+			{
+				return Json(new { success = false } + response, JsonRequestBehavior.AllowGet);
+			}
+
+			return Json(pageDtoGrid, JsonRequestBehavior.AllowGet);
+		}		
+	}
+}
---
+++ trunk/Prototypes/iRINGTools.Applications/Models/RefDataRepository.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Xml.Linq;
+using System.Web;
+
+using org.iringtools.library;
+using org.iringtools.utility;
+using org.iringtools.mapping;
+using org.ids_adi.qmxf;
+using Ninject;
+
+namespace iRINGTools.Web.Models
+{
+  public class RefDataRepository : IRefDataRepository
+  {
+    private NameValueCollection _settings = null;
+    private WebHttpClient _client = null;
+    private string _refDataServiceURI = string.Empty;
+    private string relativeUri = string.Empty;
+
+    [Inject]
+    public RefDataRepository()
+    {
+      _settings = ConfigurationManager.AppSettings;
+      _client = new WebHttpClient(_settings["ReferenceDataServiceUri"]);
+    }
+
+    public RefDataEntities Search(string query, int start, int limit)
+    {
+      relativeUri = string.Format("/search/{0}/{1}/{2}", query, start, limit);
+      return _client.Get<RefDataEntities>(relativeUri);
+    }
+
+    public RefDataEntities Search(string query)
+    {
+      relativeUri = string.Format("/search/{0}/0/0", query);
+      return _client.Get<RefDataEntities>(relativeUri);
+    }
+
+    public RefDataEntities SearchReset(string query)
+    {
+      relativeUri = string.Format("/search/{0}/reset", query);
+      return  _client.Get<RefDataEntities>(relativeUri);
+       
+    }
+
+    public Entity GetClassLabel(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}/label", classId);
+      return _client.Get<Entity>(relativeUri);
+    }
+
+    public Entities GetSubClasses(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}/subclasses", classId);
+      return _client.Get<Entities>(relativeUri);
+    }
+
+    public Entities GetSubClassesCount(string classId)
+    {
+        relativeUri = string.Format("/classes/{0}/subclasses/count", classId);
+        return _client.Get<Entities>(relativeUri);
+    }
+
+  
+
+    public Entities GetSuperClasses(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}/superclasses", classId);
+      return _client.Get<Entities>(relativeUri);
+    }
+
+    public Entities GetClassTemplates(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}/templates", classId);
+      return _client.Get<Entities>(relativeUri);
+    }
+
+    public Entities GetClassTemplatesCount(string classId)
+    {
+        relativeUri = string.Format("/classes/{0}/templates/count", classId);
+        return _client.Get<Entities>(relativeUri);
+    }
+
+    public QMXF GetClasses(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}", classId);
+      return _client.Get<QMXF>(relativeUri);
+    }
+
+    public QMXF GetTemplate(string id)
+    {
+      relativeUri = string.Format("/templates/{0}", id);
+      return _client.Get<QMXF>(relativeUri);
+    }
+
+    public Entities GetClassMembers(string classId)
+    {
+      relativeUri = string.Format("/classes/{0}/members", classId);
+      return _client.Get<Entities>(relativeUri);
+    }
+
+  }
+}
