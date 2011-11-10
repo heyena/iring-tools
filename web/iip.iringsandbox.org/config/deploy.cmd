71d34e93eefb880150f0728d7fe2044d1a27ef20
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-11-10 16:27:11 +0000

    More iip deployment update.

--- web/iip.iringsandbox.org/config/deploy.cmd
+++ web/iip.iringsandbox.org/config/deploy.cmd
@@ -6,8 +6,11 @@ set tomcat_home="c:\Program Files\Apache Software Foundation\Tomcat 6.0\"
 
 cd %~dp0%
 svn update
-copy %java_dist%iringtools-apps.war %tomcat_home%iipapps.war
-copy %java_dist%iringtools-services.war %tomcat_home%iipservices.war
+
+copy %java_dist%iringtools-apps.war %tomcat_home%webapps\iipapps.war
+copy %java_dist%iringtools-services.war %tomcat_home%webapps\iipservices.war
+rem give tomcat 30 seconds to deploy war files
+ping -n 30 localhost
 xcopy tomcat\*.* %tomcat_home% /s /y
 
 xcopy iis\*.* ..\ /s /y /d
