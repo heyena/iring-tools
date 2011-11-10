864ddae335e97f1654baf88046aa4cb38dccd3a6
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-11-10 22:26:22 +0000

    Update dev env.

--- web/dev.iringsandbox.org/config/deploy.cmd
+++ web/dev.iringsandbox.org/config/deploy.cmd
@@ -1,4 +1,16 @@
+@echo off
+
+setlocal
+set java_dist="c:\bamboo-home\xml-data\build-dir\IRT-TRUNK-JAVA\dist\"
+set tomcat_home="c:\Program Files\Apache Software Foundation\Tomcat 6.0\"
+
 cd %~dp0%
 svn update
-xcopy iis\*.* ..\ /s /y
-xcopy tomcat\*.* "C:\Program Files\Apache Software Foundation\Tomcat 6.0" /s /y
+
+copy %java_dist%iringtools-apps.war %tomcat_home%webapps\devapps.war
+copy %java_dist%iringtools-services.war %tomcat_home%webapps\devservices.war
+rem give tomcat 30 seconds to deploy war files
+ping -n 30 localhost
+xcopy tomcat\*.* %tomcat_home% /s /y
+
+xcopy iis\*.* ..\ /s /y /d
