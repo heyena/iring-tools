63ce589e49877e7242940276f0c3f6ee8c3090ef
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-11-30 15:22:57 +0000

    removed double update fixed build and package cmds removed old files

--- package.cmd
+++ package.cmd
@@ -4,5 +4,5 @@ set basedir=%~dp0
 set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
 
 echo Creating Deployment Packages ...
-msbuild bamboo.xml /t:CreatePackages
+msbuild build.xml /t:CreatePackages
 pause
