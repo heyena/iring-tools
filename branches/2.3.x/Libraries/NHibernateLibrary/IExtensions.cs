347775c638d13b7de4a3724efecaff4fb9cf4291
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2011-12-02 19:27:20 +0000

    added nhibernate extensions interfaces

---
+++ branches/2.3.x/Libraries/NHibernateLibrary/IExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.library;
+
+namespace org.iringtools.nhibernate
+{
+  public interface IAuthorization
+  {
+    DataFilter Authorize(DataFilter dataFilter);
+  }
+
+  public interface ISummary
+  {
+    IList<Object> GetSummary();
+  }
+}
