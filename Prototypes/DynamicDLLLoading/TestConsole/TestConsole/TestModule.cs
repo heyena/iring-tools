f3b614f479ee72ada3fecbb48055f1b766af908b
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-01-23 20:15:20 +0000

    Add DynamicDLLLoading prototype, which loads DLLs in other locations than bin.

---
+++ Prototypes/DynamicDLLLoading/TestConsole/TestConsole/TestModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+using ClassLibrary;
+
+namespace TestConsole
+{
+  public class TestModule : NinjectModule
+  {
+    public override void Load()
+    {
+      Bind<GlobalSettings>().ToSelf().InSingletonScope();
+      Bind<Projection>().ToSelf().InRequestScope();
+      Bind<IDataLayer>().To<BarDataLayer>().Named("bar");
+    }
+  }
+}
