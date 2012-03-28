1f1bffbcd6ab1bda429c208ecaff86b21fb1ff53
neha11.b@tcs.com <neha11.b@tcs.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-03-28 08:29:52 +0000

    Minor tweaks in Search Panel UI

--- trunk/iRINGTools.Applications/Scripts/AM/view/search/SearchTree.js
+++ trunk/iRINGTools.Applications/Scripts/AM/view/search/SearchTree.js
@@ -10,43 +10,41 @@
     animate: true,
     selModel: Ext.create('Ext.selection.RowModel', { singleSelect: true }),
     lines: true,
+    store: Ext.create('Ext.data.TreeStore', {
+        model: 'AM.model.SearchModel',
+        autoLoad: false,
+        storeId: this.id,
+        clearOnLoad: true,
+        root: {
+            expanded: true
+        },
+        proxy: {
+            url: 'refdata/getnode',
+            type: 'ajax',
+            timeout: 600000,
+            actionMethods: { read: 'POST' },
+            extraParams: {
+                id: null,
+                type: null,
+                query: null,
+                reset: null,
+                limit: null,
+                start: 0
+            },
+            reader: { type: 'json' }
+        }
+    }),
+    viewConfig: {
+        plugins: {
+            ptype: 'treeviewdragdrop',
+            dragGroup: 'refdataGroup',
+            enableDrop: false,
+            dragText: '{0}',
+            dragField: 'text'
+        }
+    },
     initComponent: function () {
         var me = this;
-        Ext.apply(me, {
-            store: Ext.create('Ext.data.TreeStore', {
-                model: 'AM.model.SearchModel',
-                autoLoad: false,
-                storeId: me.id,
-                clearOnLoad: true,
-                root: {
-                    expanded: true
-                },
-                proxy: {
-                    url: 'refdata/getnode',
-                    type: 'ajax',
-                    timeout: 600000,
-                    actionMethods: { read: 'POST' },
-                    extraParams: {
-                        id: null,
-                        type: null,
-                        query: null,
-                        reset: null,
-                        limit: null,
-                        start: 0
-                    },
-                    reader: { type: 'json' }
-                }
-            }),
-            viewConfig: {
-                plugins: {
-                    ptype: 'treeviewdragdrop',
-                    dragGroup: 'refdataGroup',
-                    enableDrop: false,
-                    dragText: '{0}',
-                    dragField: 'text'
-                }
-            }
-        });
         me.callParent(arguments);
     }
 });
