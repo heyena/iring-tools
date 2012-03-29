0c2420967db667ab69f6d7260774766cc282afd5
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-03-29 18:12:14 +0000

    updated sample

--- Samples/UtilitySample/UtilitySample/Program.cs
+++ Samples/UtilitySample/UtilitySample/Program.cs
@@ -9,6 +9,24 @@ namespace UtilitySample
     {
         static void Main(string[] args)
         {
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+            ServiceSettings settings = new ServiceSettings();
+            settings.AppendSettings(appSettings);
+
+            WebProxyCredentials webProxyCredentials = settings.GetWebProxyCredentials();
+
+            WebHttpClient client = new WebHttpClient(settings["BaseUri"],
+                webProxyCredentials.userName,
+                webProxyCredentials.password,
+                webProxyCredentials.domain,
+                webProxyCredentials.proxyHost,
+                webProxyCredentials.proxyPort);
+
+            client.AccessToken = settings["AccessToken"];
+
+            Stream stream = client.GetStream(settings["RelativeUri"]);
+
+            Utility.WriteStream(stream, @"..\..\Test.pdf");
         }
     }
 }
