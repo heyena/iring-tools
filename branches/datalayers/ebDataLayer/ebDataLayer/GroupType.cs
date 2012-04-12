3f4371a97f40beda1d25dc09ef4b04052fc6e8da
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-04-12 21:40:20 +0000

    Some cleanup.

---
+++ branches/datalayers/ebDataLayer/ebDataLayer/GroupType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.iringtools.adapter.datalayer
+{
+  public enum GroupType
+  {
+    Tag = 1,
+    Document = 17
+  }
+}
