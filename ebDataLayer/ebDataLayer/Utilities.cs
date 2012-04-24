098dcc9397e39ca312c54b8a5c27c123882314e7
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-04-24 18:55:39 +0000

    Add utility to convert data filter to EQL filter.

--- ebDataLayer/ebDataLayer/Utilities.cs
+++ ebDataLayer/ebDataLayer/Utilities.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using org.iringtools.library;
 using System.Text.RegularExpressions;
+using org.iringtools.utility;
 
 namespace org.iringtools.adaper.datalayer.eb
 {
   public static class Utilities
   {
-    public static string RELATED_COLUMN_SUFFIX = "(Related)";
+    public static string RELATED_COLUMN_TOKEN = "(Related)";
 
     public static DataType ToCSharpType(string ebType)
     {
@@ -33,6 +34,30 @@ namespace org.iringtools.adaper.datalayer.eb
     public static string ToPropertyName(string columnName)
     {
       return Regex.Replace(columnName, @" |\.", "");
+    }
+
+    public static string ToSqlWhereClause(DataFilter filter, DataObject objDef)
+    {
+      DatabaseDictionary dbDictionary = new DatabaseDictionary();
+      dbDictionary.Provider = "SQL Server";
+
+      DataObject newObjDef = Utility.CloneDataContractObject<DataObject>(objDef);
+      
+      foreach (DataProperty prop in newObjDef.dataProperties)
+      {
+        if (prop.isReadOnly == false && prop.columnName.EndsWith(RELATED_COLUMN_TOKEN))
+        {
+          prop.columnName = prop.columnName.Replace(RELATED_COLUMN_TOKEN, string.Empty);
+        }
+        else
+        {
+          prop.columnName = string.Format("Attributes[\"Global\", \"{0}\"].Value", prop.columnName);
+        }
+      }
+      
+      dbDictionary.dataObjects.Add(newObjDef);
+
+      return filter.ToSqlWhereClause(dbDictionary, newObjDef.tableName, string.Empty);
     }
   }
 }
