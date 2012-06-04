0d5e0de6218859589f44feee1f9759179d327b43
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-06-04 15:51:36 +0000

    Fix manifest valueListMaps data contract.

--- iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/dxfr/manifest/ObjectFactory.java
+++ iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/dxfr/manifest/ObjectFactory.java
@@ -30,59 +30,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link Role }
-     * 
-     */
-    public Role createRole() {
-        return new Role();
-    }
-
-    /**
-     * Create an instance of {@link Graph }
-     * 
-     */
-    public Graph createGraph() {
-        return new Graph();
-    }
-
-    /**
-     * Create an instance of {@link Templates }
-     * 
-     */
-    public Templates createTemplates() {
-        return new Templates();
-    }
-
-    /**
      * Create an instance of {@link Manifest }
      * 
      */
     public Manifest createManifest() {
         return new Manifest();
-    }
-
-    /**
-     * Create an instance of {@link Template }
-     * 
-     */
-    public Template createTemplate() {
-        return new Template();
-    }
-
-    /**
-     * Create an instance of {@link Roles }
-     * 
-     */
-    public Roles createRoles() {
-        return new Roles();
-    }
-
-    /**
-     * Create an instance of {@link ClassTemplates }
-     * 
-     */
-    public ClassTemplates createClassTemplates() {
-        return new ClassTemplates();
     }
 
     /**
@@ -94,11 +46,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link Class }
+     * Create an instance of {@link Templates }
      * 
      */
-    public Class createClass() {
-        return new Class();
+    public Templates createTemplates() {
+        return new Templates();
     }
 
     /**
@@ -107,6 +59,54 @@ public class ObjectFactory {
      */
     public Graphs createGraphs() {
         return new Graphs();
+    }
+
+    /**
+     * Create an instance of {@link Role }
+     * 
+     */
+    public Role createRole() {
+        return new Role();
+    }
+
+    /**
+     * Create an instance of {@link ClassTemplates }
+     * 
+     */
+    public ClassTemplates createClassTemplates() {
+        return new ClassTemplates();
+    }
+
+    /**
+     * Create an instance of {@link Graph }
+     * 
+     */
+    public Graph createGraph() {
+        return new Graph();
+    }
+
+    /**
+     * Create an instance of {@link Class }
+     * 
+     */
+    public Class createClass() {
+        return new Class();
+    }
+
+    /**
+     * Create an instance of {@link Roles }
+     * 
+     */
+    public Roles createRoles() {
+        return new Roles();
+    }
+
+    /**
+     * Create an instance of {@link Template }
+     * 
+     */
+    public Template createTemplate() {
+        return new Template();
     }
 
 }
--- iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/mapping/Mapping.java
+++ iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/mapping/Mapping.java
@@ -19,7 +19,7 @@ import javax.xml.bind.annotation.XmlType;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element ref="{http://www.iringtools.org/mapping}graphMaps"/>
- *         &lt;element ref="{http://www.iringtools.org/mapping}valueListMaps"/>
+ *         &lt;element name="valueListMaps" type="{http://www.iringtools.org/mapping}ValueListMaps"/>
  *         &lt;element name="version" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
