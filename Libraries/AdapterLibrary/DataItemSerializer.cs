3d874086da7375367f2c23aa6207618d577a7a1b
chaojin_xu@yahoo.com <chaojin_xu@yahoo.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-06-13 15:30:19 +0000

    To use UTF8 instead of Default for Encoding

--- Libraries/AdapterLibrary/DataItemSerializer.cs
+++ Libraries/AdapterLibrary/DataItemSerializer.cs
@@ -41,7 +41,7 @@ namespace org.iringtools.adapter
           serializer.MaxJsonLength = int.MaxValue;
           serializer.RegisterConverters(new JavaScriptConverter[] { _converter });
           string json = serializer.Serialize(graph);
-          byte[] byteArray = Encoding.Default.GetBytes(json);
+          byte[] byteArray = Encoding.UTF8.GetBytes(json);
           stream = new MemoryStream(byteArray);
         }
       }
