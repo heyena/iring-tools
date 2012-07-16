7d22eba1dfc06f468166503e7b3c9379c6d9891c
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-07-16 21:22:21 +0000

    Fix DTI internal identifier bug.

--- branches/2.4.x/AssemblyInfo.cs
+++ branches/2.4.x/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.4.0.5079")]
-[assembly: AssemblyFileVersion("2.4.0.5079")]
+[assembly: AssemblyVersion("2.4.0.5237")]
+[assembly: AssemblyFileVersion("2.4.0.5237")]
