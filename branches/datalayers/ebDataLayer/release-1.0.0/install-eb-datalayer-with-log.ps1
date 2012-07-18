6968bfab3da517423e9cbf704a367edc0f5c61c2
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-07-18 14:55:01 +0000

    Fix a typo.

--- branches/datalayers/ebDataLayer/release-1.0.0/install-eb-datalayer-with-log.ps1
+++ branches/datalayers/ebDataLayer/release-1.0.0/install-eb-datalayer-with-log.ps1
@@ -3,4 +3,4 @@
 param($context, $app, $community)
 
 $base_path = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-& $base_path\install-iw-datalayer.ps1 $context $app $community > $env:temp\install-eb-datalayer.log 2>&1
+& $base_path\install-eb-datalayer.ps1 $context $app $community > $env:temp\install-eb-datalayer.log 2>&1
