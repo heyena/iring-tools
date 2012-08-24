2a7362c3f291932e8b91c9c5fa1fe5d1061fc8e1
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-08-24 14:30:18 +0000

    Created database dictionary which is generated from a configuration file

--- SmartPlant3D/SP3DDataLayer/BusinessObjectConfiguration.cs
+++ SmartPlant3D/SP3DDataLayer/BusinessObjectConfiguration.cs
@@ -32,7 +32,7 @@ using System.Linq;
 
 namespace iringtools.sdk.sp3ddatalayer
 {
-  [DataContract(Name = "businessObjectDictionary", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
+  [DataContract(Name = "businessObjectConfiguration", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
   public class BusinessObjectConfiguration
   {
     public BusinessObjectConfiguration()
@@ -53,40 +53,27 @@ namespace iringtools.sdk.sp3ddatalayer
     public BusinessObject GetTableObject(string name)
     {
       BusinessObject BusinessObject = null;
-      BusinessObject = this.businessObjects.FirstOrDefault<BusinessObject>(o => (o.interfaceName + "." + o.objectName).ToLower() == name.ToLower());
+      BusinessObject = this.businessObjects.FirstOrDefault<BusinessObject>(o => (o.objectName).ToLower() == name.ToLower());
       return BusinessObject;
     }
   }
 
-  [DataContract(Name = "businessObject", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
-  public class BusinessObject
+  [DataContract(Name = "businessInterface", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
+  public class BusinessInterface
   {
-    public BusinessObject()
+    public BusinessInterface()
     {
       businessProperties = new List<BusinessProperty>();
-      businessRelationships = new List<BusinessRelationship>();
     }
 
     [DataMember(IsRequired = true, Order = 0)]
     public string interfaceName { get; set; }
 
-    [DataMember(IsRequired = true, Order = 1)]
-    public string objectName { get; set; }
+    [DataMember(IsRequired = true, Order = 2)]
+    public string tableName { get; set; }
 
-    [DataMember(IsRequired = false, Order = 2, EmitDefaultValue = false)]
+    [DataMember(IsRequired = true, Order = 3)]
     public List<BusinessProperty> businessProperties { get; set; }
-
-    [DataMember(IsRequired = false, Order = 3, EmitDefaultValue = false)]
-    public List<BusinessRelationship> businessRelationships { get; set; }
-
-    [DataMember(IsRequired = false, Order = 4, EmitDefaultValue = false)]
-    public bool isReadOnly { get; set; }
-
-    [DataMember(IsRequired = false, Order = 5, EmitDefaultValue = false)]
-    public string description { get; set; }
-
-    [DataMember(IsRequired = false, Order = 6, EmitDefaultValue = false)]
-    public DataFilter dataFilter { get; set; }
 
     public bool deleteProperty(BusinessProperty businessProperty)
     {
@@ -102,39 +89,139 @@ namespace iringtools.sdk.sp3ddatalayer
     }
   }
 
+  [DataContract(Name = "businessObject", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
+  public class BusinessObject
+  {
+    public BusinessObject()
+    {
+      businessKeyProperties = new List<BusinessKeyProperty>();
+      businessInterfaces = new List<BusinessInterface>();
+      businessRelationships = new List<BusinessRelationship>();
+    }
+
+    [DataMember(IsRequired = true, Order = 0)]
+    public string objectName { get; set; }
+
+    [DataMember(IsRequired = true, Order = 1)]
+    public List<BusinessKeyProperty> businessKeyProperties { get; set; }
+
+    [DataMember(IsRequired = false, Order = 2, EmitDefaultValue = false)]
+    public List<BusinessInterface> businessInterfaces { get; set; }
+
+    [DataMember(IsRequired = false, Order = 3, EmitDefaultValue = false)]
+    public List<BusinessRelationship> businessRelationships { get; set; }
+
+    [DataMember(IsRequired = false, Order = 4, EmitDefaultValue = false)]
+    public bool isReadOnly { get; set; }
+
+    [DataMember(IsRequired = false, Order = 5, EmitDefaultValue = false)]
+    public string description { get; set; }
+
+    [DataMember(IsRequired = false, Order = 6, EmitDefaultValue = false)]
+    public DataFilter dataFilter { get; set; }
+
+    public bool isKeyProperty(string propertyName)
+    {
+      foreach (BusinessKeyProperty keyProperty in businessKeyProperties)
+      {
+        if (keyProperty.keyPropertyName.ToLower() == propertyName.ToLower())
+          return true;
+      }
+
+      return false;
+    }
+
+    public BusinessProperty getKeyProperty(string keyPropertyName, string interfaceName)
+    {
+      foreach (BusinessInterface businessInterface in businessInterfaces)
+      {
+        if (businessInterface.interfaceName.ToLower() == interfaceName.ToLower())
+        {
+          if (businessInterface.businessProperties.Count > 0)
+          {
+            foreach (BusinessProperty businessProperty in businessInterface.businessProperties)
+            {
+              if (businessProperty != null)
+                if (businessProperty.propertyName.ToLower() == keyPropertyName.ToLower())
+                  return businessProperty;
+            }
+          }
+          else
+            return null;
+        }
+      }
+
+      return null;
+    }
+
+    public bool deleteProperty(BusinessProperty dataProperty)
+    {
+      foreach (BusinessInterface businessInterface in businessInterfaces)
+      {
+        foreach (BusinessProperty property in businessInterface.businessProperties)
+        {
+          if (dataProperty == property)
+          {
+            businessInterface.businessProperties.Remove(dataProperty);
+            break;
+          }
+        }
+      }
+
+      foreach (BusinessKeyProperty keyProperty in businessKeyProperties)
+      {
+        if (keyProperty.keyPropertyName.ToLower() == dataProperty.propertyName.ToLower())
+        {
+          businessKeyProperties.Remove(keyProperty);
+          break;
+        }
+      }
+      return true;
+    }
+
+    public bool addKeyProperty(BusinessProperty keyProperty, string interfaceName)
+    {
+      foreach (BusinessInterface businessInterface in businessInterfaces)
+      {
+        if (businessInterface.interfaceName.ToLower() == interfaceName.ToLower())
+        {
+          businessInterface.businessProperties.Add(keyProperty);
+        }
+        return false;
+      }
+
+      this.businessKeyProperties.Add(new BusinessKeyProperty { keyPropertyName = keyProperty.propertyName });
+      return true;
+    }
+  }
+
+  [DataContract(Name = "businessKeyProperty", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
+  public class BusinessKeyProperty
+  {
+    [DataMember(IsRequired = true, Order = 0)]
+    public string keyPropertyName { get; set; }    
+  }
+
   [DataContract(Name = "businessProperty", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
   public class BusinessProperty
   {
-    [DataMember(IsRequired = false, Order = 0, EmitDefaultValue = false)]
-    public string tableName { get; set; }
-
-    [DataMember(IsRequired = true, Order = 1)]
-    public string interfaceName { get; set; }
-
-    [DataMember(IsRequired = true, Order = 2)]
+    [DataMember(IsRequired = true, Order = 0)]
     public string propertyName { get; set; }
 
+    [DataMember(IsRequired = false, Order = 1, EmitDefaultValue = false)]
+    public string dataType { get; set; }
+
+    [DataMember(IsRequired = false, Order = 2, EmitDefaultValue = false)]
+    public bool isNullable { get; set; }
+
     [DataMember(IsRequired = false, Order = 3, EmitDefaultValue = false)]
-    public DataType dataType { get; set; }    
-
-    [DataMember(IsRequired = false, Order = 4, EmitDefaultValue = false)]
-    public bool isNullable { get; set; }   
-
-    [DataMember(IsRequired = false, Order = 5, EmitDefaultValue = false)]
     public bool isReadOnly { get; set; }
 
-    [DataMember(IsRequired = false, Order = 6, EmitDefaultValue = false)]
+    [DataMember(IsRequired = false, Order = 4, EmitDefaultValue = false)]
     public string description { get; set; }
-  }
 
-  [DataContract(Name = "businessRelatedProperty", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
-  public class BusinessRelatedProperty
-  {
-    [DataMember(IsRequired = true)]
-    public string relatedInterfaceName { get; set; }
-
-    [DataMember(IsRequired = true)]
-    public string relatedPropertyName { get; set; }
+    [DataMember(IsRequired = false, Order = 5, EmitDefaultValue = false)]
+    public string dbColumn { get; set; }
   }
 
   [DataContract(Name = "businessRelationship", Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
@@ -142,11 +229,11 @@ namespace iringtools.sdk.sp3ddatalayer
   {
     public BusinessRelationship()
     {
-      this.BusinessRelatedProperties = new List<BusinessRelatedProperty>();
+      this.businessRelatedInterfaces = new List<BusinessInterface>();
     }
 
-    [DataMember(Order = 0, Name = "businessRelatedProperties", IsRequired = true)]
-    public List<BusinessRelatedProperty> BusinessRelatedProperties { get; set; }
+    [DataMember(Order = 0, Name = "businessRelatedInterfaces", IsRequired = true)]
+    public List<BusinessInterface> businessRelatedInterfaces { get; set; }
 
     [DataMember(Order = 1, Name = "relatedObjectName", IsRequired = true)]
     public string relatedObjectName { get; set; }
@@ -155,34 +242,4 @@ namespace iringtools.sdk.sp3ddatalayer
     public string relationshipName { get; set; }
   }
 
-  [DataContract(Namespace = "http://www.iringtools.sdk/sp3ddatalayer")]
-  public enum DataType
-  {
-    [EnumMember]
-    @Boolean,
-    [EnumMember]
-    @Byte,
-    [EnumMember]
-    @Char,
-    [EnumMember]
-    @DateTime,
-    [EnumMember]
-    @TimeStamp,
-    [EnumMember]
-    @Decimal,
-    [EnumMember]
-    @Double,
-    [EnumMember]
-    @Int16,
-    [EnumMember]
-    @Int32,
-    [EnumMember]
-    @Int64,
-    [EnumMember]
-    @Single,
-    [EnumMember]
-    @String,
-    [EnumMember]
-    @Reference
-  }
 }
