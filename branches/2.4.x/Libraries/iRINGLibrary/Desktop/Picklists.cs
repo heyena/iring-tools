8bc5affd733c24dc2fef8156d72f1aca95bd41fe
rdecarlo73@gmail.com <rdecarlo73@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-09-13 18:51:35 +0000

    Tweaked Picklists for consistent response structure.

--- branches/2.4.x/Libraries/iRINGLibrary/Desktop/Picklists.cs
+++ branches/2.4.x/Libraries/iRINGLibrary/Desktop/Picklists.cs
@@ -7,57 +7,28 @@ using System.Runtime.Serialization;
 namespace org.iringtools.library
 {
   [DataContract(Namespace = "http://www.iringtools.org/data", Name = "Picklists")]
-  public class Picklists
+  public class Picklists : DataItems
   {
-    public Picklists()
-    {
-      picklistItems = new List<PicklistItem>();
-    }
-
-    [DataMember(IsRequired = true, Order = 0)]
-    public string name { get; set; }
+    public Picklists() : base()
+    {}
 
     [DataMember(IsRequired = false, Order = 1)]
     public string title { get; set; }
 
     [DataMember(IsRequired = false, Order = 2)]
     public int valueColumnIndex { get; set; }
-
-    [DataMember(Name = "total", Order = 3)]
-    public long total { get; set; }
-
-    [DataMember(Name = "start", Order = 4)]
-    public int start { get; set; }
-
-    [DataMember(Name = "limit", Order = 5)]
-    public int limit { get; set; }
-
-    [DataMember(IsRequired = true, Order = 6)]
-    public List<PicklistItem> picklistItems { get; set; }
   }
 
   [DataContract(Namespace = "http://www.iringtools.org/data", Name = "PicklistItem")]
-  public class PicklistItem
-  {
-    public PicklistItem()
-    {
-      picklistColumns = new Dictionary<string, string>();
-      links = new List<Link>();
-    }
+  public class PicklistItem : DataItem
+  {}
 
-    [DataMember(IsRequired = true, Order = 0)]
-    public Dictionary<string, string> picklistColumns { get; set; }
-    
-    [DataMember(Name = "links", Order = 1, EmitDefaultValue = false)]
-    public List<Link> links { get; set; }
-  }
+  //public class PicklistColumn
+  //{
+  //  [DataMember(IsRequired = true, Order = 0)]
+  //  public string columnName { get; set; }
 
-  public class PicklistColumn
-  {
-    [DataMember(IsRequired = true, Order = 0)]
-    public string columnName { get; set; }
-
-    [DataMember(IsRequired = true, Order = 1)]
-    public string columnValue { get; set; }
-  }
+  //  [DataMember(IsRequired = true, Order = 1)]
+  //  public string columnValue { get; set; }
+  //}
 }
