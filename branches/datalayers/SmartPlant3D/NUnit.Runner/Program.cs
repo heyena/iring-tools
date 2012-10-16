21a859b0279709f6af6cb249de9997c8028d9432
sophia21703@gmail.com <sophia21703@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-10-16 15:15:50 +0000

    Add missing files

---
+++ branches/datalayers/SmartPlant3D/NUnit.Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Gui;
+
+namespace org.iringtools.test
+{
+  class Program
+  {
+    [STAThread]
+    static void Main()
+    {
+      string commandLine = @"/run ../../../SPPIDDataLayer.Tests/SPPIDDataLayer.Tests.csproj";
+
+      string[] args = commandLine.Split(' ');
+      AppEntry.Main(args);
+    }
+  }
+}
