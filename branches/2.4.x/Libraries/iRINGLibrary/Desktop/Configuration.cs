b76d75031e15f0c684d2209d59d989e79faaa6d1
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-10-23 20:25:52 +0000

    Add get and post app settings to adapter service.

---
+++ branches/2.4.x/Libraries/iRINGLibrary/Desktop/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace org.iringtools.library
+{
+  [XmlRoot(Namespace = "", ElementName = "configuration")]
+  public class Configuration
+  {
+    [XmlElement(ElementName = "appSettings")]
+    public AppSettings AppSettings { get; set; }
+  }
+
+  public class AppSettings
+  {
+    [XmlElement(ElementName = "add")]
+    public List<AppSetting> Settings { get; set; }
+  }
+
+  public class AppSetting
+  {
+    [XmlAttribute(AttributeName = "key")]
+    public String Key { get; set; }
+
+    [XmlAttribute(AttributeName = "value")]
+    public String Value { get; set; }
+  }
+}
