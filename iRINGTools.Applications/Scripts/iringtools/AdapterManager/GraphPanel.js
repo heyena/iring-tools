8b704751fe6d64da257d1f15b7469e8655e5be6a
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2012-11-26 04:41:37 +0000

    Fix graphmap editing issue with IE.

--- iRINGTools.Applications/Scripts/iringtools/AdapterManager/GraphPanel.js
+++ iRINGTools.Applications/Scripts/iringtools/AdapterManager/GraphPanel.js
@@ -1,39 +1,20 @@
 ﻿Ext.ns('AdapterManager');
-/**
-* @class AdapterManager.GraphPanel
-* @extends Panel
-* @author by Gert Jansen van Rensburg
-*/
-AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
+
+AdapterManager.GraphPanel = Ext.extend(Ext.Window, {
   layout: 'fit',
-  border: false,
-  frame: false,
-  split: true,
+  width: 440,
+  height: 330,
+  closable: true,
+  resizable: false,
+  modal: false,
   form: null,
   record: null,
   url: null,
   node: null,
-  resizable: false,
   identifierPrompt: 'Drop property node(s) here.',
   classPrompt: 'Drop a class node here.',
 
-  /**
-  * initComponent
-  * @protected
-  */
   initComponent: function () {
-    this.addEvents({
-      close: true,
-      save: true,
-      reset: true,
-      validate: true,
-      tabChange: true,
-      refresh: true,
-      selectionchange: true
-    });
-
-    this.bbar = this.buildToolbar();
-
     var nodeId = '';
     var formId = '';
     var scope = '';
@@ -41,10 +22,12 @@ AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
     var oldGraphName = '';
     var graphName = '';
     var objectName = '';
-    var identifier = '';
+    var identifier = this.identifierPrompt;
     var delimiter = '_';
-    var className = '';
+    var className = this.identifierPrompt;
     var classId = '';
+
+    this.bbar = this.buildToolbar();
 
     if (this.node != null) {
       nodeId = this.node.id;
@@ -65,9 +48,6 @@ AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
       delimiter = classMap.identifierDelimiter
     }
 
-    var identifierValue = (identifier == '') ? this.identifierPrompt : identifier;
-    var classValue = (className == '') ? this.classPrompt : className;
-
     var thisform = new Ext.FormPanel({
       labelWidth: 17,
       url: this.url,
@@ -87,118 +67,110 @@ AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
         { text: 'Identifier Delimiter (required for composite identifier): ', xtype: 'label' },
         { name: 'delimiter', xtype: 'textfield', style: 'width:100px; margin:5px 0px 10px 0px', value: delimiter },
         { name: 'scope', xtype: 'hidden', value: scope },
+        { text: 'Identifier: ', xtype: 'label' },
+        { name: 'identifier', xtype: 'textfield', readOnly: true, style: 'height:40px; width:376px; margin:5px 0px 10px 0px;padding-top:10px', value: identifier },
+        { text: 'Root Class: ', xtype: 'label' },
+        { name: 'className', xtype: 'textfield', readOnly: true, style: 'height:40px; width:376px; margin:5px 0px 10px 0px;padding-top:10px', value: className },
         { name: 'app', xtype: 'hidden', value: app },
         { name: 'oldGraphName', xtype: 'hidden', value: oldGraphName },
-        { name: 'className', xtype: 'hidden', value: className },
-        { name: 'identifier', xtype: 'hidden', value: identifier },
         { name: 'objectName', xtype: 'hidden', value: objectName },
-        { name: 'classId', xtype: 'hidden', value: classId },
-      ],
-
-      html: 'Identifier(s): <div class="property-target' + formId + '" '
-              + 'style="border:1px silver solid;margin:5px 5px 10px 20px;padding:10;height:20">'
-              + identifierValue + '</div>'
-              + 'Root Class: <div class="class-target' + formId + '" '
-              + 'style="border:1px silver solid;margin:5px 5px 10px 20px;padding:10;height:20">'
-              + classValue + '</div>',
-
-      afterRender: function (cmp) {
-        Ext.FormPanel.prototype.afterRender.apply(this, arguments);
-
-        var propertyTarget = this.body.child('div.property-target' + formId);
-        var propertydd = new Ext.dd.DropTarget(propertyTarget, {
-          ddGroup: 'propertyGroup',
-          notifyEnter: function (propertydd, e, data) {
-            if (data.node.attributes.type != 'DataPropertyNode' &&
-                data.node.attributes.type != 'KeyDataPropertyNode')
-              return this.dropNotAllowed;
-            else
-              return this.dropAllowed;
-          },
-          notifyOver: function (propertydd, e, data) {
-            if (data.node.attributes.type != 'DataPropertyNode' &&
-                data.node.attributes.type != 'KeyDataPropertyNode')
-              return this.dropNotAllowed;
-            else
-              return this.dropAllowed;
-          },
-          notifyDrop: function (propertydd, e, data) {
-            if (data.node.attributes.type != 'DataPropertyNode' &&
-                data.node.attributes.type != 'KeyDataPropertyNode') {
-              return false;
-            }
-            else {
-              thisform.getForm().findField('objectName').setValue(data.node.id.split('/')[4]);
-              var currIdentifier = thisform.body.child('div.property-target' + formId).dom.innerHTML;
-
-              if (currIdentifier == thisform.ownerCt.identifierPrompt) {
-                objectName = data.node.parentNode.text;
-                identifier = objectName + '.' + data.node.text;
-              }
-              else {
-                if (objectName != data.node.parentNode.text) {
-                  var message = 'Identifiers must come from the same Data Object!';
-                  showDialog(400, 100, 'Error', message, Ext.Msg.OK, null);
-                  return false;
-                }
-
-                if (identifier.indexOf(objectName + "." + data.node.text) != -1) {
-                  var message = 'Duplicate identifiers are not allowed!';
-                  showDialog(400, 100, 'Error', message, Ext.Msg.OK, null);
-                  return false;
-                }
-
-                identifier += ',' + objectName + '.' + data.node.text;
-              }
-
-              thisform.body.child('div.property-target' + formId).update(identifier);
-              thisform.getForm().findField('identifier').setValue(identifier);
-
-              return true;
-            }
-          } //eo notifyDrop
-        }); //eo propertydd
-
-        var classTarget = this.body.child('div.class-target' + formId);
-        var classdd = new Ext.dd.DropTarget(classTarget, {
-          ddGroup: 'refdataGroup',
-          notifyEnter: function (classdd, e, data) {
-            if (data.node.attributes.type != 'ClassNode')
-              return this.dropNotAllowed;
-            else
-              return this.dropAllowed;
-          },
-          notifyOver: function (classdd, e, data) {
-            if (data.node.attributes.type != 'ClassNode')
-              return this.dropNotAllowed;
-            else
-              return this.dropAllowed;
-          },
-          notifyDrop: function (classdd, e, data) {
-            if (data.node.attributes.type != 'ClassNode') {
-              var message = 'Please slect a RDL Class...';
-              showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
-              return false;
-            }
-            else {
-              var lbl = data.node.attributes.record.Label;
-              thisform.getForm().findField('className').setValue(lbl);
-              thisform.getForm().findField('classId').setValue(data.node.attributes.record.Uri);
-              thisform.body.child('div.class-target' + formId).update(lbl);
-              return true;
-            }
-          } //eo notifyDrop
-        }); //eo classdd
-      } //eo after render
-    }); //eo form
+        { name: 'classId', xtype: 'hidden', value: classId }
+      ]
+    });
 
     this.items = [
-        thisform
-        ];
+      thisform
+    ];
 
     this.form = thisform;
 
-    // super
+    this.initDragDrop = function (cmp) {
+      Ext.FormPanel.prototype.afterRender.apply(this, arguments);
+
+      var propertyTarget = thisform.getForm().findField('identifier').el;
+      var propertydd = new Ext.dd.DropTarget(propertyTarget, {
+        ddGroup: 'propertyGroup',
+        notifyEnter: function (propertydd, e, data) {
+          if (data.node.attributes.type != 'DataPropertyNode' &&
+              data.node.attributes.type != 'KeyDataPropertyNode')
+            return this.dropNotAllowed;
+          else
+            return this.dropAllowed;
+        },
+        notifyOver: function (propertydd, e, data) {
+          if (data.node.attributes.type != 'DataPropertyNode' &&
+              data.node.attributes.type != 'KeyDataPropertyNode')
+            return this.dropNotAllowed;
+          else
+            return this.dropAllowed;
+        },
+        notifyDrop: function (propertydd, e, data) {
+          if (data.node.attributes.type != 'DataPropertyNode' &&
+              data.node.attributes.type != 'KeyDataPropertyNode') {
+            return false;
+          }
+          else {
+            var currIdentifier = thisform.getForm().findField('identifier').getValue();
+
+            if (currIdentifier == '' || currIdentifier == thisform.ownerCt.identifierPrompt) {
+              objectName = data.node.parentNode.text;
+              identifier = objectName + '.' + data.node.text;
+              thisform.getForm().findField('objectName').setValue(objectName);
+            }
+            else {
+              if (objectName != data.node.parentNode.text) {
+                var message = 'Identifiers must come from the same Data Object!';
+                showDialog(400, 100, 'Error', message, Ext.Msg.OK, null);
+                return false;
+              }
+
+              if (identifier.indexOf(objectName + "." + data.node.text) != -1) {
+                var message = 'Duplicate identifiers are not allowed!';
+                showDialog(400, 100, 'Error', message, Ext.Msg.OK, null);
+                return false;
+              }
+
+              identifier += ',' + objectName + '.' + data.node.text;
+            }
+
+            thisform.getForm().findField('identifier').setValue(identifier);
+
+            return true;
+          }
+        }
+      });
+
+      var classTarget = thisform.getForm().findField('className').el;
+      var classdd = new Ext.dd.DropTarget(classTarget, {
+        ddGroup: 'refdataGroup',
+        notifyEnter: function (classdd, e, data) {
+          if (data.node.attributes.type != 'ClassNode')
+            return this.dropNotAllowed;
+          else
+            return this.dropAllowed;
+        },
+        notifyOver: function (classdd, e, data) {
+          if (data.node.attributes.type != 'ClassNode')
+            return this.dropNotAllowed;
+          else
+            return this.dropAllowed;
+        },
+        notifyDrop: function (classdd, e, data) {
+          if (data.node.attributes.type != 'ClassNode') {
+            var message = 'Please slect a RDL Class...';
+            showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
+            return false;
+          }
+          else {
+            var lbl = data.node.attributes.record.Label;
+            thisform.getForm().findField('className').setValue(lbl);
+            thisform.getForm().findField('classId').setValue(data.node.attributes.record.Uri);
+            return true;
+          }
+        }
+      });
+    },
+
     AdapterManager.GraphPanel.superclass.initComponent.call(this);
   },
 
@@ -230,27 +202,23 @@ AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
   },
 
   onReset: function () {
-    var scope = this.form.getForm().findField('scope').getValue();
-    var app = this.form.getForm().findField('app').getValue();
-    this.form.body.child('div.property-target-' + scope + '-' + app).dom.innerHTML = 'Drop property node(s) here.';
     this.form.getForm().findField('objectName').setValue('');
-    this.form.getForm().findField('identifier').setValue('');
+    this.form.getForm().findField('identifier').setValue(this.identifierPrompt);
   },
 
   onCancel: function () {
-    this.form.getForm().reset();
-    this.fireEvent('Cancel', this);
+    this.close();
   },
 
   onSave: function () {
-    var that = this;    // consists the main/previous class object	
+    var that = this;
     var thisForm = this.form.getForm();
 
     if (thisForm.findField('graphName').getValue() == '' ||
         thisForm.findField('identifier').getValue() == '' ||
         thisForm.findField('objectName').getValue() == '' ||
         thisForm.findField('className').getValue() == '') {
-      var msg = 'All fields except for Identifier Delimiter are required!';
+      var msg = 'All fields are required!';
       showDialog(420, 50, 'Error', msg, Ext.Msg.OK, null);
       return;
     }
@@ -264,12 +232,17 @@ AdapterManager.GraphPanel = Ext.extend(Ext.Panel, {
 
     thisForm.submit({
       success: function (f, a) {
-        that.fireEvent('Save', that);
+        that.fireEvent('save', that);
       },
       failure: function (f, a) {
         var message = 'Error saving changes!';
         showDialog(400, 50, 'Error', message, Ext.Msg.OK, null);
       }
     });
+  },
+
+  show: function () {
+    AdapterManager.GraphPanel.superclass.show.call(this);
+    this.initDragDrop();
   }
 });
