9da8f6ef55dffe6cfdf0d1bb66383ec41c3d928a
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-01-02 16:39:56 +0000

    Add GenericContentObject.

---
+++ branches/2.4.x/Libraries/iRINGLibrary/Desktop/GenericContentObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace org.iringtools.library
+{
+  public class GenericContentObject : GenericDataObject, IContentObject
+  {
+    [IgnoreDataMember]
+    public Stream content { get; set; }
+
+    public string contentType { get; set; }
+
+    public string hash { get; set; }
+
+    public string hashType { get; set; }
+
+    public string url { get; set; }
+
+    public string identifier { get; set; }
+  }
+}
--- branches/2.4.x/Libraries/iRINGLibrary/Desktop/GenericDataObject.cs
+++ branches/2.4.x/Libraries/iRINGLibrary/Desktop/GenericDataObject.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using org.iringtools.library;
+﻿using System.Collections.Generic;
 
 namespace org.iringtools.library
 {
