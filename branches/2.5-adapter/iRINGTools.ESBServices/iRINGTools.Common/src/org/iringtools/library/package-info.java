071f2f6d0f20cea8811d485571333e6d51ee4617
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-01-25 14:54:27 +0000

    Clean up and initial build.

--- branches/2.5-adapter/iRINGTools.ESBServices/iRINGTools.Common/src/org/iringtools/library/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.iringtools.org/library", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.iringtools.library;
