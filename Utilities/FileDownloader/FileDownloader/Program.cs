e5650507dc59a3bed7ea0d8d3df3a12b14d57752
gagan.bechtel@gmail.com <gagan.bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-02-05 13:24:43 +0000

    File Downloading console app

---
+++ Utilities/FileDownloader/FileDownloader/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.library;
+using System.Net;
+using System.IO;
+using org.iringtools.utility;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FileDownloader
+{
+    class Program
+    {
+        [STAThread] 
+        static void Main(string[] args)
+        {
+            string baseUrl = String.Empty;
+            string relativeUrl = String.Empty;
+            string filePath = String.Empty;
+
+            try
+            {
+                if (args.Length >= 3)
+                {
+                    baseUrl = args[0];
+                    relativeUrl = args[1];
+                    filePath = args[2];
+
+                    string uri = baseUrl + relativeUrl;
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                    request.Method = "GET";
+
+                    // request.MediaType = "application/x-msaccess";               
+                    StreamReader reader = new System.IO.StreamReader(request.GetResponse().GetResponseStream());
+
+                    var bytes = default(byte[]);
+                    using (var memstream = new MemoryStream())
+                    {
+                        reader.BaseStream.CopyTo(memstream);
+                        bytes = memstream.ToArray();
+                    }
+
+                    System.IO.File.WriteAllBytes(filePath, bytes);
+                    reader.Close();
+                }
+            }
+            catch(Exception e)
+            {
+             
+            }
+        }
+    }
+}
