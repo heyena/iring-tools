3644dd1272c0099565bf84435327bdc5f0a65280
strydom5607@gmail.com <strydom5607@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-04-04 23:00:07 +0000

    added web modules/WcfReadEntityBodyModule.cs to force the HttpContext.Request.ReadEntityBody to be "Classic" and will ensure compatibility with .NET 4 application in a .NET 4.5 environment. if not in .NET 4.5 environment remove module from web.config

---
+++ branches/2.5-adapter/iRINGTools.Services/modules/WcfReadEntityBodyModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace org.iringtools.services.modules
+{
+  public class WcfReadEntityBodyModule : IHttpModule
+  {
+    public void Dispose() { }
+
+    public void Init(HttpApplication context)
+    {
+      context.BeginRequest += context_BeginRequest;
+    }
+
+    void context_BeginRequest(object sender, EventArgs e)
+    {
+      //This will force the HttpContext.Request.ReadEntityBody to be "Classic" and will ensure compatibility with .NET 4 application in a .NET 4.5 environment.
+if not in .NET 4.5 environment remove module from web.config 
+      var stream = (sender as HttpApplication).Request.InputStream;
+    }    
+  }
+}
