e1adfe51c64d22110622113a823d1320bfeaaecb
rijwan2info@gmail.com <rijwan2info@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-04-24 13:17:14 +0000

    made sencha extjs4.1 related changes.

--- branches/2.5-adapter/iRINGTools.Services/modules/WcfReadEntityBodyModule.cs
+++ branches/2.5-adapter/iRINGTools.Services/modules/WcfReadEntityBodyModule.cs
@@ -18,7 +18,7 @@ namespace org.iringtools.services.modules
     void context_BeginRequest(object sender, EventArgs e)
     {
       //This will force the HttpContext.Request.ReadEntityBody to be "Classic" and will ensure compatibility with .NET 4 application in a .NET 4.5 environment.
-if not in .NET 4.5 environment remove module from web.config 
+     //if not in .NET 4.5 environment remove module from web.config 
       var stream = (sender as HttpApplication).Request.InputStream;
     }    
   }
