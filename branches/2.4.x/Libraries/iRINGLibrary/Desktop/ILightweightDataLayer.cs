319594369623f8439f304efd0f08aba84d68c625
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-05-06 22:31:00 +0000

    Add lightweight data layer interface.

---
+++ branches/2.4.x/Libraries/iRINGLibrary/Desktop/ILightweightDataLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace org.iringtools.library
+{
+  public interface ILightweightDataLayer
+  {
+    DataDictionary Dictionary(string objectType, bool refresh, DataFilter inFilter, out DataFilter outFilter);
+    IList<IDataObject> Get(string objectType);
+    Response Update(IList<IDataObject> dataObjects);
+    IList<IContentObject> GetContents(IDictionary<string, string> idFormats);
+  }
+}
