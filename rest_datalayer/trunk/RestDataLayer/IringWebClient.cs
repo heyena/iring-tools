9f0fb36ffeb20fa02e37a2b50dea4a0f6aae407f
bkumarbt <bkumarbt@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-05-14 11:03:38 +0000

    Code added to set proxy settings

--- rest_datalayer/trunk/RestDataLayer/IringWebClient.cs
+++ rest_datalayer/trunk/RestDataLayer/IringWebClient.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using org.iringtools.adapter;
+using System.Net;
 
 namespace Bechtel.DataLayer
 {
@@ -17,20 +19,38 @@ namespace Bechtel.DataLayer
 
     internal class IringWebClient : IWebClient
     {
-        org.iringtools.utility.WebHttpClient _client = null ;
+        org.iringtools.utility.WebHttpClient _client = null;
 
         public IringWebClient()
         {
             _client = new org.iringtools.utility.WebHttpClient("");
         }
 
-        public IringWebClient(string baseUrl)
+        public IringWebClient(string baseUrl, AdapterSettings settings)
         {
-            _client = new org.iringtools.utility.WebHttpClient(baseUrl);
+            if (!string.IsNullOrEmpty(settings["ProxyHost"])
+         && !string.IsNullOrEmpty(settings["ProxyPort"])
+         && !string.IsNullOrEmpty(settings["ProxyCredentialToken"])) /// need to use proxy
+            {
+                var pcred = settings.GetWebProxyCredentials();
+                WebProxy Proxy = pcred.GetWebProxy() as WebProxy;
+                NetworkCredential ProxyCredentials = pcred.GetNetworkCredential();
+
+                _client = new org.iringtools.utility.WebHttpClient(baseUrl, ProxyCredentials, Proxy);
+            }
+            else
+            {
+                _client = new org.iringtools.utility.WebHttpClient(baseUrl);
+            }
+
+
+
         }
 
-        public IringWebClient(string baseUrl, string appKey, string accessToken)
-            : this(baseUrl)
+
+
+        public IringWebClient(string baseUrl, string appKey, string accessToken, AdapterSettings settings)
+            : this(baseUrl, settings)
         {
             _client.AppKey = appKey;
             _client.AccessToken = accessToken;
@@ -38,11 +58,11 @@ namespace Bechtel.DataLayer
 
             string testAppKey = _client.AppKey;
             string testToken = _client.AccessToken;
-            
+
         }
 
-        public IringWebClient(string baseUrl, string appKey, string accessToken,string contentType)
-            : this(baseUrl)
+        public IringWebClient(string baseUrl, string appKey, string accessToken, string contentType, AdapterSettings settings)
+            : this(baseUrl, settings)
         {
             _client.AppKey = appKey;
             _client.AccessToken = accessToken;
@@ -55,10 +75,10 @@ namespace Bechtel.DataLayer
 
         public string MakeGetRequest(string url)
         {
-           return  _client.GetJson(url);
-           //Stream st = _client.GetStream(url);
-           //string response = _client.GetMessage(url);
-           //return response;
+            return _client.GetJson(url);
+            //Stream st = _client.GetStream(url);
+            //string response = _client.GetMessage(url);
+            //return response;
         }
 
         public void MakePutRequest(string url, string jsonString)
