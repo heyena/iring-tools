3a7c6c2e1bb42da09f89efd09999d2a5677a342d
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-07-26 17:42:01 +0000

    Update build script.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
+
 set basedir=%~dp0
-set javabasedir=%basedir%iRINGTools.ESBServices\
 set deploymentdir=%basedir%Deployment\
 set "PATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319;%PATH%"
 
@@ -11,5 +11,5 @@ svn update
 pause
 
 echo Building C# projects ...
-msbuild build.xml /t:ReBuild,CreatePackages /fileLogger /flp:errorsonly;logfile=msbuild.error.log /fileLogger /flp1:warningsonly;logfile=msbuild.warning.log
+msbuild build.xml /t:ReBuild,CreatePackages
 pause
