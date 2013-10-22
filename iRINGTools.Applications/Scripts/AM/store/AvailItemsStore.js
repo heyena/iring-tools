c625ce08ed2fed34efd30507ec35f9f8d2a980c1
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-10-22 20:56:10 +0000

    Removed unnecessary backend call.

--- iRINGTools.Applications/Scripts/AM/store/AvailItemsStore.js
+++ iRINGTools.Applications/Scripts/AM/store/AvailItemsStore.js
@@ -24,7 +24,7 @@ Ext.define('AM.store.AvailItemsStore', {
     var me = this;
     cfg = cfg || {};
     me.callParent([Ext.apply({
-      autoLoad: true,
+      autoLoad: false,
       model: 'AM.model.AvailItemsModel',
       storeId: 'AvailItemsStore',
       proxy: {
