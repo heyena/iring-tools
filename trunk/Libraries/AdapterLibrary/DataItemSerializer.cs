be67dbcca1325cbf6b1fbe3d148f4c52b8f4eb73
farshad.kargar@gmail.com <farshad.kargar@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-10-22 14:06:38 +0000

    Added JSONLD

--- trunk/Libraries/AdapterLibrary/DataItemSerializer.cs
+++ trunk/Libraries/AdapterLibrary/DataItemSerializer.cs
@@ -24,25 +24,27 @@ namespace org.iringtools.adapter
       _converter = new DataItemConverter(idFieldName, linksFieldName, displayLinks);
     }
 
-    public MemoryStream SerializeToMemoryStream<T>(T graph, bool useDataContractSerializer)
+    public MemoryStream SerializeToMemoryStream<T>(T graph, bool useDataContractSerializer, bool jsonLD = false )
     {
-      MemoryStream stream = new MemoryStream();
-
-      try
-      {
+        MemoryStream stream = new MemoryStream();
+        _converter.bJsonLd = jsonLD;
+       
+        try
+        {
         if (useDataContractSerializer)
         {
-          DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-          serializer.WriteObject(stream, graph);
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            serializer.WriteObject(stream, graph);
         }
         else
         {
-          JavaScriptSerializer serializer = new JavaScriptSerializer();
-          serializer.MaxJsonLength = int.MaxValue;
-          serializer.RegisterConverters(new JavaScriptConverter[] { _converter });
-          string json = serializer.Serialize(graph);
-          byte[] byteArray = Encoding.UTF8.GetBytes(json);
-          stream = new MemoryStream(byteArray);
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            serializer.MaxJsonLength = int.MaxValue;
+            serializer.RegisterConverters(new JavaScriptConverter[] { _converter });
+            string json = serializer.Serialize(graph);
+  
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+            stream = new MemoryStream(byteArray);
         }
       }
       catch (Exception exception)
@@ -112,6 +114,8 @@ namespace org.iringtools.adapter
     private string _contentTypeFieldName = "_CONTENT_TYPE_";
     private bool _displayLinks = false;
 
+    public bool bJsonLd = false;
+
     public DataItemConverter(string idFieldName, string linksFieldName, bool displayLinks)
     {
       if (!string.IsNullOrEmpty(idFieldName))
@@ -130,38 +134,47 @@ namespace org.iringtools.adapter
 
     public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
     {
-      DataItem dataItem = (DataItem)obj;
-      Dictionary<string, object> result = new Dictionary<string, object>();
+        DataItem dataItem = (DataItem)obj;
+        Dictionary<string, object> result = new Dictionary<string, object>();
 
-      if (dataItem != null)
-      {
-        result[_idFieldName] = dataItem.id;
-
-        if (result.Keys.Contains(_hasContentFieldName)) 
-          result[_hasContentFieldName] = dataItem.hasContent;
-
-        //if (dataItem.hasContent)
-        //{
-        //    if (dataItem.content != null && dataItem.content.Length > 0)
-        //    {
-        //        result[_contentFieldName] = dataItem.content;
-        //    }
-        //}
-
-        foreach (var property in dataItem.properties)
+        if (dataItem != null)
         {
-          object value = property.Value;
+            //FKM
+            if (bJsonLd)
+            {
+               //FKM don't need it for now
+               // result["PlantArea.IdentificationByTag.valIdentifier"] = dataItem.id;
+            }
+            else
+            {
+                result[_idFieldName] = dataItem.id;
+            }
 
-          result[property.Key] = property.Value;
+            if (result.Keys.Contains(_hasContentFieldName))
+                result[_hasContentFieldName] = dataItem.hasContent;
+
+            //if (dataItem.hasContent)
+            //{
+            //    if (dataItem.content != null && dataItem.content.Length > 0)
+            //    {
+            //        result[_contentFieldName] = dataItem.content;
+            //    }
+            //}
+
+            foreach (var property in dataItem.properties)
+            {
+                object value = property.Value;
+
+                result[property.Key] = property.Value;
+            }
+
+            if (_displayLinks)
+            {
+                result[_linksFieldName] = dataItem.links;
+            }
         }
 
-        if (_displayLinks)
-        {
-          result[_linksFieldName] = dataItem.links;
-        }
-      }
-
-      return result;
+        return result;
     }
 
     public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
