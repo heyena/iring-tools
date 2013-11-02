24eb2e0b724b03f027e0a74fa3af3c88989fe591
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-11-02 20:06:32 +0000

    Fix app data grid filters.

--- iRINGTools.Applications/Scripts/AM/view/override/directory/DataGridPanel.js
+++ iRINGTools.Applications/Scripts/AM/view/override/directory/DataGridPanel.js
@@ -2,7 +2,7 @@ Ext.define('AM.view.override.directory.DataGridPanel', {
   override: 'AM.view.directory.DataGridPanel',
   initComponent: function () {
     var me = this;
-     var storeId = Ext.data.IdGenerator.get("uuid").generate();
+    var storeId = Ext.data.IdGenerator.get("uuid").generate();
 
     me.store = Ext.create('AM.store.DataGridStore', {
       storeId: "DataGrid" + storeId,
@@ -20,51 +20,22 @@ Ext.define('AM.view.override.directory.DataGridPanel', {
       store: me.store,
       displayInfo: true,
       displayMsg: 'Records {0} - {1} of {2}',
-      emptyMsg: "No records to display",
-      plugins: [Ext.create('Ext.ux.plugin.GridPageSizer', { options: [25, 50, 100, 200] })]
+      emptyMsg: "No records to display"/*,
+      plugins: [Ext.create('Ext.ux.plugin.GridPageSizer', { options: [25, 50, 100, 500] })]*/
     });
-    
-     var filters = {
-            ftype: 'filters',
-    	      autoReload: true,
-            updateBuffer : 5000,
-            local: false,
-            //encode: true, // json encode the filter query
-            remoteSort: true,   // defaults to false (remote filtering)
-            buildQuery: function (filters) {
-            var processed_filters = [];
 
-            for (var i = 0; i < filters.length; i++) {
-              var pf = {};
-              var filter = filters[i];
-              pf.field = filter.field;
-
-              if (filter.data.type == 'numeric') {
-                pf.comparison = filter.data.comparison;
-                pf.value = filter.data.value;
-                pf.type = filter.data.type;
-              }
-              else {
-                for (var key in filter.data) {
-                  pf[key] = filter.data[key];
-                }
-    			      pf.comparison = 'eq';
-              }
-
-              processed_filters.push(pf);
-
-            }
-            return { filter: Ext.encode(processed_filters) };
-          }
-        };
+    var filters = {
+        ftype: 'filters',
+        local: false,
+        remoteSort: true,
+        encode: true
+    };
     
     Ext.apply(me, {
       bbar: ptb,
-      iconCls: 'tabsData',
-
       features: [filters]
     });
     
-  me.callOverridden(arguments);
+    me.callOverridden(arguments);
   } 
 });
