fd29e8b5d6272b4c64582d083b40cef4de4df5bf
farshad.kargar@gmail.com <farshad.kargar@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-11-05 15:18:12 +0000

    Implemented Post/Put/Delete/Get methods. Implemented filters, version and tip dictionary.

--- iRINGTools.Services/HMCommonDataService.cs
+++ iRINGTools.Services/HMCommonDataService.cs
@@ -10,6 +10,8 @@ using org.iringtools.library;
 using System.ServiceModel.Web;
 using System.Net;
 using System.IO;
+using org.iringtools.utility;
+using org.iringtools.library.tip;
 
 namespace org.iringtools.services
 {
@@ -63,7 +65,21 @@ namespace org.iringtools.services
             }
         }
 
-        public void GetPicklists(string project, string app, string format)
+        new public void GetObjectType(string project, string app, string objectType, string format)
+        {
+            format = "jsonld";
+
+            DataDictionary dictionary = _abstractPrivder.GetDictionary(project, app);
+
+            DataObject dataObject = dictionary.dataObjects.Find(o => o.objectName.ToLower() == objectType.ToLower());
+
+            if (dataObject == null)
+                throw new FileNotFoundException();
+
+            _abstractPrivder.FormatOutgoingMessage<DataObject>(dataObject, format, true);
+        }
+
+        new public void GetPicklists(string project, string app, string format)
         {
             try
             {
@@ -77,7 +93,43 @@ namespace org.iringtools.services
             }
         }
 
-        public void GetWithFilter(string project, string app, string resource, string format, int start, int limit, string indexStyle, Stream stream)
+        public void GetPicklist(string project, string app, string name, string format, int start, int limit)
+        {
+            format = "jsonld";
+            try
+            {
+                Picklists obj = _abstractPrivder.GetPicklist(project, app, name, format, start, limit);
+
+                XElement xml = obj.ToXElement<Picklists>();
+
+                _abstractPrivder.FormatOutgoingMessage(xml, format);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Search(string project, string app, string resource, string query, string format, int start, int limit, string sortOrder, string sortBy, string indexStyle)
+        {
+            try
+            {
+                NameValueCollection parameters = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters;
+
+                bool fullIndex = false;
+                if (indexStyle != null && indexStyle.ToUpper() == "FULL")
+                    fullIndex = true;
+
+                XDocument xDocument = _abstractPrivder.Search(project, app, resource, ref format, query, start, limit, sortOrder, sortBy, fullIndex, parameters);
+                _abstractPrivder.FormatOutgoingMessage(xDocument.Root, format);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        new public void GetWithFilter(string project, string app, string resource, string format, int start, int limit, string indexStyle, Stream stream)
         {
             try
             {
@@ -112,12 +164,248 @@ namespace org.iringtools.services
             }
         }
 
+        new public void GetRelatedItem(string project, string app, string resource, string id, string related, string relatedId, string format)
+        {
+            try
+            {
+                format = "jsonld";
+                XDocument xDocument = _abstractPrivder.GetRelatedItem(project, app, resource, id, related, relatedId, ref format);
+                _abstractPrivder.FormatOutgoingMessage(xDocument.Root, format);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         new public void GetContexts(string app, string format)
         {
 
             Contexts contexts = _abstractPrivder.GetContexts(app);
 
             _abstractPrivder.FormatOutgoingMessage<Contexts>(contexts, "json", true);
+        }
+
+        new public void UpdateRelatedItem(string project, string app, string resource, string parentid, string relatedresource, string id, string format, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                DataItems dataItems = _abstractPrivder.FormatIncomingMessage(stream);
+
+                if (dataItems != null && dataItems.items != null && dataItems.items.Count > 0)
+                {
+                    dataItems.items[0].id = id;
+                    XElement xElement = dataItems.ToXElement();
+                    response = _abstractPrivder.UpdateRelated(project, app, resource, parentid, relatedresource, format, new XDocument(xElement));
+                }
+                else
+                {
+                    throw new Exception("No items to update or invalid payload.");
+                }
+ 
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+
+        public void UpdateItem(string project, string app, string resource, string id, string format, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                DataItems dataItems = _abstractPrivder.FormatIncomingMessage(stream);
+
+                if (dataItems != null && dataItems.items != null && dataItems.items.Count > 0)
+                {
+                    dataItems.items[0].id = id;
+                    XElement xElement = dataItems.ToXElement();
+                    response = _abstractPrivder.Update(project, app, resource, format, new XDocument(xElement));
+                }
+                else
+                {
+                    throw new Exception("No items to update or invalid payload.");
+                }
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void DeleteRelatedItem(string project, string app, string resource, string parentid, string relatedresource, string id, string format)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                response = _abstractPrivder.DeleteRelated(project, app, resource, parentid, relatedresource, id, format);
+            }
+            catch (Exception ex)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(ex.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void DeleteItem(string project, string app, string resource, string id, string format)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                response = _abstractPrivder.DeleteItem(project, app, resource, id, format);
+
+            }
+            catch (Exception ex)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(ex.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void CreateRelatedItem(string project, string app, string resource, string format, string parentid, string relatedResource, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                XElement xElement = _abstractPrivder.FormatIncomingMessage(stream, format);
+
+                response = _abstractPrivder.UpdateRelated(project, app, resource, parentid, relatedResource, format, new XDocument(xElement));
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void CreateItem(string project, string app, string resource, string format, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                XElement xElement = _abstractPrivder.FormatIncomingMessage(stream, format);
+
+                response = _abstractPrivder.Create(project, app, resource, format, new XDocument(xElement));
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            PrepareResponse(ref response);
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void UpdateList(string project, string app, string resource, string format, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+                 XElement xElement = _abstractPrivder.FormatIncomingMessage(stream, format);
+
+                 response = _abstractPrivder.Update(project, app, resource, format, new XDocument(xElement));
+
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        public void UpdateRelatedList(string project, string app, string resource, string parentid, string relatedResource, string format, Stream stream)
+        {
+            Response response = new Response();
+
+            try
+            {
+                format = "jsonld";
+
+
+                XElement xElement = _abstractPrivder.FormatIncomingMessage(stream, format);
+
+                response = _abstractPrivder.UpdateRelated(project, app, resource, parentid, relatedResource, format, new XDocument(xElement));
+
+            }
+            catch (Exception e)
+            {
+                response.Level = StatusLevel.Error;
+                response.Messages.Add(e.Message);
+            }
+
+            _abstractPrivder.FormatOutgoingMessage<Response>(response, format, false);
+        }
+
+        private void PrepareResponse(ref Response response)
+        {
+            if (response.Level == StatusLevel.Success)
+            {
+                response.StatusCode = HttpStatusCode.OK;
+            }
+            else
+            {
+                response.StatusCode = HttpStatusCode.InternalServerError;
+            }
+
+            if (response.Messages != null)
+            {
+                foreach (string msg in response.Messages)
+                {
+                    response.StatusText += msg;
+                }
+            }
+
+            foreach (Status status in response.StatusList)
+            {
+                foreach (string msg in status.Messages)
+                {
+                    response.StatusText += msg;
+                }
+            }
         }
 
         new public void GetList(string project, string app, string resource, string format, int start, int limit, string sortOrder, string sortBy, string indexStyle)
@@ -149,6 +437,27 @@ namespace org.iringtools.services
                 format = "jsonld";
                 object content = _abstractPrivder.GetItem(project, app, resource, String.Empty, id, ref format, false);
                 _abstractPrivder.FormatOutgoingMessage(content, format);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void GetRelatedList(string project, string app, string resource, string id, string related, string format, int start, int limit, string sortOrder, string sortBy, string indexStyle)
+        {
+            try
+            {
+                format = "jsonld";
+
+                NameValueCollection parameters = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters;
+
+                bool fullIndex = false;
+                if (indexStyle != null && indexStyle.ToUpper() == "FULL")
+                    fullIndex = true;
+
+                XDocument xDocument = _abstractPrivder.GetRelatedList(project, app, resource, id, related, ref format, start, limit, sortOrder, sortBy, fullIndex, parameters);
+                _abstractPrivder.FormatOutgoingMessage(xDocument.Root, format);
             }
             catch (Exception ex)
             {
