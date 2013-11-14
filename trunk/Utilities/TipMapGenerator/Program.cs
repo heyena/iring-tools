6e8bd498c224e170974cb114955de80a88219eca
farshad.kargar@gmail.com <farshad.kargar@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-11-14 20:14:08 +0000

    Added TIP Generator utility.

---
+++ trunk/Utilities/TipMapGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.iringtools.adapter;
+using System.Configuration;
+
+namespace TipMapGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractProvider abstractProvider = new AbstractProvider(ConfigurationManager.AppSettings);
+            abstractProvider.GenerateAllTips();
+        }
+    }
+}
