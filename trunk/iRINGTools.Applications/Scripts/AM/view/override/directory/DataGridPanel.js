68a5151f19af9df9d04792b7878cce9f631b3bf7
hdle6880 <hdle6880@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-12-04 15:33:37 +0000

    Fixed GridPageSizer to always load the first page when value changed.

--- trunk/iRINGTools.Applications/Scripts/AM/view/override/directory/DataGridPanel.js
+++ trunk/iRINGTools.Applications/Scripts/AM/view/override/directory/DataGridPanel.js
@@ -1,41 +1,40 @@
 Ext.define('AM.view.override.directory.DataGridPanel', {
-  override: 'AM.view.directory.DataGridPanel',
-  initComponent: function () {
-    var me = this;
-    var storeId = Ext.data.IdGenerator.get("uuid").generate();
+    override: 'AM.view.directory.DataGridPanel',
 
-    me.store = Ext.create('AM.store.DataGridStore', {
-      storeId: "DataGrid" + storeId,
-        listeners: {        
-        beforeload: {
-          fn: me.handleBeforeLoad,
-          scope: me
-        }
-      }
+    requires: [
+        'Ext.ux.grid.FiltersFeature',
+        'Ext.ux.plugin.GridPageSizer'
+    ],
 
-    });
-    
-    var ptb = Ext.create('Ext.PagingToolbar', {
-      pageSize: 25,
-      store: me.store,
-      displayInfo: true,
-      displayMsg: 'Records {0} - {1} of {2}',
-      emptyMsg: "No records to display"/*,
-      plugins: [Ext.create('Ext.ux.plugin.GridPageSizer', { options: [25, 50, 100, 500] })]*/
-    });
+    initComponent: function () {
+        var me = this;
+        var storeId = Ext.data.IdGenerator.get("uuid").generate();
 
-    var filters = {
-        ftype: 'filters',
-        local: false,
-        remoteSort: true,
-        encode: true
-    };
-    
-    Ext.apply(me, {
-      bbar: ptb,
-      features: [filters]
-    });
-    
-    me.callOverridden(arguments);
-  } 
+        me.store = Ext.create('AM.store.DataGridStore', {
+            storeId: "DataGrid" + storeId
+        });
+
+        var ptb = Ext.create('Ext.PagingToolbar', {
+            pageSize: 25,
+            store: me.store,
+            displayInfo: true,
+            displayMsg: 'Records {0} - {1} of {2}',
+            emptyMsg: "No records to display",
+            plugins: [Ext.create('Ext.ux.plugin.GridPageSizer', { options: [25, 50, 100, 200] })]
+        });
+
+        var filters = {
+            ftype: 'filters',
+            local: false,
+            remoteSort: true,
+            encode: true
+        };
+
+        Ext.apply(me, {
+            bbar: ptb,
+            features: [filters]
+        });
+
+        me.callOverridden(arguments);
+    }
 });
