a1c052b3ec592bd57e7d60261a8132bc667f5754
bkumarbt@gmail.com <bkumarbt@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2013-12-12 14:32:10 +0000

    Multiple exchange configuration functionality.

--- Utilities/ExchangeTask/Program.cs
+++ Utilities/ExchangeTask/Program.cs
@@ -10,16 +10,20 @@ namespace ExchangeTask
 {
   class Program
   {
-    private static string _baseUrl = String.Empty;
-    private static string _scope = String.Empty;
-    private static string _commodityName = String.Empty;
-    private static string _exchangeId = String.Empty;
+   // private static string _baseUrl = String.Empty;
+   // private static string _scope = String.Empty;
+   // private static string _commodityName = String.Empty;
+  // private static string _exchangeId = String.Empty;
     private static string _proxyHost = String.Empty;
     private static string _ssoUrl = String.Empty;
     private static string _clientId = String.Empty;
     private static string _clientSecret = String.Empty;
     private static string _grantType = String.Empty;
-    private static string _bearerToken = String.Empty; 
+    private static string _bearerToken = String.Empty;
+    private static string _configurationFiles = String.Empty;
+    private static int  _httpRequestTimeout ;
+    
+    private static List<Properties> _exchangeConfigurationList = new List<Properties>();
 
     private static int _proxyPort =0;
     private static string _proxyCredentialToken = String.Empty;
@@ -42,6 +46,7 @@ namespace ExchangeTask
     {
       if (Initialize(args))
       {
+
         Console.WriteLine("make request to get user tocken ...");
         if (String.IsNullOrWhiteSpace(_ssoUrl))
         {
@@ -59,35 +64,46 @@ namespace ExchangeTask
         dynamic responseObject = JObject.Parse(responseBody);
         _bearerToken = responseObject.access_token;
 
-        //Console.WriteLine(responseObject.token_type);
-        //Console.WriteLine(responseObject.expires_in);
-        //Console.WriteLine(responseObject.access_token);
 
-        //-----------------------------
-        
-        string relativeUrl = String.Format("?scope={0}&xid={1}", _scope, _exchangeId);
-
-        if (String.IsNullOrWhiteSpace(_proxyCredentialToken))
+        foreach (var exchangeConfiguration in _exchangeConfigurationList)
         {
-          _httpClient = new WebHttpClient(_baseUrl);
-        }
-        else
-        {
-          WebCredentials credential = new WebCredentials(_proxyCredentialToken);
-          _httpClient = new WebHttpClient(_baseUrl, credential.userName, credential.password, credential.domain, _proxyHost, _proxyPort);
-        }
+            string baseUrl = exchangeConfiguration["BaseUrl"];
+            string scope = exchangeConfiguration["Scope"]; ;
+            string exchangeId = exchangeConfiguration["ExchangeId"]; 
 
-        if (_httpClient.Headers == null)
-            _httpClient.Headers = new Dictionary<string, string>();
-        _httpClient.Headers.Add("ClientToken", _bearerToken);
+            try
+            {
+                string relativeUrl = String.Format("?scope={0}&xid={1}", scope, exchangeId);
 
-        _logger.Debug("Send exchange request...");
-        Console.WriteLine("Send exchange request...");
-        string responseMessage = _httpClient.GetMessage(relativeUrl);
-        Console.WriteLine("Response Message");
-        _logger.Info("Response Message");
-        Console.WriteLine(responseMessage);
-        _logger.Info(responseMessage);
+                if (String.IsNullOrWhiteSpace(_proxyCredentialToken))
+                {
+                    _httpClient = new WebHttpClient(baseUrl);
+                }
+                else
+                {
+                    WebCredentials credential = new WebCredentials(_proxyCredentialToken);
+                    _httpClient = new WebHttpClient(baseUrl, credential.userName, credential.password, credential.domain, _proxyHost, _proxyPort);
+                }
+                if (_httpRequestTimeout != 0)
+                    _httpClient.Timeout = _httpRequestTimeout;
+
+                if (_httpClient.Headers == null)
+                    _httpClient.Headers = new Dictionary<string, string>();
+                _httpClient.Headers.Add("ClientToken", _bearerToken);
+
+                _logger.Debug(String.Format("Sending exchange request  for scope: {0} and exchange Id: {1}", scope,exchangeId));
+                Console.WriteLine(String.Format("Sending exchange request  for scope: {0} and exchange Id: {1}", scope, exchangeId));
+                string responseMessage = _httpClient.GetMessage(relativeUrl);
+                Console.WriteLine(String.Format("Exchange response message for scope: {0} and exchange Id: {1}", scope, exchangeId));
+                _logger.Info(String.Format("Exchange response message for scope: {0} and exchange Id: {1}", scope, exchangeId));
+                Console.WriteLine(responseMessage);
+                _logger.Info(responseMessage);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(String.Format("Error in exchange aginst for scope : {0} and exchange Id: {1}", scope,exchangeId), ex);
+            }
+        }
 
       }
     }
@@ -98,19 +114,44 @@ namespace ExchangeTask
 
       try
       {
-      _baseUrl = System.Configuration.ConfigurationManager.AppSettings["BaseUrl"];
-      _scope = System.Configuration.ConfigurationManager.AppSettings["Scope"];
-      _commodityName = System.Configuration.ConfigurationManager.AppSettings["CommodityName"];
-      _exchangeId = System.Configuration.ConfigurationManager.AppSettings["ExchangeId"];
+      //_baseUrl = System.Configuration.ConfigurationManager.AppSettings["BaseUrl"];
+      //_scope = System.Configuration.ConfigurationManager.AppSettings["Scope"];
+      //_commodityName = System.Configuration.ConfigurationManager.AppSettings["CommodityName"];
+      //_exchangeId = System.Configuration.ConfigurationManager.AppSettings["ExchangeId"];
       _proxyHost = System.Configuration.ConfigurationManager.AppSettings["ProxyHost"];
       _proxyCredentialToken = System.Configuration.ConfigurationManager.AppSettings["ProxyCredentialToken"];
       _ssoUrl = System.Configuration.ConfigurationManager.AppSettings["SSO_URL"];
       _clientId = System.Configuration.ConfigurationManager.AppSettings["client_id"];
       _clientSecret = System.Configuration.ConfigurationManager.AppSettings["client_secret"];
       _grantType = System.Configuration.ConfigurationManager.AppSettings["grant_type"];
+      _configurationFiles = System.Configuration.ConfigurationManager.AppSettings["ConfigurationFiles"];
+
+      string httpRequestTimeout = System.Configuration.ConfigurationManager.AppSettings["HttpRequestTimeout"];
+      if (!String.IsNullOrWhiteSpace(httpRequestTimeout))
+          _httpRequestTimeout = Convert.ToInt32(httpRequestTimeout);
+      
 
       string proxyPortString = System.Configuration.ConfigurationManager.AppSettings["ProxyPort"];
       int.TryParse(proxyPortString,out _proxyPort);
+      
+      string[] confArray = _configurationFiles.Split(',');
+      foreach (var path in confArray)
+      {
+          _logger.Debug("Reading configuration files...");
+          try
+          {
+              if (!String.IsNullOrWhiteSpace(path))
+              {
+                  Properties props = new Properties();
+                  props.Load(path);
+                  _exchangeConfigurationList.Add(props);
+              }
+          }
+          catch(Exception ex)
+          {
+              _logger.Error(String.Format("Error in reading file: {0}", path), ex);
+          }
+      }
 
       return true;
       }
