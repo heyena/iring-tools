dda0c30ebf811609315862e0699352c7492a793d
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-02-24 07:19:39 +0000

    Added for throwing user define exception handling

---
+++ Libraries/iRINGLibrary/CustomError.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Web;
+
+
+namespace org.iringtools.library
+{
+    public class CustomError
+    {
+        public Guid msgId { get; set; }
+        public string errMessage { get; set; }
+        public string stackTraceDescription { get; set; }
+      
+    }
+
+   
+
+
+
+
+}
