b65abe9428b5c3c931e86f269802f7d30d80dc15
hemant21 <hemant21@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-04-30 12:36:24 +0000

    Initial Creation

---
+++ iRINGAgent/iRINGAgent/AgentConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using org.iringtools.utility;
+using org.iringtools.library;
+using System.Collections.ObjectModel;
+
+
+namespace org.iringtools.agent
+{
+    [CollectionDataContract(Namespace = "http://www.iringtools.org/library", Name = "agentconfig")]
+    public class AgentConfig : List<TaskSequence> { }
+
+    [Serializable]
+    [DataContract(Namespace = "http://www.iringtools.org/library", Name = "tasksequence")]
+    public class TaskSequence
+    {
+        [DataMember(Name = "name", Order = 0)]
+        public string Name { get; set; }
+
+        [DataMember(Name = "tasks", Order = 1)]
+        public List<Task> Tasks { get; set; }
+    }
+
+    [Serializable]
+    [DataContract(Namespace = "http://www.iringtools.org/library", Name = "task")]
+    public class Task
+    {
+        [DataMember(Name = "baseURL", Order = 0)]
+        public string BaseURL { get; set; }
+
+        [DataMember(Name = "assembly", Order = 1)]
+        public string Assembly { get; set; }
+
+        [DataMember(Name = "scope", Order = 2)]
+        public string Scope { get; set; }
+
+        [DataMember(Name = "app", Order = 3)]
+        public string App { get; set; }
+
+        [DataMember(Name = "params", Order = 4)]
+        public Dictionary<string, string> taskParams { get; set; }
+    }
+}
---
+++ iRINGAgent/iRINGAgent/AgentProvider.cs
@@ -0,0 +1,58 @@
+﻿using org.iringtools.adapter;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.iringtools.utility;
+using org.iringtools.library;
+using org.iringtools.mapping;
+using log4net;
+using Newtonsoft.Json;
+using Ninject;
+using Ninject.Extensions.Xml;
+
+namespace org.iringtools.agent
+{
+    class AgentProvider : BaseProvider
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(AgentProvider));
+        private string[] arrSpecialcharlist;
+        private string[] arrSpecialcharValue;
+
+        [Inject]
+        public AgentProvider(NameValueCollection settings)
+            : base(settings)
+        {
+
+            try
+            {
+                if (_settings["SpCharList"] != null && _settings["SpCharValue"] != null)
+                {
+                    arrSpecialcharlist = _settings["SpCharList"].ToString().Split(',');
+                    arrSpecialcharValue = _settings["SpCharValue"].ToString().Split(',');
+                }
+
+                if (_settings["LdapConfiguration"] != null && _settings["LdapConfiguration"].ToLower() == "true")
+                {
+                    //utility.Utility.isLdapConfigured = true;
+                    //utility.Utility.InitializeConfigurationRepository(new Type[] { 
+            //typeof(DataDictionary), 
+            //typeof(DatabaseDictionary),
+            //typeof(XElementClone),
+            //typeof(AuthorizedUsers),
+            //typeof(Mapping)
+          //});
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Error initializing adapter provider: " + e.Message);
+            }
+        }
+
+        //InitializeScope(project, application);
+        //InitializeDataLayer();
+    }
+}
