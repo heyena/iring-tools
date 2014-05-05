5f7900301a43fddad9c7daa8fb03a13938e1df5d
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-05-05 13:26:19 +0000

    updated version

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.7.0.0")]
-[assembly: AssemblyFileVersion("2.7.0.0")]
+[assembly: AssemblyVersion("2.7.0")]
+[assembly: AssemblyFileVersion("2.7.0")]
