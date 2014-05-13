fcc2db5911f26fcc3d5b5f3b52df3437a2f10d0b
hemant21 <hemant21@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-05-13 17:30:44 +0000

    Initial Issue

---
+++ Utilities/Agent/iRINGAgent/AgentProvider.cs
@@ -0,0 +1,219 @@
+﻿using org.iringtools.adapter;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using org.iringtools.utility;
+using org.iringtools.library;
+using org.iringtools.mapping;
+using log4net;
+using Newtonsoft.Json;
+using Ninject;
+using Ninject.Extensions.Xml;
+
+namespace org.iringtools.agent
+{
+    class AgentProvider : BaseProvider
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(AgentProvider));
+        private string[] arrSpecialcharlist;
+        private string[] arrSpecialcharValue;
+
+        private static string _proxyCredentialToken = string.Empty;
+        private static string _proxyHost = string.Empty;
+        private static int _proxyPort = 0;
+
+        private static string _ssoURL = string.Empty;
+        private static string _clientId = string.Empty;
+        private static string _clientSecret = string.Empty;
+        private static string _authType = string.Empty;
+        private static string _clientToken = string.Empty;
+
+        private static int _requestTimeout = 28800000;  // 8 hours
+        
+
+        [Inject]
+        public AgentProvider(NameValueCollection settings)
+            : base(settings)
+        {
+
+            try
+            {
+                if (_settings["SpCharList"] != null && _settings["SpCharValue"] != null)
+                {
+                    arrSpecialcharlist = _settings["SpCharList"].ToString().Split(',');
+                    arrSpecialcharValue = _settings["SpCharValue"].ToString().Split(',');
+                }
+
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Error initializing adapter provider: " + e.Message);
+            }
+        }
+
+        public void ProcessTask(Task task)
+        {
+            //DataFilter filter = null;
+            //List<IDataObject> dataObjects = null;
+
+            try
+            {
+                Initialize();
+                string clientToken = GetClientToken();
+
+                if (task.TaskType.ToLower().Equals("cache"))
+                {
+                    InitializeScope(task.Project, task.App);
+                    InitializeDataLayer();
+                    _dictionary = _dataLayerGateway.GetDictionary();
+
+                    RefreshCache(task.Scope, task.App, false);
+
+                }
+                else if (task.TaskType.ToLower().Equals("exchange"))
+                {
+                    string url = string.Format("{0}?scope={1}&xid={2}", task.BaseURL, task.Scope, task.ExchangeId);
+                    WebRequest request = CreateWebRequest(url);
+
+                    if (!string.IsNullOrEmpty(clientToken))
+                    {
+                        _logger.Info("Use client token.");
+                        request.Headers.Add("AuthType", _authType);
+                        request.Headers.Add("ClientToken", clientToken);
+                        request.Headers.Add("UserName", _clientId);
+                    }
+
+                    request.Timeout = _requestTimeout;
+                    string responseText = GetResponseText(request);
+
+                    _logger.Info("Exchange task finished: " + responseText);
+
+                }     
+              
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Error processing task: " + task.TaskType + " for " + task.Scope + e.Message);
+            }
+        }
+
+        static bool Initialize()
+        {
+            _logger.Debug("Initialize ...");
+
+            try
+            {
+                #region init proxy info
+                _proxyCredentialToken = ConfigurationManager.AppSettings["ProxyCredentialToken"];
+                _proxyHost = ConfigurationManager.AppSettings["ProxyHost"];
+
+                string proxyPort = ConfigurationManager.AppSettings["ProxyPort"];
+                int.TryParse(proxyPort, out _proxyPort);
+                #endregion
+
+                #region init client credentials
+                _ssoURL = ConfigurationManager.AppSettings["SSO_URL"];
+                _clientId = ConfigurationManager.AppSettings["client_id"];
+                _clientSecret = ConfigurationManager.AppSettings["client_secret"];
+                _authType = ConfigurationManager.AppSettings["grant_type"];
+
+                string clientKey = ConfigurationManager.AppSettings["client_key"];
+                if (!string.IsNullOrEmpty(clientKey))
+                {
+                    _clientSecret = EncryptionUtility.Decrypt(_clientSecret, clientKey);
+                }
+                else
+                {
+                    _clientSecret = EncryptionUtility.Decrypt(_clientSecret);
+                }
+                #endregion
+
+                #region init agent sequence and timeout
+                //string configPath = ConfigurationManager.AppSettings["AgentConfig"];
+
+                //if (!File.Exists(configPath))
+                //{
+                //    _logger.Error("Agent Configuration not found.");
+                //    return false;
+                //}
+
+                string httpRequestTimeout = ConfigurationManager.AppSettings["RequestTimeout"];
+                if (!string.IsNullOrWhiteSpace(httpRequestTimeout))
+                {
+                    _requestTimeout = Convert.ToInt32(httpRequestTimeout);
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Initialization failed: " + ex.Message, ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        static WebRequest CreateWebRequest(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+
+            if (!string.IsNullOrEmpty(_proxyHost))
+            {
+                WebCredentials proxyCreds = new WebCredentials(_proxyCredentialToken);
+                if (proxyCreds.isEncrypted) proxyCreds.Decrypt();
+
+                WebProxy proxy = new WebProxy(_proxyHost, _proxyPort);
+                proxy.Credentials = proxyCreds.GetNetworkCredential();
+                request.Proxy = proxy;
+            }
+
+            return request;
+        }
+
+        static string GetResponseText(WebRequest request)
+        {
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string responseText = reader.ReadToEnd();
+            return responseText;
+        }
+
+        static string GetClientToken()
+        {
+            string clientToken = string.Empty;
+
+            try
+            {
+                WebRequest request = CreateWebRequest(_ssoURL);
+                request.Method = "POST";
+                string postData = string.Format("client_id={0}&client_secret={1}&grant_type={2}", _clientId, _clientSecret, _authType);
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(byteArray, 0, byteArray.Length);
+                requestStream.Close();
+
+                string responseText = GetResponseText(request);
+                Dictionary<string, string> responseObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseText);
+
+                clientToken = responseObj["access_token"];
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error getting access token: ", ex);
+            }
+
+            return clientToken;
+        }
+      
+    }
+}
---
+++ Utilities/Agent/iRINGAgent/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using log4net;
+using Newtonsoft.Json.Linq;
+using org.iringtools.library;
+using org.iringtools.adapter;
+using org.iringtools.utility;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using org.iringtools.agent;
+
+
+namespace org.iringtools.agent
+{
+    class Program
+    {
+        private static ILog _logger = LogManager.GetLogger(typeof(Program));
+        private static TaskSequence _sequence = null;
+       
+        static void Main(string[] args)
+        {
+            try
+            {
+                log4net.Config.XmlConfigurator.Configure();
+                //GenerateSampleAgentSequence();
+                string config = args[0]; //"D:\\Dev\\svn\\branches\\agent\\iring-tools\\iRINGAgent\\iRINGAgent\\bin\\App_Data\\AgentConfig.xml"; 
+                if (Initialize(config))
+                {
+                    RunTask();
+                }
+
+                _logger.Info("Agent task completed.");
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Agent task failed: " + ex.Message, ex);
+            }
+        }
+
+        static void RunTask()
+        {
+            _logger.Debug("RunTask ...");
+            AdapterSettings adapterSettings;
+            NameValueCollection settings;
+
+            foreach (Task task in _sequence.Tasks)
+            {
+                string project = task.Project;
+                string app = task.App;
+                settings = ConfigurationManager.AppSettings;
+
+                adapterSettings = new AdapterSettings();
+                adapterSettings.AppendSettings(settings);
+                adapterSettings["ProjectName"] = project;
+                adapterSettings["ApplicationName"] = app;
+                adapterSettings["Scope"] = project + "." + app;
+
+                //Call agent provider
+                AgentProvider agentProvider = new AgentProvider(adapterSettings);
+                agentProvider.ProcessTask(task);
+            }
+                    
+            _logger.Info("Task finished: " );
+        }
+
+        static bool Initialize(string configPath)
+        {
+            _logger.Debug("Initialize ...");
+
+            try
+            {
+                //string configPath = ConfigurationManager.AppSettings["AgentConfig"];
+                if (File.Exists(configPath))
+                {
+                    AgentConfig config = Utility.Read<AgentConfig>(configPath, true);
+                    foreach (TaskSequence sequence in config)
+                    {
+                        _sequence = sequence;
+                    }
+                }
+                else
+                {
+                    _logger.Error("Agent Configuration not found.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Initialization failed: " + ex.Message, ex);
+                return false;
+            }
+
+            return true;
+        }
+
+        static void GenerateSampleAgentSequence()
+        {
+            TaskSequence sequence = new TaskSequence
+            {
+                Name = "Test",
+                Tasks = new List<Task>
+                {
+                    new Task
+                    {
+                        TaskType = "cache",
+                        BaseURL = "",
+                        Project = "99999_000",
+                        App = "iw",
+                        Scope = "99999_000",
+                        ExchangeId = ""
+                    }
+                }
+            };
+
+            //Stream stream = Utility.SerializeToBinaryStream<AgentSequence>(sequence);
+
+            //Utility.WriteStream(stream, "SampleSequence.dat");
+
+            Stream stream = Utility.ReadStream("SampleAgentSequence.dat");
+
+            sequence = Utility.DeserializeBinaryStream<TaskSequence>(stream);
+
+            Utility.Write<TaskSequence>(sequence, "SampleAgentSequence.xml", true);
+        }
+
+       
+    }
+}
