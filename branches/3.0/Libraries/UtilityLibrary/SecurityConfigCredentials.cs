869827647187a088d6d000abedf47cf4547011a6
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-06-10 10:33:36 +0000

    Added for encrypting and Decrypting sec. config. connection string

---
+++ branches/3.0/Libraries/UtilityLibrary/SecurityConfigCredentials.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Runtime.Serialization;
+using System.Net;
+
+namespace org.iringtools.utility
+{
+    [DataContract]
+    public class SecurityConfigCredential
+    {
+        //protected static string _delimiter = "{`|";
+        protected static string _delimiter = "_";
+        protected static string[] _delimiterArray = { _delimiter };
+
+        public SecurityConfigCredential()
+        {
+            isEncrypted = false;
+        }
+
+        public SecurityConfigCredential(string encryptedCredentials)
+        {
+            if (encryptedCredentials != String.Empty && encryptedCredentials != null)
+            {
+                encryptedToken = encryptedCredentials;
+                isEncrypted = true;
+            }
+            else
+            {
+                isEncrypted = false;
+            }
+        }
+
+        public virtual void Encrypt()
+        {
+            string conString = dataSource + _delimiter + initialCatalog + _delimiter + userId +_delimiter + password;
+            
+            encryptedToken = Encryption.EncryptString(conString);
+            dataSource = null;
+            initialCatalog = null;
+            userId = null;
+            password = null;
+
+            isEncrypted = true;
+        }
+
+        public virtual void Decrypt()
+        {
+            string credentials = Encryption.DecryptString(encryptedToken);
+            string[] credentialsArray = credentials.Split(_delimiterArray, StringSplitOptions.None);
+
+            dataSource = credentialsArray[0];
+            initialCatalog = credentialsArray[1];
+            userId = credentialsArray[2];
+            password = credentialsArray[3];
+            encryptedToken = null;
+            isEncrypted = false;
+        }
+
+        public NetworkCredential GetNetworkCredential()
+        {
+            NetworkCredential credentials = null;
+
+            //if (this.userName != string.Empty && this.userName != null)
+            //{
+            //    credentials = new NetworkCredential(userName, password, domain);
+            //}
+            //else
+            //{
+            //    credentials = CredentialCache.DefaultNetworkCredentials;
+            //}
+
+            return credentials;
+        }
+
+        [DataMember(EmitDefaultValue = false)]
+        public bool isEncrypted { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string dataSource { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string initialCatalog { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string userId { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string password { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string encryptedToken { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string domain { get; set; }
+    }
+
+    
+}
