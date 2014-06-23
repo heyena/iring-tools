85dd3a2850f6b4ada61518d2c68a5ef599966072
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-06-23 09:23:39 +0000

    Added for getting all users

---
+++ branches/3.0/iRINGTools.Applications/Models/IsecurityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using org.iringtools.UserSecurity;
+using org.iringtools.library;
+using org.iringtools.mapping;
+using iRINGTools.Web.Helpers;
+using System.IO;
+
+namespace iRINGTools.Web.Models
+{
+    public interface IsecurityRepository
+    {
+      Users GetAllUsers(string format);
+
+     //   List<User> GetAllUsers(string format);
+
+    }
+}
