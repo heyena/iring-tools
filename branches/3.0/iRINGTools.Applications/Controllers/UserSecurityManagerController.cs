229a8ddc5956cbc6ed20ce84195abb9771432b3b
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-06-23 09:10:01 +0000

    Addded code for getting all users.

--- branches/3.0/iRINGTools.Applications/Controllers/UserSecurityManagerController.cs
+++ branches/3.0/iRINGTools.Applications/Controllers/UserSecurityManagerController.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
-namespace org.iringtools.web.Controllers
+using iRINGTools.Web.Helpers;
+using iRINGTools.Web.Models;
+using log4net;
+using org.iringtools.library;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
+using org.iringtools.utility;
+using System.Web.Script.Serialization;
+using org.iringtools.UserSecurity;
+namespace org.iringtools.web.controllers
 {
-    public class UserSecurityManagerController : org.iringtools.web.controllers.BaseController
+    public class UserSecurityManagerController : BaseController
     {
         //
         // GET: /UserSecurityManager/
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(UserSecurityManagerController));
+        private SecurityRepository _repository;
+        private CustomError _CustomError = null;
+        private CustomErrorLog _CustomErrorLog = null;
+
+        public UserSecurityManagerController() : this(new SecurityRepository()) { }
+
+        public UserSecurityManagerController(SecurityRepository repository)
+            : base()
+        {
+            _repository = repository;
+            //_repository.AuthHeaders = _authHeaders;
+        }
 
         public ActionResult Index()
         {
-            return View();
+            Users user = _repository.GetAllUsers("xml");
+            List<Users> items = new List<Users>();
+            items.Add(user);
+            return Json(items, JsonRequestBehavior.AllowGet);
+            
+
         }
 
     }
