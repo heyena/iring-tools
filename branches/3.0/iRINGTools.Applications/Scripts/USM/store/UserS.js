6c5f7b5c065740851906411fe823b0a613308402
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-06-25 10:50:39 +0000

    change in getting all users.

--- branches/3.0/iRINGTools.Applications/Scripts/USM/store/UserS.js
+++ branches/3.0/iRINGTools.Applications/Scripts/USM/store/UserS.js
@@ -18,7 +18,8 @@
             //data: [{ "UserName": "prashant", "UserFirstName": "Prashant", "UserLastName": "Dubey", "UserEmail": "pdubey@bechtel.com", "UserPhone": "8992", "UserDesc": "pdubey@bechtel.com"}] 
             proxy: {
                 type: 'ajax',
-                url: '/Scripts/USM/jsonfiles/users.json',
+                //url: '/Scripts/USM/jsonfiles/users.json',
+                url: 'usersecuritymanager/getUsers',
                 reader: {
                     type: 'json',
                     root: 'items'
