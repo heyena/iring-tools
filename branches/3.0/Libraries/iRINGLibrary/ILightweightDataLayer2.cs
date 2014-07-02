d55fcd4a5dcc253eec3f182194380bf7de619104
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-07-02 14:28:25 +0000

    Added for getting cache data on the basis of page size.

---
+++ branches/3.0/Libraries/iRINGLibrary/ILightweightDataLayer2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using log4net;
+using org.iringtools.adapter;
+using org.iringtools.utility;
+namespace org.iringtools.library
+{
+    interface ILightweightDataLayer2 : ILightweightDataLayer
+    {
+        IList<IDataObject> Search(string objectType, string query, int pageSize, int startIndex);
+        IList<IDataObject> Search(string objectType, string query, DataFilter filter, int pageSize, int startIndex);
+    }
+
+    public abstract class BaseLightweightDataLayer2 : BaseLightweightDataLayer, ILightweightDataLayer2
+    {
+        public BaseLightweightDataLayer2(AdapterSettings settings)
+            : base(settings)
+        {
+            _settings = settings;
+        }
+
+        public abstract IList<IDataObject> Search(string objectType, string query, int pageSize, int startIndex);
+        public abstract IList<IDataObject> Search(string objectType, string query, DataFilter filter, int pageSize, int startIndex);
+    }
+}
