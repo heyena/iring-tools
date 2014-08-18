8b69322cc3d621ef6c5138f290aceeb892533441
prashantdubey.it@gmail.com <prashantdubey.it@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-08-18 09:56:25 +0000

    add user form

---
+++ branches/3.0/iRINGTools.Applications/Scripts/USM/view/users/UserWindow.js
@@ -0,0 +1,96 @@
+
+Ext.define('USM.view.users.UserWindow', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.userwindow',
+
+    requires: [
+        'USM.view.users.UserForm'
+    ],
+
+    floating: 'true',
+    border: false,
+    modal: true,
+    layout: {
+        type: 'fit'
+    },
+    bodyPadding: 0,
+    title: 'User',
+    resizable: false,
+    initComponent: function () {
+        var me = this;
+
+        Ext.applyIf(me, {
+            dockedItems: [
+                {
+                    xtype: 'toolbar',
+                    dock: 'bottom',
+                    items: [
+                        {
+                            xtype: 'tbfill'
+                        },
+                        {
+                            xtype: 'button',
+                            handler: function (button, event) {
+                                me.onSave();
+                            },
+                            iconCls: 'icon-accept',
+                            text: 'Ok'
+                        },
+                        {
+                            xtype: 'button',
+                            handler: function (button, event) {
+                                me.onReset();
+                            },
+                            iconCls: 'icon-cancel',
+                            text: 'Cancel'
+                        }
+                    ]
+                }
+            ],
+            items: [
+                {
+                    xtype: 'userform'
+                }
+            ]
+        });
+
+        me.callParent(arguments);
+    },
+
+    onReset: function () {
+        var me = this;
+        var win = me.up('window');
+        me.down('userform').getForm().reset();
+        me.destroy();
+    },
+
+    onSave: function () {
+        var me = this;
+        var message;
+        var msg;
+        var form = me.down('userform');
+        if (form.getForm().isValid()) {
+            msg = new Ext.window.MessageBox();
+            msg.wait('Saving User ....');
+            form.submit({
+                url: 'usersecuritymanager/insertUsers',
+                success: function (f, a) {
+                    msg.close();
+                    me.destroy();
+                    var message = 'User saved successfully.';
+                    showDialog(400, 50, 'Alert', message, Ext.Msg.OK, null);
+                    Ext.getCmp('iduser').store.reload();
+                    return;
+                },
+                failure: function (f, a) {
+                    msg.close();
+                    me.destroy();
+                }
+            });
+        } else {
+            message = 'Please fil the form to save.';
+            showDialog(400, 100, 'Warning', message, Ext.Msg.OK, null);
+        }
+    }
+
+});
