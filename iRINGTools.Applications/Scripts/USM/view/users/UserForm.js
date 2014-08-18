8443d176bc32f1563c75ee09fbd78b0b2d8da5fe
prashantdubey.it@gmail.com <prashantdubey.it@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-08-18 09:56:25 +0000

    add user form

---
+++ iRINGTools.Applications/Scripts/USM/view/users/UserForm.js
@@ -0,0 +1,77 @@
+
+Ext.define('USM.view.users.UserForm', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.userform',
+    frame: false,
+    width: 400,
+    bodyPadding: 10,
+
+    initComponent: function() {
+        var me = this;
+
+        Ext.applyIf(me, {
+            items: [
+                {
+                    xtype: 'hiddenfield',
+                    name: 'ActionType',
+                    value: 'ADD'
+                },
+                {
+                    xtype: 'hiddenfield',
+                    name: 'UserId'
+                },
+                {
+                    xtype: 'hiddenfield',
+                    name: 'SiteId'
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'User Name',
+                    name: 'UserName',
+                    anchor: '100%',
+                    maxLength: 100,
+                    allowBlank: false
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'First Name',
+                    anchor: '100%',
+                    name: 'UserFirstName',
+                    maxLength: 50
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Last Name',
+                    anchor: '100%',
+                    name: 'UserLastName',
+                    maxLength: 50
+                },
+                 {
+                     xtype: 'textfield',
+                     fieldLabel: 'Email',
+                     name: 'UserEmail',
+                     anchor: '100%',
+                     maxLength: 50,
+                     vtype: 'email'
+                 },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Phone',
+                    anchor: '100%',
+                    name: 'UserPhone',
+                    maxLength: 50
+                },
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Description',
+                    anchor: '100%',
+                    name: 'UserDesc',
+                    maxLength: 255
+                }
+            ]
+        });
+
+        me.callParent(arguments);
+    }
+
+});
