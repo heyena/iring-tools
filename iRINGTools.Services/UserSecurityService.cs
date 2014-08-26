baf50fc315c3e97e139c822fb876b815fe8bcb76
deepak.bechtel@gmail.com <deepak.bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-08-26 14:15:22 +0000

    Add/Edit Delete functionality of permissions to role

--- iRINGTools.Services/UserSecurityService.cs
+++ iRINGTools.Services/UserSecurityService.cs
@@ -1037,6 +1037,37 @@ namespace org.iringtools.services
             _userSecurityProvider.FormatOutgoingMessage<Response>(response, format, true);
         }
 
+        [Description("Link permissions with role to the database.")]
+        [WebInvoke(Method = "POST", UriTemplate = "/insertRolePermissions?format={format}")]
+        public void InsertRolePermissions(string format, Stream stream)
+        {
+            if (string.IsNullOrEmpty(format))
+            { format = "xml"; }
+
+            Response response = new Response();
+            try
+            {
+                format = MapContentType(format);
+                if (format == "raw")
+                {
+                    throw new Exception("");
+                }
+                else
+                {
+                    XElement xElement = _userSecurityProvider.FormatIncomingMessage<RolePermissions>(stream, format);
+                    response = _userSecurityProvider.InsertRolePermissions(new XDocument(xElement));
+                }
+            }
+            catch (Exception ex)
+            {
+                CustomErrorLog objCustomErrorLog = new CustomErrorLog();
+                _CustomError = objCustomErrorLog.customErrorLogger(ErrorMessages.errGetUISettings, ex, _logger);
+                objCustomErrorLog.throwJsonResponse(_CustomError);
+            }
+            PrepareResponse(ref response);
+            _userSecurityProvider.FormatOutgoingMessage<Response>(response, format, true);
+        }
+
         #region Private Methods
         private string MapContentType(string format)
         {
