5dc332b3fe98589742ffd31ae18cecea61af1e13
deepak.bechtel@gmail.com <deepak.bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-09-24 07:45:59 +0000

    Added new endpoint to get databasedictionary

---
+++ branches/3.0/Libraries/AdapterLibrary/DictionaryProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using log4net;
+using System.Collections.Specialized;
+using Ninject;
+using org.iringtools.library;
+using System.Web;
+using org.iringtools.utility;
+
+
+namespace org.iringtools.adapter
+{
+    public class DictionaryProvider : BaseProvider
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(DictionaryProvider));
+        private string _connSecurityDb;
+        private int _siteID;
+        
+        [Inject]
+        public DictionaryProvider(NameValueCollection settings)
+            : base(settings)
+        {
+            try
+            {
+                // We have _settings collection available here.
+                _connSecurityDb = settings["SecurityConnection"];
+                _siteID = Convert.ToInt32(settings["SiteId"]);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Error initializing adapter provider: " + e.Message);
+            }
+        }
+
+
+        public DatabaseDictionary GetDBDictionary(string applicatyionId)
+        {
+            DatabaseDictionary databaseDictionary = new DatabaseDictionary();
+            try
+            {
+                NameValueList nvl = new NameValueList();
+                nvl.Add(new ListItem() { Name = "@ApplicationID", Value = applicatyionId });
+
+                string xmlString = DBManager.Instance.ExecuteXmlQuery(_connSecurityDb, "spgDataBaseDictionary", nvl);
+                databaseDictionary = utility.Utility.Deserialize<DatabaseDictionary>(xmlString, true);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error getting  Applications: " + ex);
+            }
+            return databaseDictionary;
+        }
+
+
+        public void FormatOutgoingMessage<T>(T graph, string format, bool useDataContractSerializer)
+        {
+            if (format.ToUpper() == "JSON")
+            {
+                string json = Utility.SerializeJson<T>(graph, useDataContractSerializer);
+
+                HttpContext.Current.Response.ContentType = "application/json; charset=utf-8";
+                HttpContext.Current.Response.Write(json);
+            }
+            else
+            {
+                string xml = Utility.Serialize<T>(graph, useDataContractSerializer);
+
+                HttpContext.Current.Response.ContentType = "application/xml";
+                HttpContext.Current.Response.Write(xml);
+            }
+        }
+
+    }
+
+
+}
---
+++ branches/3.0/iRINGTools.Services/DictionaryService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using log4net;
+using org.iringtools.adapter;
+using org.iringtools.library;
+using System.Configuration;
+using System.ComponentModel;
+using System.ServiceModel.Web;
+
+namespace org.iringtools.services
+{
+    [ServiceContract(Namespace = "http://www.iringtools.org/service")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class DictionaryService
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(DictionaryService));
+        private DictionaryProvider _dictionaryProvider = null;
+        private CustomError _CustomError = null;
+
+        public DictionaryService()
+        {
+            _dictionaryProvider = new DictionaryProvider(ConfigurationManager.AppSettings);
+        }
+
+
+        [Description("Gets all applications available from the data base.")]
+        [WebGet(UriTemplate = "/getDBDictionary/{applicatyionId}?format={format}")]
+        public void GetDBDictionary(string applicatyionId, string format)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(format))
+                { format = "xml"; }
+
+                DatabaseDictionary applications = _dictionaryProvider.GetDBDictionary(applicatyionId);
+                _dictionaryProvider.FormatOutgoingMessage<DatabaseDictionary>(applications, format, true);
+            }
+
+            catch (Exception ex)
+            {
+                CustomErrorLog objCustomErrorLog = new CustomErrorLog();
+                _CustomError = objCustomErrorLog.customErrorLogger(ErrorMessages.errGetScope, ex, _logger);
+                objCustomErrorLog.throwJsonResponse(_CustomError);
+            }
+        }
+
+    }
+}
