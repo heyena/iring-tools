2cac91734a4e8bd4cb67064999f00f92c34e9dba
sujandotnet@gmail.com <sujandotnet@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-09-24 19:11:54 +0000

    -> added precision and scale in Manifest.

--- trunk/Libraries/iRINGLibrary/Manifest.cs
+++ trunk/Libraries/iRINGLibrary/Manifest.cs
@@ -407,9 +407,12 @@ namespace org.iringtools.dxfr.manifest
 
         private string valueField;
 
+        private string dbDataTypeField;
+
         private int precisionField;
 
         private int scaleField;
+
 
         private org.iringtools.dxfr.manifest.Class classField;
 
@@ -519,6 +522,18 @@ namespace org.iringtools.dxfr.manifest
             }
         }
         [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 8)]
+        public string dbDataType
+        {
+            get
+            {
+                return this.dbDataTypeField;
+            }
+            set
+            {
+                this.dbDataTypeField = value;
+            }
+        }
+        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 9)]
         public int precision
         {
             get
@@ -530,7 +545,9 @@ namespace org.iringtools.dxfr.manifest
                 this.precisionField = value;
             }
         }
-        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 9)]
+
+
+        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 10)]
         public int scale
         {
             get
@@ -1227,6 +1244,7 @@ namespace org.iringtools.mapping
             }
         }
 
+        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 8)]
         public int dataLength
         {
             get
@@ -1238,6 +1256,7 @@ namespace org.iringtools.mapping
                 this.dataLengthField = value;
             }
         }
+        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 9)]
         public int scale
         {
             get
@@ -1250,6 +1269,7 @@ namespace org.iringtools.mapping
             }
         }
 
+        [System.Runtime.Serialization.DataMemberAttribute(EmitDefaultValue = false, Order = 10)]
         public int precision
         {
             get
