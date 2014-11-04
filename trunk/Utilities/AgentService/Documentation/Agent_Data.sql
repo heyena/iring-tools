7db5271064ab0b48237c49a9ce179c9f349c9052
sujandotnet@gmail.com <sujandotnet@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-11-04 19:23:34 +0000

    -> Updated insert statement for SCHEDULECACHE and SCHEDULEExchange.

--- trunk/Utilities/AgentService/Documentation/Agent_Data.sql
+++ trunk/Utilities/AgentService/Documentation/Agent_Data.sql
@@ -1,11 +1,16 @@
 USE [iRINGAgent]
 
-
 DELETE FROM [SCHEDULECACHE]
-INSERT INTO [SCHEDULECACHE] ([TASK_NAME],[PROJECT],[APP],[CACHE_PAGE_SIZE],[SSO_URL],[CLIENT_ID],[CLIENT_SECRET],[GRANT_TYPE],[APP_KEY],[ACCESS_TOKEN],[REQUEST_TIMEOUT],[START_TIME],[END_TIME],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[STATUS],[ACTIVE] ) VALUES('99999_000.iw','99999_000','iw',2000,'https://sso.staging.mypsn.com/as/token.oauth2','IW_DataLayer','tmPhR7ZNngdXtsQrmEKhLhi7wa7mHhG0GX3Zn9HvillzCCwf5b/JDMC/a65QkeB6','client_credentials','wHKxvUyEqrLTNSvsVTPX1GJs02nAo5IF','TmMopozebXnR8ky6YgRnAV22ICOz',300000,'08/07/2014 06:00:00','','08/07/2014 07:00:00','Gakhar Hemant','Daily','','10/16/2014 06:00:00','Ready',1)
-INSERT INTO [SCHEDULECACHE] ([TASK_NAME],[PROJECT],[APP],[CACHE_PAGE_SIZE],[SSO_URL],[CLIENT_ID],[CLIENT_SECRET],[GRANT_TYPE],[APP_KEY],[ACCESS_TOKEN],[REQUEST_TIMEOUT],[START_TIME],[END_TIME],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[STATUS],[ACTIVE] ) VALUES('55555_000.iw','55555_000','iw',2000,'https://sso.staging.mypsn.com/as/token.oauth2','IW_DataLayer','tmPhR7ZNngdXtsQrmEKhLhi7wa7mHhG0GX3Zn9HvillzCCwf5b/JDMC/a65QkeB6','client_credentials','wHKxvUyEqrLTNSvsVTPX1GJs02nAo5IF','TmMopozebXnR8ky6YgRnAV22ICOz',300000,'08/07/2014 06:00:00','','08/07/2014 06:00:00','Gakhar Hemant','Daily','','09/26/2014 06:00:00','Ready',1)
+INSERT INTO [iRINGAgent].[dbo].[SCHEDULECACHE] ([TASK_NAME],[PROJECT],[APP],[CACHE_PAGE_SIZE],[SSO_URL],[CLIENT_ID],[CLIENT_SECRET],[GRANT_TYPE],[APP_KEY],[ACCESS_TOKEN],[REQUEST_TIMEOUT],[START_TIME],[END_TIME],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[STATUS],[ACTIVE] ) 
+VALUES('scope.App','Scope','App',2000,'https://sso.mypsn.com/as/token.oauth2','iRingTools','JxX6VHu8uZYff7zPUkTNw7sdDodvF3G3','client_credentials','wHKxvUyEqrLTNSvsVTPX1GJs02nAo5IF','TmMopozebXnR8ky6YgRnAV22ICOz',300000,'08/07/2014 06:00:00','','08/07/2014 07:00:00','Sujan Gorkhali','Daily','','10/16/2014 06:00:00','Ready',1)	
 
 
-DELETE FROM [SCHEDULECACHE]
-INSERT INTO [SCHEDULECACHE] ([TASK_NAME],[PROJECT],[APP],[CACHE_PAGE_SIZE],[SSO_URL],[CLIENT_ID],[CLIENT_SECRET],[GRANT_TYPE],[APP_KEY],[ACCESS_TOKEN],[REQUEST_TIMEOUT],[START_TIME],[END_TIME],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[STATUS],[ACTIVE] ) VALUES('agent.source','agent','source',2000,'https://sso.mypsn.com/as/token.oauth2','iRingTools','0Lvnvat5T5OJk5n6VwD4optFJoq7/0POq++NfYkIgHYtmy6Pluix3aGy7EAN1Jxp','client_credentials','wHKxvUyEqrLTNSvsVTPX1GJs02nAo5IF','TmMopozebXnR8ky6YgRnAV22ICOz',300000,'08/07/2014 06:00:00','','08/07/2014 07:00:00','Gakhar Hemant','Daily','','10/16/2014 06:00:00','Ready',1)
+DELETE FROM [SCHEDULEExchange]
+INSERT INTO [iRINGAgent].[dbo].[SCHEDULEExchange] 
+	([Task_Name],[Scope],[Base_Url],[Exchange_Id],[Sso_Url],[Client_Id],[Client_Secret],[Grant_Type],[Request_Timeout],[Start_Time],[End_Time],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[Status],[Active] ) 
+	VALUES('Task_Name','ExchangeScope','ExchangeServerUrl/runUnattendedExchange','ExchangeID','https://sso.mypsn.com/as/token.oauth2','iRingTools','JxX6VHu8uZYff7zPUkTNw7sdDodvF3G3','client_credentials',300000,'08/07/2014 06:00:00','','08/07/2014 07:00:00','Sujan Gorkhali','Daily','','10/16/2015 06:00:00','Ready',1)
+
+INSERT INTO [iRINGAgent].[dbo].[SCHEDULEExchange] 
+	([Task_Name],[Scope],[Base_Url],[Exchange_Id],[Sso_Url],[Client_Id],[Client_Secret],[Grant_Type],[Request_Timeout],[Start_Time],[End_Time],[Created_Date],[Created_By],[Occurance],[NextStart_Date_Time],[End_Date_Time],[Status],[Active] ) 
+	VALUES('Sender->Receiver','AgentServices','http://localhost:8087/iringtools-apps/xchmgr','1','https://sso.mypsn.com/as/token.oauth2','iRingTools','JxX6VHu8uZYff7zPUkTNw7sdDodvF3G3','client_credentials',300000,'08/07/2014 06:00:00','','08/07/2014 07:00:00','Sujan Gorkhali','Daily','','10/16/2015 06:00:00','Ready',1)
 
