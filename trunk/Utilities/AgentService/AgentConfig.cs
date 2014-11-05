9f52b5d19074e66e676bf438294e3fb0102d09e2
hemant21 <hemant21@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-11-05 18:11:42 +0000

    Modified to support new database changes

--- trunk/Utilities/AgentService/AgentConfig.cs
+++ trunk/Utilities/AgentService/AgentConfig.cs
@@ -13,22 +13,28 @@ namespace iRINGAgentService
     public class AgentConfig
     {
 
-        public string TaskId { get; set; }
-        public int TaskType { get; set; }
-        public string Project { get; set; }
-        public string App { get; set; }
+        public string JobId { get; set; }
+        public int IsExchange { get; set; }
         public string Scope { get; set; }
-        public string BaseUrl { get; set; }
+        public string App { get; set; }
+        public string DataObject { get; set; }
         public string ExchangeId { get; set; }
+        public string ExchangeUrl { get; set; }
         public string CachePageSize { get; set; }
         public string SsoUrl { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-        public string GrantType { get; set; }
-        public string AppKey { get; set; }
         public string AccessToken { get; set; }
+        public string AppKey { get; set; }
+        public string GrantType { get; set; }
         public int RequestTimeout { get; set; }
+        public string ScheduleId { get; set; }
+        public string Occurance { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
         public string Status { get; set; }
+        public DateTime NextStartDateTime { get; set; }
+        public DateTime LastStartDateTime { get; set; }
         public int Active { get; set; }
 
     }
