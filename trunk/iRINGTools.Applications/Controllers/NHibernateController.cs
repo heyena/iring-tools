4666ef8f238fa116ee7670185bf33c57a05b41fe
rohit.jangwal@gmail.com <rohit.jangwal@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2014-11-17 08:17:59 +0000

    add new method (ObjectsTreeSql) in NHibernateController.cs for SqlObjectsTreePanel

--- trunk/iRINGTools.Applications/Controllers/NHibernateController.cs
+++ trunk/iRINGTools.Applications/Controllers/NHibernateController.cs
@@ -177,6 +177,263 @@ namespace org.iringtools.web.controllers
                     bool checkDictionary = (dictionary != null && dictionary.dataObjects != null && dictionary.dataObjects.Count > 0);
                     List<Node> objectNodes = root.children;
 
+                    foreach (DataObject dbObject in dbObjects)
+                    {
+                        Node keyPropertiesNode = new Node()
+                        {
+                            text = "Keys",
+                            type = "keys",
+                            iconCls = "folder",
+                            expanded = true,
+                            children = new List<Node>()
+                        };
+
+                        Node dataPropertiesNode = new Node()
+                        {
+                            text = "Properties",
+                            type = "properties",
+                            iconCls = "folder",
+                            expanded = true,
+                            children = new List<Node>()
+                        };
+
+                        Node relationshipsNode = new Node()
+                        {
+                            text = "Relationships",
+                            type = "relationships",
+                            iconCls = "folder",
+                            expanded = true,
+                            children = new List<Node>()
+                        };
+
+                        // create object node
+                        Node dataObjectNode = new Node()
+                        {
+                            text = dbObject.tableName,
+                            type = "dataObject",
+                            iconCls = "treeObject",
+                            children = new List<Node>() { keyPropertiesNode, dataPropertiesNode, relationshipsNode },
+                            properties = new Dictionary<string, object>() { 
+                                {"dataProperties", dbObject.dataProperties} 
+                            }
+                        };
+
+                        DataObject dictObject = null;
+
+                        //
+                        // apply what has been configured in dictionary to db object
+                        //
+                        if (checkDictionary)
+                        {
+                            dictObject = dictionary.dataObjects.Find(x => x.tableName == dbObject.tableName);
+                        }
+
+                        if (dictObject == null)  // has not been configured
+                        {
+                            dataObjectNode.properties.Add("objectNamespace", ONS_PREFIX + scope + "." + app);
+                            dataObjectNode.properties.Add("objectName", dbObject.objectName);
+                            dataObjectNode.properties.Add("tableName", dbObject.tableName);
+                            dataObjectNode.properties.Add("keyDelimiter", dbObject.keyDelimeter);
+                            dataObjectNode.properties.Add("description", dbObject.description);
+                        }
+                        else  // has been configured, apply object configurations
+                        {
+                            dataObjectNode.properties.Add("objectNamespace", ONS_PREFIX + scope + "." + app);
+                            dataObjectNode.properties.Add("objectName", dictObject.objectName);
+                            dataObjectNode.properties.Add("tableName", dictObject.tableName);
+                            dataObjectNode.properties.Add("keyDelimiter", dictObject.keyDelimeter);
+                            dataObjectNode.properties.Add("description", dictObject.description);
+
+                            // apply relationship configurations
+                            foreach (DataRelationship relationship in dictObject.dataRelationships)
+                            {
+                                Node relationshipNode = new Node()
+                                {
+                                    text = relationship.relationshipName,
+                                    type = "relationship",
+                                    iconCls = "relationship",
+                                    leaf = true,
+                                    properties = new Dictionary<string, object>()
+                                    {
+                                        {"name", relationship.relationshipName},
+                                        {"type", relationship.relationshipType.ToString() },
+                                        {"sourceObject", dictObject.objectName},
+                                        {"relatedObject", relationship.relatedObjectName},
+                                        {"propertyMaps", relationship.propertyMaps}
+                                    }
+                                };
+
+                                relationshipsNode.children.Add(relationshipNode);
+                            }
+                        }
+
+                        foreach (DataProperty dbProperty in dbObject.dataProperties)
+                        {
+                            if (dictObject != null)
+                            {
+                                DataProperty dictProperty = dictObject.dataProperties.Find(x => x.columnName == dbProperty.columnName);
+
+                                if (dictProperty != null)   // property has been configured
+                                {
+                                    Dictionary<string, object> properties = new Dictionary<string, object>()
+                                    {
+                                        {"columnName", dictProperty.columnName},
+                                        {"propertyName", dictProperty.propertyName},
+                                        {"dataType", dictProperty.dataType},
+                                        {"dataLength", dictProperty.dataLength},
+                                        {"isNullable", dictProperty.isNullable},
+                                        {"keyType", dictProperty.keyType},
+                                        {"numberOfDecimals", dictProperty.numberOfDecimals},
+                                        {"isReadOnly", dictProperty.isReadOnly},
+                                        {"showOnIndex", dictProperty.showOnIndex},
+                                        {"showOnSearch", dictProperty.showOnSearch},
+                                        {"isHidden", dictProperty.isHidden},
+                                        {"isVirtual", dictProperty.isVirtual},
+                                        {"precision", dictProperty.precision},
+                                        {"scale", dictProperty.scale}
+                                    };
+
+                                    bool isKey = false;
+
+                                    if (dictObject.isKeyProperty(dictProperty.columnName))
+                                    {
+                                        Node keyPropertyNode = new Node()
+                                        {
+                                            text = dictProperty.columnName,
+                                            type = "keyProperty",
+                                            iconCls = "treeKey",
+                                            leaf = true,
+                                            properties = properties
+                                        };
+
+                                        keyPropertiesNode.children.Add(keyPropertyNode);
+                                        isKey = true;
+                                    }
+                                    else if (!isKey)
+                                    {
+                                        Node dataPropertyNode = new Node()
+                                        {
+                                            text = dictProperty.columnName,
+                                            type = "dataProperty",
+                                            iconCls = "treeProperty",
+                                            leaf = true,
+                                            properties = properties
+                                        };
+
+                                        dataPropertiesNode.children.Add(dataPropertyNode);
+                                    }
+                                }
+                            }
+                        }
+
+                        objectNodes.Add(dataObjectNode);
+                    }
+                }
+
+                return Json(objectsTree);
+            }
+            catch (Exception e)
+            {
+                //return Json(new { success = false, message = e.ToString() });
+                _logger.Error(e.ToString());
+                _CustomErrorLog = new CustomErrorLog();
+                _CustomError = _CustomErrorLog.customErrorLogger(ErrorMessages.errUItree, e, _logger);
+                return Json(new { success = false, message = "[ Message Id " + _CustomError.msgId + "] - " + _CustomError.errMessage, stackTraceDescription = _CustomError.stackTraceDescription }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public ActionResult ObjectsTreeSql(FormCollection form)
+        {
+            try
+            {
+
+                string scope = form["scope"];
+                string app = form["app"];
+                string selectedTables = form["selectedTables"];
+
+                _repository.Session = Session;
+
+                List<Node> objectsTree = new List<Node>();
+
+                Node root = new Node()
+                {
+                    text = "Data Objects",
+                    type = "dataObjects",
+                    iconCls = "folder",
+                    expanded = true,
+                    children = new List<Node>(),
+                    properties = new Dictionary<string, object>()
+                };
+
+                objectsTree.Add(root);
+
+                DatabaseDictionary dictionary = _repository.GetDBDictionary(scope, app);
+
+                Dictionary<string, string> conElts = null;
+                if (dictionary != null && !string.IsNullOrEmpty(dictionary.ConnectionString))
+                {
+                    string dbProvider = dictionary.Provider;
+                    string conStr = Utility.DecodeFrom64(dictionary.ConnectionString);
+
+                    conElts = GetConnectionElements(dbProvider, conStr);
+                    conElts["dbSchema"] = dictionary.SchemaName;
+                    root.properties.Add("connectionInfo", conElts);
+                }
+                else
+                {
+                    conElts = new Dictionary<string, string>();
+                    conElts.Add("dbProvider", form["dbProvider"]);
+                    conElts.Add("dbServer", form["dbServer"]);
+                    conElts.Add("dbInstance", form["dbInstance"]);
+                    conElts.Add("serName", form["serName"]);
+                    conElts.Add("dbName", form["dbName"]);
+                    conElts.Add("dbSchema", form["dbSchema"]);
+                    conElts.Add("dbUserName", form["dbUserName"]);
+                    conElts.Add("dbPassword", form["dbPassword"]);
+                    conElts.Add("portNumber", form["portNumber"]);
+                }
+
+                List<DataObject> dbObjects = null;
+                if (!string.IsNullOrEmpty(conElts["dbServer"]))
+                {
+                    //
+                    // get available tables
+                    //
+                    List<string> availTables = _repository.GetTableNames(scope, app, conElts);
+                    root.properties.Add("tableNames", availTables);
+
+                    //
+                    // get selected tables metadata
+                    //
+                    List<string> configTables = null;
+
+                    if (!string.IsNullOrEmpty(selectedTables))
+                    {
+                        string[] array = selectedTables.Split(',');
+                        configTables = new List<string>(array);
+                    }
+                    else if (dictionary != null && dictionary.dataObjects.Count > 0)
+                    {
+                        configTables = new List<string>();
+
+                        foreach (DataObject dataObject in dictionary.dataObjects)
+                        {
+                            configTables.Add(dataObject.tableName);
+                        }
+                    }
+
+                    if (configTables != null && configTables.Count > 0)
+                    {
+                        dbObjects = _repository.GetDBObjects(scope, app, conElts, configTables);
+                    }
+                }
+
+                if (dbObjects != null)
+                {
+                    bool checkDictionary = (dictionary != null && dictionary.dataObjects != null && dictionary.dataObjects.Count > 0);
+                    List<Node> objectNodes = root.children;
+
                     List<string> availTables = new List<string>();
 
                     foreach (DataObject dbObject in dbObjects)
@@ -382,9 +639,9 @@ namespace org.iringtools.web.controllers
                 _CustomError = _CustomErrorLog.customErrorLogger(ErrorMessages.errUItree, e, _logger);
                 return Json(new { success = false, message = "[ Message Id " + _CustomError.msgId + "] - " + _CustomError.errMessage, stackTraceDescription = _CustomError.stackTraceDescription }, JsonRequestBehavior.AllowGet);
             }
-}
+        }
 
-       
+
       
         public ActionResult SaveDBDictionary()
         {
