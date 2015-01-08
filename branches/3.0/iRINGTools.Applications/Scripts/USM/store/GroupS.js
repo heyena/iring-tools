6f8aaa77909043f789d85392222625fa1246e3a0
deepak.bechtel@gmail.com <deepak.bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-01-08 04:28:26 +0000

    Resolved bug: Before opening users if we open group and try to add users in group,in that case it was not showing users, same applies to group/roles and other mapping

--- branches/3.0/iRINGTools.Applications/Scripts/USM/store/GroupS.js
+++ branches/3.0/iRINGTools.Applications/Scripts/USM/store/GroupS.js
@@ -13,7 +13,7 @@
             actionMethod: {
                 read: 'GET'
             },
-            autoLoad: false,
+            autoLoad: true,
             model: 'USM.model.GroupM',
             storeId: storeId,
             proxy: {
