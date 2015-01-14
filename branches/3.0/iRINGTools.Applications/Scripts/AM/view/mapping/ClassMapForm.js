932e5b337f7cfba28f53c21a7475ad276469d341
sujandotnet@gmail.com <sujandotnet@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-01-14 21:35:31 +0000

    -> changing default delimiter to ~ from _ .

--- branches/3.0/iRINGTools.Applications/Scripts/AM/view/mapping/ClassMapForm.js
+++ branches/3.0/iRINGTools.Applications/Scripts/AM/view/mapping/ClassMapForm.js
@@ -118,7 +118,7 @@ Ext.define('AM.view.mapping.ClassMapForm', {
             margin: '5 0 15 25',
             width: 100,
             name: 'delimeter',
-            value: '_',
+            value: '~',
             allowBlank: false
         },
         {
@@ -356,7 +356,7 @@ Ext.define('AM.view.mapping.ClassMapForm', {
         var classId = '';
         var classIndex = '0';
         var newEdit = 'NEW';
-        var delimeter = '_';
+        var delimeter = '~';
         /*if (record != null && record.classMap != null) {
             identifier = record.classMap.identifiers[0];
             if (record.classMap.identifiers.length > 1) {
