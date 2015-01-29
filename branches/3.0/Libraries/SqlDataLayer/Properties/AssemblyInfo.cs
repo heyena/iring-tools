afe144a53820c31ad86e7e308b5d23e604874dbc
Abhishek.Bechtel@gmail.com <Abhishek.Bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-01-29 03:47:42 +0000

    Changed assembly version as per iring standards

--- branches/3.0/Libraries/SqlDataLayer/Properties/AssemblyInfo.cs
+++ branches/3.0/Libraries/SqlDataLayer/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SqlDataLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bechtel")]
-[assembly: AssemblyProduct("SqlDataLayer")]
-[assembly: AssemblyCopyright("Copyright © Bechtel 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("86f9f479-c951-45c8-b5cb-40173ca42f1a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
