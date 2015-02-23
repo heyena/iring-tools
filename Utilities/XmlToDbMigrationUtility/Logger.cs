6fa9450694f9e23917e54d875f693b9c9b97b98e
Abhishek.Bechtel@gmail.com <Abhishek.Bechtel@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-02-23 09:08:39 +0000

    Added utility application to handle XML data migration.

---
+++ Utilities/XmlToDbMigrationUtility/Logger.cs
@@ -0,0 +1,43 @@
+﻿namespace XmlToDbMigrationUtility.Logging
+{
+    using log4net;
+    using log4net.Repository.Hierarchy;
+    using log4net.Core;
+    using log4net.Appender;
+    using log4net.Layout;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class Logger
+    {
+        public static void Setup()
+        {
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            PatternLayout patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%date [%thread] %-5level %logger - %message%newline";
+            patternLayout.ActivateOptions();
+
+            RollingFileAppender roller = new RollingFileAppender();
+            roller.AppendToFile = false;
+            roller.File = @"Logs\XmlToDbMigrationUtilityLog.log";
+            roller.Layout = patternLayout;
+            roller.MaxSizeRollBackups = 5;
+            roller.MaximumFileSize = "1GB";
+            roller.RollingStyle = RollingFileAppender.RollingMode.Size;
+            roller.StaticLogFileName = true;
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            MemoryAppender memory = new MemoryAppender();
+            memory.ActivateOptions();
+            hierarchy.Root.AddAppender(memory);
+
+            hierarchy.Root.Level = Level.Info;
+            hierarchy.Configured = true;
+
+            log4net.Config.BasicConfigurator.Configure(roller);
+        }
+    }
+}
