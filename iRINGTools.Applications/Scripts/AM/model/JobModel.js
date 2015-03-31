4e73fc554c4d990adada73848e6135671e667173
rohit.jangwal@gmail.com <rohit.jangwal@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-03-31 09:10:53 +0000

    Add model for scheduler

---
+++ iRINGTools.Applications/Scripts/AM/model/JobModel.js
@@ -0,0 +1,47 @@
+﻿Ext.define('AM.model.JobModel', {
+    extend: 'Ext.data.Model',
+
+    fields: [
+        {
+
+            name: 'job_Id'
+        },
+        {
+            name: 'SiteId'
+        },
+        {
+            name: 'Scope'
+          
+        },
+        {
+            name: 'App'
+        },
+        {
+            name: 'DataObject'   
+        },
+        {
+            name: 'Status'
+        },
+        {
+            name: 'Last_Start_DateTime'
+        },
+        {
+            name: 'Next_Start_DateTime'
+        },
+        {
+            name: 'Start_DateTime'
+        },
+        {
+            name: 'End_DateTime'
+        }, 
+        {
+            name: 'PlatformId'
+        }, 
+        {
+            name: 'TotalRecords'
+        },
+        {
+            name: 'CachedRecords'
+        }
+    ]
+});
