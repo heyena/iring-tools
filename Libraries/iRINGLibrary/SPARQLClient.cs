0fa76c04f5e12874d1eba5925dfd8fba7d661a34
rdecarlo73 <rdecarlo73@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-04-06 20:04:45 +0000

    Added functionality to the MultiPartMessage POST without breaking existing requests.

--- Libraries/iRINGLibrary/SPARQLClient.cs
+++ Libraries/iRINGLibrary/SPARQLClient.cs
@@ -18,91 +18,92 @@ using org.iringtools.utility;
 
 namespace org.iringtools.library
 {
-  public class SPARQLClient
-  {
-		private static readonly ILog _logger = LogManager.GetLogger(typeof(SPARQLClient));
-    public static SPARQLResults PostQuery(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
+    public class SPARQLClient
     {
-      try
-      {
-        SPARQLResults sparqlResults = null;
-
-        string message = "query=" + HttpUtility.UrlEncode(sparql);
-
-        WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
-        sparqlResults = webClient.PostMessage<SPARQLResults>("", message, false);
-
-        return sparqlResults;
-      }
-      catch (Exception exception)
-      {
-				_logger.Error("Error in PostQuery: " + exception);
-        throw exception;
-      }
-    }
-
-    public static void PostQueryAsMultipartMessage(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
-    {
-      try
-      {
-        string result = string.Empty;
-        MultiPartMessage requestMessage = new MultiPartMessage
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(SPARQLClient));
+        public static SPARQLResults PostQuery(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
         {
-          name = "update",
-          type = MultipartMessageType.FormData,
-          message = sparql,
-        };
+            try
+            {
+                SPARQLResults sparqlResults = null;
 
-        List<MultiPartMessage> requestMessages = new List<MultiPartMessage>
+                string message = "query=" + HttpUtility.UrlEncode(sparql);
+
+                WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
+                sparqlResults = webClient.PostMessage<SPARQLResults>("", message, false);
+
+                return sparqlResults;
+            }
+            catch (Exception exception)
+            {
+                _logger.Error("Error in PostQuery: " + exception);
+                throw exception;
+            }
+        }
+
+        public static void PostQueryAsMultipartMessage(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
+        {
+            try
+            {
+                string result = string.Empty;
+                MultiPartMessage requestMessage = new MultiPartMessage
+                {
+                    name = "update",
+                    type = MultipartMessageType.FormData,
+                    message = sparql,
+                };
+
+                List<MultiPartMessage> requestMessages = new List<MultiPartMessage>
         {
           requestMessage
         };
 
-        WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
+                WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
 
-        webClient.PostMultipartMessage("", requestMessages);
-      }
-      catch (Exception exception)
-      {
-				_logger.Error("Error in PostQueryAsMultipartMessage: " + exception);
-				throw exception;
-      }
+                string response = String.Empty;
+                webClient.PostMultipartMessage("", requestMessages, ref response);
+            }
+            catch (Exception exception)
+            {
+                _logger.Error("Error in PostQueryAsMultipartMessage: " + exception);
+                throw exception;
+            }
+        }
+
+        public static SPARQLResults Query(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
+        {
+            try
+            {
+                SPARQLResults sparqlResults = null;
+
+                string relativeUri = "?query=" + HttpUtility.UrlEncode(sparql);
+
+                WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
+                sparqlResults = webClient.Get<SPARQLResults>(relativeUri, false);
+
+                return sparqlResults;
+            }
+            catch (Exception exception)
+            {
+                _logger.Error("Error in Query: " + exception);
+                throw exception;
+            }
+        }
+        public static string Update(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
+        {
+            try
+            {
+                string message = String.Empty;
+                string relativeUri = "?update=" + HttpUtility.UrlEncode(sparql);
+                WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
+                message = webClient.GetMessage(relativeUri);
+                return message;
+            }
+            catch (Exception exception)
+            {
+                _logger.Error("Error in Update: " + exception);
+                throw exception;
+            }
+        }
     }
-
-    public static SPARQLResults Query(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
-    {
-      try
-      {
-        SPARQLResults sparqlResults = null;
-
-        string relativeUri = "?query=" + HttpUtility.UrlEncode(sparql);
-
-        WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
-        sparqlResults = webClient.Get<SPARQLResults>(relativeUri, false);
-
-        return sparqlResults;
-      }
-      catch (Exception exception)
-      {
-				_logger.Error("Error in Query: " + exception);
-				throw exception;
-      }
-    }
-    public static string Update(string baseUri, string sparql, WebCredentials targetCredentials, WebProxyCredentials proxyCredentials)
-    {
-      try
-      {
-        string message = String.Empty;
-        string relativeUri = "?update=" + HttpUtility.UrlEncode(sparql);
-        WebHttpClient webClient = new WebHttpClient(baseUri, targetCredentials.GetNetworkCredential(), proxyCredentials.GetWebProxy());
-        message = webClient.GetMessage(relativeUri);
-        return message;
-      }
-      catch (Exception exception)
-      {
-				_logger.Error("Error in Update: " + exception);
-				throw exception;
-      }
-    }
-  }
 }
