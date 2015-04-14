2f5801c9d97b39ea4ba48f1b5d053ffa61d11ea2
rohit.jangwal@gmail.com <rohit.jangwal@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-04-14 10:01:16 +0000

    fixed issue for extension drag to the identifier box in Graph

--- trunk/iRINGTools.Applications/Scripts/AM/view/mapping/ClassMapForm.js
+++ trunk/iRINGTools.Applications/Scripts/AM/view/mapping/ClassMapForm.js
@@ -202,13 +202,13 @@ Ext.define('AM.view.mapping.ClassMapForm', {
             copy: false,
             overClass: 'over',
             notifyOver: function (dragSource, event, data) {
-                if (data.records[0].data.type != 'DataPropertyNode' && data.records[0].data.type != 'KeyDataPropertyNode')
+                if (data.records[0].data.type != 'DataPropertyNode' && data.records[0].data.type != 'KeyDataPropertyNode' && data.records[0].data.type != 'ExtensionNode')
                     return this.dropNotAllowed;
                 else
                     return this.dropAllowed;
             },
             notifyDrop: function (dragSource, event, data) {
-                if (data.records[0].data.type != 'DataPropertyNode' && data.records[0].data.type != 'KeyDataPropertyNode') {
+                if (data.records[0].data.type != 'DataPropertyNode' && data.records[0].data.type != 'KeyDataPropertyNode' && data.records[0].data.type != 'ExtensionNode') {
                     return false;
                 }
                 else {
