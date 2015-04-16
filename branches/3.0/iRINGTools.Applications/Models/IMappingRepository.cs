83d214064dfbd9d17104623297d5d33baffb85e9
atul.s2210@gmail.com <atul.s2210@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-04-16 10:49:45 +0000

    Added insert graph functionality logic

--- branches/3.0/iRINGTools.Applications/Models/IMappingRepository.cs
+++ branches/3.0/iRINGTools.Applications/Models/IMappingRepository.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using org.iringtools.mapping;
-
+using org.iringtools.applicationConfig;
 namespace iRINGTools.Web.Models
 {
   public interface IMappingRepository
   {
     Mapping GetMapping(string scopeName, string applicationName);
     void UpdateMapping(string scopeName, string applicationName, Mapping mapping);
+    void UpdateMapping(string scopeName, string applicationName,  Graph graph,string userName);
+
   }
 }
