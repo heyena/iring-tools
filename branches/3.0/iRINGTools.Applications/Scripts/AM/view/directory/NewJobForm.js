d33070ec078b07933a56cf08aec1c1f7c23edbd1
rameshsrmbabu@gmail.com <rameshsrmbabu@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-04-22 07:16:35 +0000

    handled error for pre-selected radio button

--- branches/3.0/iRINGTools.Applications/Scripts/AM/view/directory/NewJobForm.js
+++ branches/3.0/iRINGTools.Applications/Scripts/AM/view/directory/NewJobForm.js
@@ -26,17 +26,13 @@ Ext.define('AM.view.directory.NewJobForm', {
         }, me.initialConfig);
 
         var applicationStore = Ext.create('Ext.data.Store', {
-            fields: ['appId', 'appName'], storeId: 'applicationStoreId'
+            fields: ['appId', 'appName'],
+            storeId: 'applicationStoreId'
         });
         var dataObjectStore = Ext.create('Ext.data.Store', {
-            fields: ['appId', 'dataObjId', 'dataObjName'], storeId: 'dataObjectStoreId'
+            fields: ['appId', 'dataObjId', 'dataObjName'],
+            storeId: 'dataObjectStoreId'
         });
-
-
-        //Ext.ComponentQuery.query('#myradiobutton').setValue(true);
-//        var radios = Ext.getCmp("myradiobutton");
-//        radios.setValue({checked:true});            
-
         Ext.applyIf(me, {
 
             items: [{
@@ -65,7 +61,7 @@ Ext.define('AM.view.directory.NewJobForm', {
                             labelWidth: 110,
                             name: 'contextName',
                             editable: false,
-                            allowBlank:false
+                            allowBlank: false
                         }, {
                             xtype: 'label',
                             name: 'applicationLabel',
@@ -81,7 +77,7 @@ Ext.define('AM.view.directory.NewJobForm', {
                             queryMode: 'local',
                             autoSelect: true,
                             editable: false,
-                            allowBlank:false,
+                            allowBlank: false,
                             listeners: {
                                 select: function (combo, records, eOpts) {
                                     var me = this;
@@ -94,7 +90,10 @@ Ext.define('AM.view.directory.NewJobForm', {
                                     tempStore.add(dataObjStore.getRange());
 
                                     form.getForm().findField('dataObjectName').bindStore(tempStore);
-                                    form.getForm().findField('dataObjectName').getStore().filter([{ property: 'appId', value: me.getValue()}]);
+                                    form.getForm().findField('dataObjectName').getStore().filter([{
+                                        property: 'appId',
+                                        value: me.getValue()
+                                    }]);
                                     form.getForm().findField('dataObjectName').setValue(tempStore.getRange());
                                 }
                             }
@@ -113,14 +112,14 @@ Ext.define('AM.view.directory.NewJobForm', {
                             xtype: 'checkboxlistcombo',
                             multiSelect: true,
                             name: 'dataObjectName',
-                            emptyText:'No Data Objects',
+                            emptyText: 'No Data Objects',
                             itemId: 'dataObjCombo',
                             store: dataObjectStore,
                             queryMode: 'local',
                             valueField: 'dataObjId',
                             displayField: 'dataObjName',
                             editable: false,
-                            allowBlank:false
+                            allowBlank: false
                         }]
                     }
                 ]
@@ -141,20 +140,19 @@ Ext.define('AM.view.directory.NewJobForm', {
                     items: [{
                         boxLabel: 'Immediate',
                         inputValue: 'Immediate',
-                        //checked:true,
+                        checked: true,
                         name: 'occuranceRadio',
                         margin: '4 80 0 0 ',
                         //itemId:'myradiobutton',
-                        id:'myradiobutton',
                         allowBlank: false,
-                        scope: this,
-                       // handler: this.onChange,
                         listeners: {
                             change: function (cb, checked) {
                                 Ext.ComponentQuery.query('#fldContainer')[0].setDisabled(checked);
                                 Ext.ComponentQuery.query('#checkBox')[0].setDisabled(checked);
                                 Ext.ComponentQuery.query('#myEndDateCombo')[0].setDisabled(checked);
                                 Ext.ComponentQuery.query('#myEndTimeCombo')[0].setDisabled(checked);
+                                Ext.ComponentQuery.query('#myStartDateCombo')[0].setDisabled(checked);
+                                Ext.ComponentQuery.query('#myStartTimeCombo')[0].setDisabled(checked);
                             }
                         }
                     }, {
@@ -196,22 +194,25 @@ Ext.define('AM.view.directory.NewJobForm', {
                         fieldLabel: 'Start DateTime',
                         name: 'startDate',
                         minValue: (new Date()),
+                        itemId: 'myStartDateCombo',
                         emptyText: 'Select Start Date',
                         width: 220,
-                        disabled: false,
+                        disabled: true,
                         anchor: '100%',
-                        allowBlank:false
+                        allowBlank: false
                     }, {
                         xtype: 'timefield',
                         name: 'startTime',
                         minValue: '00:00',
                         maxValue: '24:00',
+                        itemId: 'myStartTimeCombo',
                         width: 60,
                         format: 'H:i',
                         increment: 1,
                         emptyText: 'Time',
+                        disabled: true,
                         margin: '0 10 0 0',
-                        allowBlank:false,
+                        allowBlank: false,
                         anchor: '100%'
                     }]
                 }]
@@ -234,10 +235,10 @@ Ext.define('AM.view.directory.NewJobForm', {
                         minValue: (new Date()),
                         emptyText: 'Select End Date',
                         width: 220,
-                        disabled: false,
+                        disabled: true,
                         itemId: 'myEndDateCombo',
                         anchor: '100%',
-                        allowBlank:false
+                        allowBlank: false
                     }, {
                         xtype: 'timefield',
                         name: 'endTime',
@@ -247,17 +248,18 @@ Ext.define('AM.view.directory.NewJobForm', {
                         format: 'H:i',
                         increment: 1,
                         emptyText: 'Time',
-                        disabled: false,
+                        disabled: true,
                         itemId: 'myEndTimeCombo',
                         margin: '0 10 0 0',
                         anchor: '100%',
-                        allowBlank:false
+                        allowBlank: false
                     }, {
                         xtype: 'checkboxfield',
                         boxLabel: 'No End Date',
                         name: 'chkBox',
                         itemId: 'checkBox',
                         margin: '4 0 0 60',
+                        disabled: true,
                         checked: false,
                         listeners: {
                             change: function (cb, checked) {
@@ -294,15 +296,8 @@ Ext.define('AM.view.directory.NewJobForm', {
                 }]
             }]
         });
-           
-        me.callParent(arguments);
-    },
 
-    onChange: function (box, checked) {
-              alert("in radio")
-                var radios = Ext.getCmp("myradiobutton");
-                radios.setDisabled(checked);
-          
+        me.callParent(arguments);
     },
     onSave: function () {
         var me = this;
@@ -348,17 +343,19 @@ Ext.define('AM.view.directory.NewJobForm', {
                     var objResponseText = Ext.decode(request.response.responseText);
                     var userMsg = objResponseText['message'];
                     var detailMsg = objResponseText['stackTraceDescription'];
-                    var expPanel = Ext.widget('exceptionpanel', { title: 'Error Notification' });
+                    var expPanel = Ext.widget('exceptionpanel', {
+                        title: 'Error Notification'
+                    });
                     Ext.ComponentQuery.query('#expValue', expPanel)[0].setValue(userMsg);
                     Ext.ComponentQuery.query('#expValue2', expPanel)[0].setValue(detailMsg);
 
                 }
             });
-        }
-
-
-        else {
-            Ext.widget('messagepanel', { title: 'Warning', msg: 'Please complete all required fields.' });
+        } else {
+            Ext.widget('messagepanel', {
+                title: 'Warning',
+                msg: 'Please complete all required fields.'
+            });
             return;
         }
     },
@@ -367,32 +364,50 @@ Ext.define('AM.view.directory.NewJobForm', {
         var win = me.up('window');
         win.fireEvent('Cancel', me);
     }
-    //    onSave: function () {
-    //        var me = this;
-    //        var win = me.up('window');
-    //        var form = me.getForm();
-    //        var node = me.node;
-    //        var contextName = me.getForm().findField('contextName').getValue();
-    //        var applicationName = me.getForm().findField('applicationName').getValue();
-    //        var dataObjectName = me.getForm().findField('dataObjectName').getValue();
-    //        var startDate = me.getForm().findField('startDate').getValue();
-    //        var startTime = me.getForm().findField('startTime').getValue();
-    //        var endDate = me.getForm().findField('endDate').getValue();
-    //        var endTime = me.getForm().findField('endTime').getValue();
-    //        var ouccaradio = me.getForm().findField('occuranceRadio').getValue();
+    //        onSave: function () {
+    //            var me = this;
+    //            var win = me.up('window');
+    //            var form = me.getForm();
+    //            var node = me.node;
+    //            var contextName = me.getForm().findField('contextName').getValue();
+    //            var applicationName = me.getForm().findField('applicationName').getValue();
+    //            var dataObjectName = me.getForm().findField('dataObjectName').getValue();
+    //            var startDate = me.getForm().findField('startDate').getValue();
+    //            var startTime = me.getForm().findField('startTime').getValue();
+    //            var endDate = me.getForm().findField('endDate').getValue();
+    //            var endTime = me.getForm().findField('endTime').getValue();
+    //            var ouccaradio = me.getForm().findField('occuranceRadio').getValue();
 
-    //        me.getForm().submit({
-    //            waitMsg: 'Saving Data...',
-    //            success: function (response, request) {
-    //                //Ext.example.msg('Notification', 'Newjob saved successfully!');
-    //                win.fireEvent('save', me);
+    //            me.getForm().submit({
+    //                waitMsg: 'Saving Data...',
+    //                success: function (response, request) {
+    //                      var objResponseText = Ext.JSON.decode(request.response.responseText);
+    //                    if (objResponseText["message"] == "Duplicate Job") {
+    //                        showDialog(400, 50, 'Alert', "Job  name already exists", Ext.Msg.OK, null);
+    //                        return;
+    //                    }
 
-    //                Ext.Msg.alert('Success', action.result.msg);
-    //            },
-    //            failure: function (response, request) {
-    //                Ext.Msg.alert('Failed', action.result.msg);
-    //            }
-    //        });
-    //    }
+
+    //                    win.fireEvent('save', me);
+
+    //                    var currentNode;
+
+    //                    var index = 0;
+
+    //                    Ext.each(Ext.JSON.decode(request.response.responseText).nodes, function (newNode) {
+    //                        currentNode.insertChild(index, newNode);
+    //                        index++;
+    //                    });
+
+    //                    me.setLoading(false);
+    //                    if (objResponseText["message"] == "Job Added Successfuly") {
+    //                        Ext.example.msg('Notification', 'Job added successfully!');
+    //                    }
+    //                },
+    //                failure: function (response, request) {
+    //                    Ext.Msg.alert('Failed', action.result.msg);
+    //                }
+    //            });
+    //        }
+
 });
-
