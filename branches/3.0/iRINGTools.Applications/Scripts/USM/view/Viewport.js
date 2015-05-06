fda2ece2fd95e51cef99249dffdc2a6bc48dd373
farshad.kargar@gmail.com <farshad.kargar@gmail.com@e5833f40-bdb0-11de-8784-f7ad4773dbab>
2015-05-06 17:58:38 +0000

    FK - Changed caption to Security Manager.

--- branches/3.0/iRINGTools.Applications/Scripts/USM/view/Viewport.js
+++ branches/3.0/iRINGTools.Applications/Scripts/USM/view/Viewport.js
@@ -20,7 +20,7 @@ Ext.define('USM.view.Viewport', {
                     html: '<div id="header" class="banner">' +
                           '  <span style="float:left">' +
                           '    <img style="margin:0 5px 0 5px" src="./Content/img/iringtools-logo.png"/>' +
-                          '       <font style="font-size:24px;font-family:Arial">Security Configuation</font>' +
+                          '       <font style="font-size:24px;font-family:Arial">Security Manager</font>' +
                           '  </span>' +
                           '  <span style="float:right;margin-top:16px">' +
                           '      <a href="http://iringug.org/wiki/index.php?title=IRINGTools" target="_blank" class="header-link">Help</a>' +
